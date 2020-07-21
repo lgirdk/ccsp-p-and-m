8ac58b31215ec26c2b57aaa26eca898dc20c4629
sarveswaran.n <sanagarajan.contractor@libertyglobal.com>
2020-07-21 16:13:24 +0200

    add TR069 configs for LAN DHCP subnet ( CR037 )
    CR037 - Mercury V2 Configuring DHCP LAN subnet - Implement TR datamodel for hideCustomerDhcpLanChange
    CR037 - Mercury V2 Configuring DHCP LAN subnet - Implement TR data model for LanBlockedSubnetTable
    CR037 - Mercury V2 Configuring DHCP LAN subnet - Implement TR data model for LanAllowedSubnetTable
    
    NEW FEATURE:
        - Added new TR param table:
           Device.DHCPv4.Server.Pool.{i}.X_LGI-COM_LanBlockedSubnetTable
           X_LGI-COM_LanBlockedSubnetTableNumberOfEntries
           X_LGI-COM_LanBlockedSubnetTable.{i}.LanBlockedSubnetIP
           X_LGI-COM_LanBlockedSubnetTable.{i}.LanBlockedSubnetMask
        - Added new APIs to Get the tr param values
           LanBlockedSubnet_GetEntryCount
           LanBlockedSubnet_GetEntry
           LanBlockedSubnet_GetParamUlongValue
        - Add new API in DHCP init to populate the TR param values
           CosaDhcpv4InitializeLanBlockedSubnetList
        - the list contains 3 dynamic entires (MTA/CM/erouter IP, Subnetmask) apart from the default list
        - Added the new TR param table
          Device.DHCPv4.Server.Pool.{i}.X_LGI-COM_LanAllowedSubnetTable.{i}
        -Implement TR datamodel:  Device.X_LGI-COM_CloudUI.HideCustomerDhcpLanChange
    
        TESTING PERFORMED:
        - Verified GPV and SPV of below TR param:
            Device.X_LGI-COM_CloudUI.HideCustomerDhcpLanChange
        - Checked the set value persists after reboot.
        - Queried the parameters using the dmcli command
        - Verified the IP, Subnet list is as expected
        - Verified the dynamic IP, Subnet values (CM, erouter)
        - Verified the set/get for X_LGI-COM_LanAllowedSubnetTable Params
    
    Signed-off-by: sarveswaran.n <sanagarajan.contractor@libertyglobal.com>
    
    fix sscanf() format string used to parse IP addresses
    
    The COSA_DML_DHCPSV4_LANBLOCKED struct contains IP addresses stored
    in variables of type ANSC_IPV4_ADDRESS, which is an array of 4 chars
    according to:
    
      ccsp-common-library/source/cosa/include/ansc_debug_wrapper_base.h
    
      #define  ANSC_IPV4_ADDRESS                    \
            union                                   \
            {                                       \
                    unsigned char Dot[4];           \
                    unsigned long Value;            \
            }
    
    Therefore the addresses of the Dot[0], Dot[1], etc are pointers to
    unsigned characters and it's not safe to use "%d" as type conversion
    specifiers in a sscanf() format string (which will cause sscanf() to
    treat the pointers as pointers to variables as type int).
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    Duplicate/overlapping IP ranges are allowed in LanAllowedSubnetTable ( OFW-140 )
    
    DESCRIPTION:
    Adding Duplicate entry to LanAllowedSubnetTable is working from TR069, 9007 error is seen but GPV/dmcli  is showing value added
    
    Fix:
    Memmory reset is not performed inside LanAllowedSubnetTable_Rollback. Added code for resetting the memory.
    
    TESTING PERFORMED:
    1.Verified that duplicate IP ranges are not allowed in LanAllowedSubnetTable using dmcli commands and ACS.
    
    Signed-off-by: Sandhya Kizhakkayil <skizhakkayil.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/plugin_main_apis.c
+++ source/TR-181/middle_layer_src/plugin_main_apis.c
@@ -108,6 +108,8 @@
 #include "cosa_dynamicdns_apis.h"
 #include "cosa_dynamicdns_internal.h"
 #endif
+#include "cosa_lgi_cloudui_apis.h"
+#include "cosa_lgi_cloudui_internal.h"
 #ifdef DSLITE_FEATURE_SUPPORT
 #include "cosa_dslite_apis.h"
 #include "cosa_dslite_internal.h"
@@ -312,6 +314,8 @@ CosaBackEndManagerInitialize
     pMyObject->hDslite         = (ANSC_HANDLE)CosaDsliteCreate();
     AnscTraceWarning(("  CosaDsliteCreate done!\n"));
 #endif
+    pMyObject->hLgiCloudUi = (ANSC_HANDLE)CosaLgiCloudUiCreate();
+    AnscTraceWarning(("  CosaLgiCloudUiCreate done !\n"));
     pMyObject->hNeighdisc     = (ANSC_HANDLE)CosaNeighdiscCreate();
     AnscTraceWarning(("  CosaNeighdiscCreate done!\n"));
     pMyObject->hMld           = (ANSC_HANDLE)CosaMldCreate();
@@ -636,6 +640,10 @@ CosaBackEndManagerRemove
     if ( pMyObject->hMld )
     {
         CosaMldRemove((ANSC_HANDLE)pMyObject->hMld);
+    }
+    if (pMyObject->hLgiCloudUi)
+    {
+        CosaLgiCloudUiRemove((ANSC_HANDLE)pMyObject->hLgiCloudUi);
     }
     /*
 #ifdef CONFIG_TI_PACM
