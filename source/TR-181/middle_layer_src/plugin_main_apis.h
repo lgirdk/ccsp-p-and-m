f97c4a6088525d9292eaab54ddb2f21d498c77d4
sarveswaran.n <sanagarajan.contractor@libertyglobal.com>
2020-07-21 16:13:24 +0200

    add TR069 configs for LAN DHCP subnet ( CR037 )
    CR037 - Mercury V2 Configuring DHCP LAN subnet - Implement TR datamodel for hideCustomerDhcpLanChange
    CR037 - Mercury V2 Configuring DHCP LAN subnet - Implement TR data model for LanBlockedSubnetTable
    CR037 - Mercury V2 Configuring DHCP LAN subnet - Implement TR data model for LanAllowedSubnetTable
    
    NEW FEATURE:
        - Added new TR param table:
           Device.DHCPv4.Server.Pool.{i}.X_LGI-COM_LanBlockedSubnetTable
           X_LGI-COM_LanBlockedSubnetTableNumberOfEntries
           X_LGI-COM_LanBlockedSubnetTable.{i}.LanBlockedSubnetIP
           X_LGI-COM_LanBlockedSubnetTable.{i}.LanBlockedSubnetMask
        - Added new APIs to Get the tr param values
           LanBlockedSubnet_GetEntryCount
           LanBlockedSubnet_GetEntry
           LanBlockedSubnet_GetParamUlongValue
        - Add new API in DHCP init to populate the TR param values
           CosaDhcpv4InitializeLanBlockedSubnetList
        - the list contains 3 dynamic entires (MTA/CM/erouter IP, Subnetmask) apart from the default list
        - Added the new TR param table
          Device.DHCPv4.Server.Pool.{i}.X_LGI-COM_LanAllowedSubnetTable.{i}
        -Implement TR datamodel:  Device.X_LGI-COM_CloudUI.HideCustomerDhcpLanChange
    
        TESTING PERFORMED:
        - Verified GPV and SPV of below TR param:
            Device.X_LGI-COM_CloudUI.HideCustomerDhcpLanChange
        - Checked the set value persists after reboot.
        - Queried the parameters using the dmcli command
        - Verified the IP, Subnet list is as expected
        - Verified the dynamic IP, Subnet values (CM, erouter)
        - Verified the set/get for X_LGI-COM_LanAllowedSubnetTable Params
    
    Signed-off-by: sarveswaran.n <sanagarajan.contractor@libertyglobal.com>
    
    fix sscanf() format string used to parse IP addresses
    
    The COSA_DML_DHCPSV4_LANBLOCKED struct contains IP addresses stored
    in variables of type ANSC_IPV4_ADDRESS, which is an array of 4 chars
    according to:
    
      ccsp-common-library/source/cosa/include/ansc_debug_wrapper_base.h
    
      #define  ANSC_IPV4_ADDRESS                    \
            union                                   \
            {                                       \
                    unsigned char Dot[4];           \
                    unsigned long Value;            \
            }
    
    Therefore the addresses of the Dot[0], Dot[1], etc are pointers to
    unsigned characters and it's not safe to use "%d" as type conversion
    specifiers in a sscanf() format string (which will cause sscanf() to
    treat the pointers as pointers to variables as type int).
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    Duplicate/overlapping IP ranges are allowed in LanAllowedSubnetTable ( OFW-140 )
    
    DESCRIPTION:
    Adding Duplicate entry to LanAllowedSubnetTable is working from TR069, 9007 error is seen but GPV/dmcli  is showing value added
    
    Fix:
    Memmory reset is not performed inside LanAllowedSubnetTable_Rollback. Added code for resetting the memory.
    
    TESTING PERFORMED:
    1.Verified that duplicate IP ranges are not allowed in LanAllowedSubnetTable using dmcli commands and ACS.
    
    Signed-off-by: Sandhya Kizhakkayil <skizhakkayil.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/plugin_main_apis.h
+++ source/TR-181/middle_layer_src/plugin_main_apis.h
@@ -168,6 +168,7 @@ extern COSARegisterCallBackAfterInitDmlProc  g_RegisterCallBackAfterInitDml;
 #endif
 
 #define COSA_DATAMODEL_MAC_FILTER_OID                          123 /* LGI ADD */
+#define COSA_DATAMODEL_LGI_CLOUDUI_OID                         130
 #define COSA_DATAMODEL_LGI_WOL_OID                             134 /* LGI ADD */
 
 /*
@@ -218,6 +219,7 @@ extern COSARegisterCallBackAfterInitDmlProc  g_RegisterCallBackAfterInitDml;
     ANSC_HANDLE                  hFileTransfer;                                             \
     ANSC_HANDLE                  hMacFilter;                     /* LGI ADD */              \
     ANSC_HANDLE                  hLanMngm;                                                  \
+    ANSC_HANDLE                  hLgiCloudUi;                                           \
     ANSC_HANDLE                  hXpc;                                                    \
     ANSC_HANDLE                  hOnboardLogging;                                                    \
     ANSC_HANDLE                  hLgiWoL;                        /* LGI ADD             */  \
