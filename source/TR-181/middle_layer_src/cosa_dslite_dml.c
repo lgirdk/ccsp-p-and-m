52a5bd9adf0b85616ba0d6734a46b9800829d118
Sai Charan <ssirpa.contractor@libertyglobal.com>
2020-07-30 15:45:52 +0200

    implement DNS Relay parameters ( RDKB-4543 )
    TESTING PERFORMED:
    
    1) Bring the CM online.
    2) Disable DNS proxy by disabling DNS relay.
    3) DNS requests are relayed to WAN DNS servers
    4) Enable proxy and dns requests are resolved by the brlan0.

--- source/TR-181/middle_layer_src/cosa_dslite_dml.c
+++ source/TR-181/middle_layer_src/cosa_dslite_dml.c
@@ -104,7 +104,7 @@ DSLite_SetParamBoolValue
               return FALSE;
         }
         rc = system(UPDATE_RESOLV_CMD);
-        return TRUE;
+        return (rc == 0) ? TRUE : FALSE;
     }
     return FALSE;
 }
