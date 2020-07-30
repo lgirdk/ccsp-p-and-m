79e3ff6bf5bf5ca630687ba1bb20bc50efcd09fc
Sai Charan <ssirpa.contractor@libertyglobal.com>
2020-07-30 15:45:52 +0200

    implement DNS Relay parameters ( RDKB-4543 )
    FEATURE DESCRIPTION : Implementation of DNS Relay parameters
    
    TESTING PERFORMED:
    
    1) Bring the CM online.
    2) Disable DNS proxy by disabling DNS relay.
    3) DNS requests are relayed to WAN DNS servers
    4) Enable proxy and dns requests are resolved by the brlan0.

--- source/TR-181/middle_layer_src/cosa_dns_dml.c
+++ source/TR-181/middle_layer_src/cosa_dns_dml.c
@@ -1538,7 +1538,7 @@ Server1_SetParamStringValue
 
     if( AnscEqualString(ParamName, "DNSServer", TRUE) )
     {
-        if(( COSA_DML_DNS_ADDR_SRC_Static  == pDnsServer->Type ) && pDnsServer->InstanceNumber <= 2)
+        if( COSA_DML_DNS_ADDR_SRC_Static  == pDnsServer->Type ) //LGI MOD
         {
             AnscCopyString(pDnsServer->DNSServer, pString);
             return TRUE;
@@ -1774,20 +1774,14 @@ Relay_GetParamBoolValue
         BOOL*                       pBool
     )
 {
-    COSA_DML_DNS_STATUS             eRelayStatus     = COSA_DML_DNS_STATUS_Disabled;
+    //LGI MOD Start
+    PCOSA_DATAMODEL_DNS             pDns         = (PCOSA_DATAMODEL_DNS)g_pCosaBEManager->hDNS;
+    PCOSA_DML_DNS_RELAY             pRelay       = &pDns->Relay;
     
     /* check the parameter name and return the corresponding value */
     if( AnscEqualString(ParamName, "Enable", TRUE))
     {
-        /* collect value */
-        eRelayStatus = CosaDmlIpDnsGetRelayStatus(NULL);
-        if ( eRelayStatus == COSA_DML_DNS_STATUS_Enabled )
-        {
-            *pBool = TRUE;
-        }else
-        {
-            *pBool = FALSE;
-        }
+        *pBool = pRelay->bEnabled;
         return TRUE;
     }
 
@@ -1878,14 +1872,25 @@ Relay_GetParamUlongValue
         ULONG*                      puLong
     )
 {
+    //LGI MOD Start
+    PCOSA_DATAMODEL_DNS             pDns         = (PCOSA_DATAMODEL_DNS)g_pCosaBEManager->hDNS;
+    PCOSA_DML_DNS_RELAY             pRelay       = &pDns->Relay;
     /* check the parameter name and return the corresponding value */
     if( AnscEqualString(ParamName, "Status", TRUE))
     {
         /* collect value */
-        *puLong = (ULONG)CosaDmlIpDnsGetRelayStatus(NULL);
+        CosaDmlIpDnsGetRelayStatus(NULL, pRelay); //LGI ADD
+        *puLong = (ULONG)pRelay->Status;
         return TRUE;
     }
 
+    //LGI MOD Start - TR-181 defines ForwardNumberOfEntries for Forwarding table
+    if( AnscEqualString(ParamName, "ForwardNumberOfEntries", TRUE))
+    {
+        *puLong = Forwarding_GetEntryCount(hInsContext);
+        return TRUE;
+    }
+    //LGI MOD End
 
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
@@ -1982,11 +1987,15 @@ Relay_SetParamBoolValue
         BOOL                        bValue
     )
 {
+    //LGI MOD Start
+    PCOSA_DATAMODEL_DNS             pDns         = (PCOSA_DATAMODEL_DNS)g_pCosaBEManager->hDNS;
+    PCOSA_DML_DNS_RELAY             pRelay       = &pDns->Relay;
+
     /* check the parameter name and set the corresponding value */
     if( AnscEqualString(ParamName, "Enable", TRUE))
     {
         /* save update to backup */
-        CosaDmlDnsEnableRelay(NULL, (BOOLEAN)bValue);
+        pRelay->bEnabled = (BOOLEAN)bValue;
         return TRUE;
     }
 
@@ -2196,8 +2205,11 @@ Relay_Commit
         ANSC_HANDLE                 hInsContext
     )
 {
-
-    return 0;
+    //LGI MOD Start
+    PCOSA_DATAMODEL_DNS pDns = (PCOSA_DATAMODEL_DNS)g_pCosaBEManager->hDNS;
+    PCOSA_DML_DNS_RELAY pRelay = &pDns->Relay;
+    return CosaDmlDnsEnableRelay(NULL, pRelay->bEnabled);
+    //LGI MOD End
 }
 
 
@@ -2230,8 +2242,11 @@ Relay_Rollback
         ANSC_HANDLE                 hInsContext
     )
 {
-
-    return 0;
+    //LGI MOD Start
+    PCOSA_DATAMODEL_DNS pDns = (PCOSA_DATAMODEL_DNS)g_pCosaBEManager->hDNS;
+    PCOSA_DML_DNS_RELAY pRelay = &pDns->Relay;
+    return CosaDmlIpDnsGetRelayStatus(NULL, pRelay);
+    //LGI MOD End
 }
 
 
@@ -2667,8 +2682,7 @@ Forwarding_Synchronize
             CcspTraceInfo
                 ((
                     "Forwarding_Synchronize -- new entry %d.%d.%d.%d with instance number %d.\n",
-                    pForward2->DNSServer.Dot[0], pForward2->DNSServer.Dot[1],
-                    pForward2->DNSServer.Dot[2], pForward2->DNSServer.Dot[3],
+		    pForward2->DNSServer, //LGI MOD
                     pForward2->InstanceNumber
                 ));
 
@@ -2838,13 +2852,13 @@ Forwarding_GetParamUlongValue
         *puLong = pForward->Status;
         return TRUE;
     }
-
+#if 0 // LGI MOD
     if( AnscEqualString(ParamName, "DNSServer", TRUE))
     {
         *puLong = pForward->DNSServer.Value;
         return TRUE;
     }
-
+#endif
     if( AnscEqualString(ParamName, "Type", TRUE))
     {
         *puLong = pForward->Type;
@@ -2921,7 +2935,12 @@ Forwarding_GetParamStringValue
         return 0;
     }
 
-
+    if( AnscEqualString(ParamName, "DNSServer", TRUE) )
+    {
+        /* collect value */
+        AnscCopyString(pValue, pForward->DNSServer);
+        return 0;
+    }
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return -1;
 }
@@ -3063,7 +3082,7 @@ Forwarding_SetParamUlongValue
 {
     PCOSA_CONTEXT_LINK_OBJECT       pCosaContext = (PCOSA_CONTEXT_LINK_OBJECT)hInsContext;
     PCOSA_DML_DNS_RELAY_ENTRY       pForward     = (PCOSA_DML_DNS_RELAY_ENTRY)pCosaContext->hContext;
-
+#if 0
     /* check the parameter name and set the corresponding value */
     if( AnscEqualString(ParamName, "DNSServer", TRUE))
     {
@@ -3076,7 +3095,7 @@ Forwarding_SetParamUlongValue
          CcspTraceWarning(("DNSServer is only writable when Type is Static \n"));
          return FALSE;
     }
-
+#endif
 
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
@@ -3143,7 +3162,17 @@ Forwarding_SetParamStringValue
          return FALSE;
     }
 
+    if( AnscEqualString(ParamName, "DNSServer", TRUE) )
+    {
+        if ( COSA_DML_DNS_ADDR_SRC_Static  == pForward->Type )
+        {
+            AnscCopyString(pForward->DNSServer, pString);
+            return TRUE;
+        }
 
+        CcspTraceWarning(("DNSServer is only writable when Type is Static \n"));
+        return FALSE;
+    }
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
 }
