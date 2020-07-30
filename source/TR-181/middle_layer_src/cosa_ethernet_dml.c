820e7704db073631acbff7a5d9bf61f9cc70a3b1
Andre McCurdy <armccurdy@gmail.com>
2020-07-29 17:31:58 -0700

    fix misuse of "\0"
    "\0" is a pointer to an empty string (ie effectively the same as ""),
    and is NOT the same as a nul charactor (ie a char with value 0).
    
    Usage of "\0" is almost always a bug. If a pointer to an empty string
    is needed then use "", if a nul character is needed then use 0 or
    '\0'.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/middle_layer_src/cosa_ethernet_dml.c
+++ source/TR-181/middle_layer_src/cosa_ethernet_dml.c
@@ -2466,7 +2466,7 @@ Link_SetParamStringValue
         {
             pEntry->Cfg.LinkType    = COSA_DML_LINK_TYPE_LAST;
             pEntry->Cfg.LinkInstNum = 0;
-            pEntry->Cfg.LinkName[0] = "\0";
+            pEntry->Cfg.LinkName[0] = '\0';
         }
         else
         {
