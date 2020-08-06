857faeff21f49a77119b481d417b7219d075ae74
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2020-08-06 09:47:18 +0200

    add support for Device.NAT.PortMapping.1.ExternalPort etc ( RDKB-4529 + RDKB-4617 )
    DESCRIPTION:
    
      -Created a back-end variable to support front-end port mapping function.
      -Made changes to support the dml requirements.
    
    TESTING PERFORMED:
    
    1)  Compiled and loaded the image into device verified that pAndM process is running fine .
    2)  Make srue DUT online and set following parameters:
        Set Device.NAT.X_Comcast_com_EnablePortMapping to true
        Addtable Device.NAT.PortMapping.
    
        Test result: Pass. dmcli will show below message:
        addtable from/to component(eRT.com.cisco.spvtg.ccsp.pam):
        Device.NAT.PortMapping.Execution succeed.
        Device.NAT.PortMapping.1. is added.
    
    3)  Set Device.NAT.PortMapping.1. parameters
        dmcli eRT setvalues Device.NAT.PortMapping.1.Enable bool true \
        Device.NAT.PortMapping.1.ExternalPort uint 1000 \
        Device.NAT.PortMapping.1.ExternalPortEndRange uint 1003 \
        Device.NAT.PortMapping.1.InternalPort uint 2000 \
        Device.NAT.PortMapping.1.
        X_RDKCENTRAL_InternalPortEndRange uint 2003 \
        Device.NAT.PortMapping.1.Protocol string UDP \
        Device.NAT.PortMapping.1.InternalClient string 192.168.0.7 \
        Device.NAT.PortMapping.1.Description string Arris
    
        Test result: Pass. dmcli will show below message:
        setvalues from/to component(eRT.com.cisco.spvtg.ccsp.pam):
        Device.NAT.PortMapping.1.Enable Execution succeed.
    
    4)  Use dmcli command and test different condition
        I)  Do not set InternalPort and X_RDKCENTRAL_InternalPortEndRange
            dmcli eRT setvalues Device.NAT.PortMapping.1.
            Enable bool true \
            Device.NAT.PortMapping.1.ExternalPort uint 1000 \
            Device.NAT.PortMapping.1.ExternalPortEndRange uint 1003 \
            Device.NAT.PortMapping.1.Protocol string UDP \
            Device.NAT.PortMapping.1.InternalClient string 192.168.0.7 \
            Device.NAT.PortMapping.1.Description string Arris
    
            Test result: Pass.
            name: Device.NAT.PortMapping.1.InternalPort
                   type:       uint,    value: 0
            name: Device.NAT.PortMapping.1.
            X_RDKCENTRAL_InternalPortEndRange
                   type:       uint,    value: 0
    
        II)    Do not set X_RDKCENTRAL_InternalPortEndRange
            dmcli eRT deltable Device.NAT.PortMapping.1.
            dmcli eRT addtable Device.NAT.PortMapping.
            dmcli eRT setvalues Device.NAT.PortMapping.2.
            Enable bool true \
            Device.NAT.PortMapping.2.ExternalPort uint 1000 \
            Device.NAT.PortMapping.2.ExternalPortEndRange uint 1003 \
            Device.NAT.PortMapping.2.InternalPort uint 2000 \
            Device.NAT.PortMapping.2.Protocol string UDP \
            Device.NAT.PortMapping.2.InternalClient string 192.168.0.7 \
            Device.NAT.PortMapping.2.Description string Arris
    
            Test result: Pass.
            name: Device.NAT.PortMapping.2.InternalPort
                   type:       uint,    value: 2000
            name: Device.NAT.PortMapping.2.
            X_RDKCENTRAL_InternalPortEndRange
                   type:       uint,    value: 2000
    
        III)InternalPort > X_RDKCENTRAL_InternalPortEndRange
            dmcli eRT deltable Device.NAT.PortMapping.2.
            dmcli eRT addtable Device.NAT.PortMapping.
            dmcli eRT setvalues Device.NAT.PortMapping.3.
            Enable bool true \
            Device.NAT.PortMapping.3.ExternalPort uint 1000 \
            Device.NAT.PortMapping.3.ExternalPortEndRange uint 1003 \
            Device.NAT.PortMapping.3.InternalPort uint 2003 \
            Device.NAT.PortMapping.3.
            X_RDKCENTRAL_InternalPortEndRange uint 2000 \
            Device.NAT.PortMapping.3.Protocol string UDP \
            Device.NAT.PortMapping.3.InternalClient string 192.168.0.7 \
            Device.NAT.PortMapping.3.Description string Arris
    
            Test result: Fail.Execution fail
            (error code:CCSP_ERR_INVALID_PARAMETER_VALUE(9007)).
    
    5)  With the above tests verified that the backend variable is
        working fine and we can set the port mapping parameters without
        error.

--- source-arm/TR-181/board_sbapi/cosa_nat_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_nat_apis.c
@@ -1950,7 +1950,11 @@ ANSC_STATUS _AddPortMapping(
         rangeInfo.protocol = SB_2_U_PF_PPOTOCOL(pEntry->Protocol);
         rangeInfo.start_port = pEntry->ExternalPort;
         rangeInfo.rule_id = pEntry->InstanceNumber;
-        rangeInfo.internal_port_range_size = 0;
+        rangeInfo.internal_port_range_size =  ((pEntry->X_RDKCENTRAL_InternalPortEndRange) - (pEntry->InternalPort));
+        if( rangeInfo.internal_port_range_size < 0)
+        {
+            rangeInfo.internal_port_range_size = 0;
+        }
         //AnscCopyString(rangeInfo.name, pEntry->Description);
         strncpy(rangeInfo.name, pEntry->Description, sizeof(rangeInfo.name));
         strncpy(rangeInfo.dest_ipv6, pEntry->X_CISCO_COM_InternalClientV6, sizeof(rangeInfo.dest_ipv6));
@@ -1990,28 +1994,42 @@ ANSC_STATUS _Update_TriggerEnable(UtopiaContext   *pCtx, boolean_t enabled){
 }
 
 int _Check_PF_parameter(PCOSA_DML_NAT_PMAPPING pPortMapping)
-{      
-    if( pPortMapping->PublicIP.Value == 0 &&
-        ((pPortMapping->ExternalPort == 0) || 
-         (pPortMapping->ExternalPortEndRange < pPortMapping->ExternalPort) ||
-         (pPortMapping->Protocol > 3 || pPortMapping->Protocol < 1) ||
-          FALSE == CosaDmlNatChkPortRange(pPortMapping->InstanceNumber, pPortMapping->bEnabled, \
-                pPortMapping->ExternalPort, pPortMapping->ExternalPortEndRange, pPortMapping->Protocol , 0)) 
-        )
-    {
-        CcspTraceWarning(("Wrong Port Mapping parameter external Port %d ~ %d, protocol %d, InternalPort %d,InternalClient %x PublicIP %x\n", \
-                   pPortMapping->ExternalPort, pPortMapping->ExternalPortEndRange, \
-                   pPortMapping->Protocol,pPortMapping->InternalPort, pPortMapping->InternalClient.Value,\
-                   pPortMapping->PublicIP.Value ));
-        return FALSE;
-    }
-    
-    if( pPortMapping->InternalClient.Value == 0 || 
-        FALSE == CosaDmlNatChkPortMappingClient(pPortMapping->InternalClient.Value)){
-        CcspTraceWarning(("Wrong InternalClient value %x\n",pPortMapping->InternalClient.Value ));
-        return FALSE;
-    }
-    return TRUE;
+{
+	if((pPortMapping->ExternalPort != 0) &&
+	pPortMapping->ExternalPortEndRange != 0 &&
+	pPortMapping->Protocol >0 )
+	{
+	if(pPortMapping->PublicIP.Value == 0 &&
+	((pPortMapping->ExternalPortEndRange < pPortMapping->ExternalPort) ||
+	pPortMapping->Protocol > 3 || pPortMapping->Protocol < 1 ||
+	FALSE == CosaDmlNatChkPortRange(pPortMapping->InstanceNumber, pPortMapping->bEnabled, \
+		pPortMapping->ExternalPort, pPortMapping->ExternalPortEndRange, pPortMapping->Protocol , 0))
+	)
+	{
+		CcspTraceWarning(("Wrong Port Mapping parameter external Port %d ~ %d, protocol %d, InternalPort %d,InternalClient %x PublicIP %x\n", \
+		pPortMapping->ExternalPort, pPortMapping->ExternalPortEndRange, \
+		pPortMapping->Protocol,pPortMapping->InternalPort, pPortMapping->InternalClient.Value,\
+		pPortMapping->PublicIP.Value ));
+		return FALSE;
+	}
+	}
+
+	if((pPortMapping->X_RDKCENTRAL_InternalPortEndRange < pPortMapping->InternalPort) && (pPortMapping->X_RDKCENTRAL_InternalPortEndRange != 0))
+	{
+		CcspTraceWarning(("Wrong Port Mapping parameter internal Port %d ~ %d\n", \
+		pPortMapping->InternalPort, pPortMapping->X_RDKCENTRAL_InternalPortEndRange));
+        }
+
+	if( pPortMapping->InternalClient.Value != 0 )
+	{
+	if(FALSE == CosaDmlNatChkPortMappingClient(pPortMapping->InternalClient.Value))
+	{
+		CcspTraceWarning(("Wrong InternalClient value %x\n",pPortMapping->InternalClient.Value ));
+		return FALSE;
+	}
+	}
+
+	return TRUE;
 }
 
 
@@ -2038,19 +2056,19 @@ int _Check_PT_parameter(PCOSA_DML_NAT_PTRIGGER pPortTrigger)
     return TRUE;
 }
 
-/* IP is type of ANSC_IPV4_ADDRESS, but ANSC_IPV4_ADDRESS is defind as union <anonymous> we cannot use it as parameter */  
-static inline void _sent_syslog_pm_sb(char *opt, UCHAR protocol, USHORT external, USHORT external_end, USHORT internal, UCHAR ip[4], BOOLEAN active)
+/* IP is type of ANSC_IPV4_ADDRESS, but ANSC_IPV4_ADDRESS is defind as union <anonymous> we cannot use it as parameter */
+static inline void _sent_syslog_pm_sb(char *opt, UCHAR protocol, USHORT external, USHORT external_end, USHORT internal, USHORT internal_end, UCHAR ip[4], BOOLEAN active)  
 {
-    char extPort[30], intPort[20];
+    char extPort[30], intPort[30];
     if(external_end == 0)
         sprintf(extPort, "ExternelPort %d", external);
     else
         sprintf(extPort, "ExternelPort(s) %d~%d", external, external_end);
     
-    if(internal != 0)
+    if(internal_end == 0)
         sprintf(intPort, "InternelPort %d",internal); 
     else
-        intPort[0]='\0';
+        sprintf(intPort, "InternelPort(s) %d~%d", internal, internal_end);
 
     syslog_systemlog("PortMapping", LOG_NOTICE, "%s: %s %s%s IP %d.%d.%d.%d %s", opt,\
         (protocol==1)? "TCP": ((protocol == 2) ? "UDP" : "TCP/UDP"), \
@@ -2068,7 +2086,7 @@ static inline void _sent_syslog_pt_sb(char *opt, UCHAR protocol, USHORT trigger_
 
 static inline void _sent_syslog_pm_u(char *opt, protocol_t protocol, int external, int external_end, int internal, char *ip, boolean_t active)
 {
-    char extPort[30], intPort[20];
+    char extPort[30], intPort[30];
     if(external_end == 0)
         sprintf(extPort, "ExternelPort %d", external);
     else
@@ -2383,6 +2401,7 @@ CosaDmlNatGetPortMapping
         pNatPMapping->ExternalPort = rangeInfo.start_port;
         pNatPMapping->ExternalPortEndRange = rangeInfo.end_port;
         pNatPMapping->InternalPort = rangeInfo.internal_port;
+        pNatPMapping->X_RDKCENTRAL_InternalPortEndRange = (rangeInfo.internal_port + rangeInfo.internal_port_range_size);
         pNatPMapping->LeaseDuration = 0;
         pNatPMapping->Protocol = U_2_SB_PF_PROTOCOL(rangeInfo.protocol);
         pNatPMapping->RemoteHost.Value = 0;
@@ -2405,6 +2424,7 @@ CosaDmlNatGetPortMapping
         pNatPMapping->ExternalPort = singleInfo.external_port;
         pNatPMapping->ExternalPortEndRange = pNatPMapping->ExternalPort;
         pNatPMapping->InternalPort = singleInfo.internal_port;
+        pNatPMapping->X_RDKCENTRAL_InternalPortEndRange = pNatPMapping->InternalPort ;
         pNatPMapping->LeaseDuration = 0;
         pNatPMapping->Protocol = U_2_SB_PF_PROTOCOL(singleInfo.protocol);
         pNatPMapping->RemoteHost.Value = 0;
@@ -2457,6 +2477,7 @@ CosaDmlNatGetPortMapping
                 pNatPMapping->ExternalPort = dynInfo.external_port;
                 pNatPMapping->ExternalPortEndRange = pNatPMapping->ExternalPort;
                 pNatPMapping->InternalPort = dynInfo.internal_port;
+                pNatPMapping->X_RDKCENTRAL_InternalPortEndRange = pNatPMapping->InternalPort; 
                 pNatPMapping->LeaseDuration = dynInfo.lease;
                 pNatPMapping->PublicIP.Value = 0; 
                 pNatPMapping->Protocol = U_2_SB_PF_PROTOCOL(dynInfo.protocol);
@@ -2646,6 +2667,7 @@ CosaDmlNatGetPortMappings
             pNatPMapping[ulIndex].ExternalPort = singleInfo[i].external_port;
             pNatPMapping[ulIndex].ExternalPortEndRange = pNatPMapping[ulIndex].ExternalPort;
             pNatPMapping[ulIndex].InternalPort = singleInfo[i].internal_port;
+            pNatPMapping[ulIndex].X_RDKCENTRAL_InternalPortEndRange= pNatPMapping[ulIndex].InternalPort;
             pNatPMapping[ulIndex].LeaseDuration = 0;
             pNatPMapping[ulIndex].Protocol = U_2_SB_PF_PROTOCOL(singleInfo[i].protocol);
             pNatPMapping[ulIndex].RemoteHost.Value = 0;
@@ -2670,6 +2692,7 @@ CosaDmlNatGetPortMappings
             pNatPMapping[ulIndex].ExternalPort = rangeInfo[i].start_port;
             pNatPMapping[ulIndex].ExternalPortEndRange = rangeInfo[i].end_port;
             pNatPMapping[ulIndex].InternalPort = rangeInfo[i].internal_port;
+            pNatPMapping[ulIndex].X_RDKCENTRAL_InternalPortEndRange = (rangeInfo[i].internal_port +  rangeInfo[i].internal_port_range_size);
             pNatPMapping[ulIndex].LeaseDuration = 0;
             pNatPMapping[ulIndex].Protocol = U_2_SB_PF_PROTOCOL(rangeInfo[i].protocol);
             pNatPMapping[ulIndex].RemoteHost.Value = 0;
@@ -2713,6 +2736,7 @@ CosaDmlNatGetPortMappings
             pNatPMapping[ulIndex].ExternalPort = dynInfo.external_port;
             pNatPMapping[ulIndex].ExternalPortEndRange = pNatPMapping[ulIndex].ExternalPort;
             pNatPMapping[ulIndex].InternalPort = dynInfo.internal_port;
+            pNatPMapping[ulIndex].X_RDKCENTRAL_InternalPortEndRange = pNatPMapping[ulIndex].InternalPort;
             pNatPMapping[ulIndex].LeaseDuration = dynInfo.lease;
             pNatPMapping[ulIndex].Protocol = U_2_SB_PF_PROTOCOL(dynInfo.protocol);
             pNatPMapping[ulIndex].RemoteHost.Value =  inet_addr(dynInfo.external_host);
@@ -2847,12 +2871,12 @@ CosaDmlNatAddPortMapping
     if( ANSC_STATUS_SUCCESS == _AddPortMapping(&Ctx, pEntry))
     {
         Utopia_Free(&Ctx, 1);
-        _sent_syslog_pm_sb("ADD", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, pEntry->InternalClient.Dot,pEntry->bEnabled);
+        _sent_syslog_pm_sb("ADD", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, pEntry->X_RDKCENTRAL_InternalPortEndRange, pEntry->InternalClient.Dot,pEntry->bEnabled);
         return ANSC_STATUS_SUCCESS;
     }
     else
     {
-        _sent_syslog_pm_sb("ADD FAILED", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, pEntry->InternalClient.Dot,pEntry->bEnabled);
+        _sent_syslog_pm_sb("ADD FAILED", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, pEntry->X_RDKCENTRAL_InternalPortEndRange, pEntry->InternalClient.Dot,pEntry->bEnabled);
         Utopia_Free(&Ctx, 0);
         return ANSC_STATUS_FAILURE;
     }
@@ -3063,7 +3087,7 @@ CosaDmlNatSetPortMapping
         rc = Utopia_DelPortForwardingRangeByRuleId(&Ctx, pEntry->InstanceNumber);
         if(rc != SUCCESS){ 
             CcspTraceWarning((" Utopia_DelPortForwardingRangeByRuleId failed rc %d in %s\n", rc, __FUNCTION__));
-            _sent_syslog_pm_sb("EDIT FAILED", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, pEntry->InternalClient.Dot,pEntry->bEnabled);
+            _sent_syslog_pm_sb("EDIT FAILED", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, pEntry->X_RDKCENTRAL_InternalPortEndRange, pEntry->InternalClient.Dot,pEntry->bEnabled);
             Utopia_Free(&Ctx, 0);
             return ANSC_STATUS_FAILURE;
         }
@@ -3077,7 +3101,7 @@ CosaDmlNatSetPortMapping
        rc = Utopia_DelPortForwardingByRuleId(&Ctx, pEntry->InstanceNumber);
        if(rc != SUCCESS){
             CcspTraceWarning((" Utopia_DelPortForwardingByRuleId failed rc %d in %s\n", rc, __FUNCTION__));
-            _sent_syslog_pm_sb("EDIT FAILED", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, pEntry->InternalClient.Dot,pEntry->bEnabled);
+            _sent_syslog_pm_sb("EDIT FAILED", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, pEntry->X_RDKCENTRAL_InternalPortEndRange, pEntry->InternalClient.Dot,pEntry->bEnabled);
             Utopia_Free(&Ctx, 0);
             return ANSC_STATUS_FAILURE;
        }
@@ -3086,13 +3110,13 @@ CosaDmlNatSetPortMapping
     if( ANSC_STATUS_SUCCESS == _AddPortMapping(&Ctx, pEntry))
     {
         Utopia_Free(&Ctx, 1);
-        _sent_syslog_pm_sb("ADD", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, pEntry->InternalClient.Dot,pEntry->bEnabled);
+        _sent_syslog_pm_sb("ADD", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, (pEntry->X_RDKCENTRAL_InternalPortEndRange-pEntry->InternalPort), pEntry->InternalClient.Dot,pEntry->bEnabled);
         return ANSC_STATUS_SUCCESS;
     }
     else
     {
         Utopia_Free(&Ctx, 1);
-        _sent_syslog_pm_sb("ADD FAILED", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, pEntry->InternalClient.Dot,pEntry->bEnabled);
+        _sent_syslog_pm_sb("ADD FAILED", pEntry->Protocol, pEntry->ExternalPort, pEntry->ExternalPortEndRange, pEntry->InternalPort, (pEntry->X_RDKCENTRAL_InternalPortEndRange-pEntry->InternalPort), pEntry->InternalClient.Dot,pEntry->bEnabled);
         return ANSC_STATUS_FAILURE;
     }
 
