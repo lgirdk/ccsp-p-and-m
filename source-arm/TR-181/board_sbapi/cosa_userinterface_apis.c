07758bea9c0343a9afc6d5be8a36b179b10a428d
Chandra Sekhar (Infosys) <csekha.contractor@libertyglobal.com>
2020-08-07 11:59:40 +0200

    add MaxPasswordLockoutTimes + LoginFailureWindow to Device.UserInterface ( RDKB-4587 )
    Eanhance Configurable Login Security Parameters
    
    [Description]: Device.UserInterface object will be extended with following parametres to meet LGI requirements
    
    MaxPasswordLockoutTimes - The maximum consecutive number of WebGUI account lockout periods allowed before requiring a device reset
    
    LoginFailureWindow - The window of time to count WebGUI login attempt failures before reaching the maximum failed login attempts and starting the lockout period
    
    TESTING PERFORMED:
    
    set/get operations performed for newly added parameters
    
      Device.UserInterface.MaxPasswordLockoutTimes
      Device.UserInterface.LoginFailureWindow

--- source-arm/TR-181/board_sbapi/cosa_userinterface_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_userinterface_apis.c
@@ -392,6 +392,16 @@ CosaDmlUserInterfaceGetCfg
                pCfg->bHTTPSecurityHeaderEnable = FALSE;
        }
 
+        if (syscfg_get (NULL, "LoginFailureWindow", buf, sizeof(buf)) == 0)
+        {
+            pCfg->LoginFailureWindow = atoi(buf);
+        }
+
+        if (syscfg_get (NULL, "MaxPasswordLockoutTimes", buf, sizeof(buf)) == 0)
+        {
+            pCfg->MaxPasswordLockoutTimes = atoi(buf);
+        }
+
     return ANSC_STATUS_SUCCESS;
 }
 
@@ -432,6 +442,16 @@ sprintf(buf, "%lu",  pCfg->PasswordLockoutTime);
 	 if (syscfg_set(NULL, "PasswordLockoutTime", buf) != 0) {
                      AnscTraceWarning(("%s : PasswordLockoutTime syscfg_set failed\n",__FUNCTION__));
 	}
+
+        sprintf(buf, "%d",  pCfg->MaxPasswordLockoutTimes);
+        if (syscfg_set(NULL, "MaxPasswordLockoutTimes", buf) != 0) {
+            AnscTraceWarning(("%s : MaxPasswordLockoutTimes syscfg_set failed\n",__FUNCTION__));
+        }
+
+        sprintf(buf, "%d",  pCfg->LoginFailureWindow);
+        if (syscfg_set(NULL, "LoginFailureWindow", buf) != 0) {
+            AnscTraceWarning(("%s : LoginFailureWindow syscfg_set failed\n",__FUNCTION__));
+        }
 
          if (syscfg_commit() != 0) {
                            AnscTraceWarning(("syscfg_commit failed\n"));
