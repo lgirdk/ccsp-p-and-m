4c94c7bb95e7edc1361101289b76f05e61507088
Sandipan Sinha <ssinha.contractor@libertyglobal.com>
2020-08-09 11:11:05 +0200

    add Device.X_LGI-COM_Gateway.IPv6LANMode ( RDKB-4586 CR017 )
    Adding DML support for IPv6 LAN Mode that is a data model
    object required for supporting stateful and stateless modes of operation.
    
    NEW FEATURE:
    CR017: New LGI-COM Data Model parameter for DHCPv6
    Stateful/Stateless configuration.
    
    FEATURE DESCRIPTION:
    
    - Added custom data model "Device.X_LGI-COM_Gateway.IPv6LANMode" for DHCPv6
      stateful/stateless configuration.
    
    - Update M-bit, O-bit, and A-bit settings in the IPv6
      Router Advertisements for Stateful and Stateless mode.
    
    - As per Observations,
    
      M-bit- Managed Address Conf Flag. It indicates that the client may use
             DHCPv6 to retrieve a Managed IPv6 address from a DHCPv6 server
    
      O-bit- Other Conf Flag. It indicates that other configuration information
             like DNS-related information is available via DHCPv6.
    
      A-bit- Implemented code changes for A-bit as per the requirement.
             It is for AutoConf
    
    TESTING PERFORMED:
    
    1. Make sure DUT online.
    2. Check for the new TR-181 datamodel through dmcli
       Device.X_LGI-COM_Gateway.IPv6LANMode.
    3. Switch between "Stateful" and "Stateless" modes
       of DHCP LAN for IPv6 clients.
    4. GPV/SPV of parameters via dmcli and tr069.
    5. Made sure connected lan client gets IPv6 address for both Stateful and Stateless modes.
    6. Verified flag values of router advertisement with the help of packet capture via wireshark.
    
    Signed-off-by: Sandipan Sinha <ssinha.contractor@libertyglobal.com>
    
    update call to _get_shell_output() from LGI code in cosa_dhcpv6_client_info()
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    drop NULL pointer check from CosaDmlLgiGwGetIpv6LanMode()
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

---
+++ source/TR-181/middle_layer_src/cosa_x_lgi_com_gateway_apis.h
@@ -0,0 +1,29 @@
+/*********************************************************************
+ * Copyright 2017-2019 ARRIS Enterprises, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************/
+
+#ifndef _COSA_LGI_GATEWAY_APIS_H
+#define _COSA_LGI_GATEWAY_APIS_H
+
+#include "../middle_layer_src/cosa_apis.h"
+
+/**********************************************************************
+                FUNCTION PROTOTYPES
+**********************************************************************/
+
+ANSC_STATUS CosaDmlLgiGwGetIpv6LanMode ( ANSC_HANDLE hContext, ULONG *pValue );
+ULONG CosaDmlLgiGwSetIpv6LanMode ( ANSC_HANDLE hContext, ULONG ulValue );
+
+#endif
