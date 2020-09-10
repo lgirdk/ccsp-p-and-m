a33dccef90a003a8d3ae275a1845b53b4e35cc92
Andre McCurdy <armccurdy@gmail.com>
2020-09-10 12:27:14 -0700

    replace various hardcoded buffer size constants with sizeof()
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_mld_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_mld_apis.c
@@ -250,7 +250,7 @@ CosaDmlMldGetCfg
 
     if ( (pid_f = fopen("/var/run/ecmh.pid", "r")) != NULL )
     {
-        if(fgets(buffer, 255,pid_f))
+        if(fgets(buffer, sizeof(buffer) ,pid_f) != NULL)
         {
             fclose(pid_f);
             pid_f = NULL;
@@ -308,7 +308,7 @@ CosaDmlMldSetCfg
 
     if ( (pid_f = fopen("/var/run/ecmh.pid", "r")) != NULL )
     {
-        if(fgets(buffer, 255,pid_f))
+        if(fgets(buffer, sizeof(buffer), pid_f) != NULL)
         {
             fclose(pid_f);
             pid_f = NULL;
@@ -367,7 +367,7 @@ CosaDmlMldGetInfo
 
     if ( (pid_f = fopen("/var/run/ecmh.pid", "r")) != NULL )
     {
-        if(fgets(buffer, 255,pid_f))
+        if(fgets(buffer, sizeof(buffer), pid_f) != NULL)
         {
             fclose(pid_f);
             pid_f = NULL;
@@ -418,7 +418,7 @@ CosaDmlMldGetGroup
 
     if ( (pid_f = fopen("/var/run/ecmh.pid", "r")) != NULL )
     {
-        if(fgets(buffer, 255,pid_f))
+        if(fgets(buffer, sizeof(buffer), pid_f) != NULL)
         {
             fclose(pid_f);
             pid_f = NULL;
@@ -446,9 +446,9 @@ CosaDmlMldGetGroup
 
     if ( (stat_f = fopen("/var/run/ecmh.dump", "r")) != NULL )
     {
-        fgets(buffer, 255, stat_f); /* Ignore the first line */
+        fgets(buffer, sizeof(buffer), stat_f); /* Ignore the first line */
         
-        while(fgets(buffer, 255,stat_f))
+        while(fgets(buffer, sizeof(buffer), stat_f) != NULL)
         {
 			st = NULL;
             pch = strtok_r(buffer, " \t\r\n", &st);
