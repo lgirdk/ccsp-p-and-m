bb6974bed8f63d0fce135ab5534b70c9de3fa275
Andre McCurdy <armccurdy@gmail.com>
2020-09-10 12:27:14 -0700

    fix various memset() issues (swapped arguments + hardcoded sizes) in cosa_deviceinfo_apis.c
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
@@ -454,7 +454,7 @@ CosaDmlDiGetModelName
     UCHAR model[128];
     char temp[2];
 
-    memset(model,0,128);
+    memset(model,0,sizeof(model));
     plat_GetFlashValue("model", model);
     
     sprintf(temp, "%x%x",model[0],model[1]);
@@ -548,7 +548,7 @@ CosaDmlDiGetSerialNumber
     )
 {
     UCHAR unitsn[128];
-    memset(unitsn,0,128);
+    memset(unitsn,0,sizeof(unitsn));
 
 #ifdef _COSA_DRG_TPG_
     plat_GetFlashValue("unitsn", unitsn);
@@ -583,7 +583,7 @@ CosaDmlDiGetHardwareVersion
 #elif _COSA_DRG_TPG_
 //Replace this with syscfg if we are pulling this from Cable modem later on 
     UCHAR hwVersion[128];
-    memset(hwVersion,0,128);    
+    memset(hwVersion,0,sizeof(hwVersion));
     plat_GetFlashValue("hwid", hwVersion);
     sprintf(pValue, "%X%X",hwVersion[0],hwVersion[1]);
     *pulSize = AnscSizeOfString(pValue);
@@ -969,7 +969,7 @@ CosaDmlGetTCPImplementation
     FILE *fp;
 
     AnscCopyString(pValue, _ERROR_);
-    memset(value,0,10);
+    memset(value,0,sizeof(value));
 
     fp = popen("cat /proc/sys/net/ipv4/tcp_congestion_control", "r");
     if (fp == NULL)
@@ -978,7 +978,7 @@ CosaDmlGetTCPImplementation
         return ANSC_STATUS_FAILURE;
     }
    
-    while(fgets(value, 25, fp) != NULL)
+    while(fgets(value, sizeof(value), fp) != NULL)
     {
         AnscCopyString(pValue ,value);
     }
@@ -1074,7 +1074,7 @@ CosaDmlDiGetFirmwareUpgradeStartTime
     }
     else
     {
-        while(fgets(value, 25, fp) != NULL)
+        while(fgets(value, sizeof(value), fp) != NULL)
         {
             AnscCopyString(pValue ,value);
         }
@@ -1101,14 +1101,14 @@ CosaDmlDiGetFirmwareUpgradeStartTime
     char value[25];
     FILE *fp;
     AnscCopyString(pValue, _ERROR_);
-    memset(value,0,10);
+    memset(value,0,sizeof(value));
     fp = popen("cat /nvram/.FirmwareUpgradeStartTime", "r");
     if (fp == NULL)
     {
         CcspTraceError(("ERROR '%s'\n","ERROR"));
         return ANSC_STATUS_FAILURE;
     }
-    while(fgets(value, 25, fp) != NULL)
+    while(fgets(value, sizeof(value), fp) != NULL)
     {
         AnscCopyString(pValue ,value);
     }
@@ -1140,7 +1140,7 @@ CosaDmlDiGetFirmwareUpgradeEndTime
     }
     else
     {
-        while(fgets(value, 25, fp) != NULL)
+        while(fgets(value, sizeof(value), fp) != NULL)
         {
             AnscCopyString(pValue ,value);
         }
@@ -1166,14 +1166,14 @@ CosaDmlDiGetFirmwareUpgradeEndTime
     char value[25];
     FILE *fp;
     AnscCopyString(pValue, _ERROR_);
-    memset(value,0,10);
+    memset(value,0,sizeof(value));
     fp = popen("cat /nvram/.FirmwareUpgradeEndTime", "r");
     if (fp == NULL)
     {
         CcspTraceError(("ERROR '%s'\n","ERROR"));
         return ANSC_STATUS_FAILURE;
     }
-    while(fgets(value, 25, fp) != NULL)
+    while(fgets(value, sizeof(value), fp) != NULL)
     {
         AnscCopyString(pValue ,value);
     }
@@ -1545,7 +1545,7 @@ int COSADmlSetMemoryStatus(char * ParamName, ULONG val)
     if(AnscEqualString(ParamName, "X_RDKCENTRAL-COM_FreeMemThreshold", TRUE))
      {
             char buf[10];
-	    memset(buf,sizeof(buf),0);
+	    memset(buf,0,sizeof(buf));
 	    snprintf(buf,sizeof(buf),"%d",val);            		    
 	    if ((syscfg_set(NULL, "MinMemoryThreshold_Value", buf) != 0)) 
 	    {
@@ -1641,7 +1641,7 @@ ULONG COSADmlGetMemoryStatus(char * ParamName)
      else if(AnscEqualString(ParamName, "X_RDKCENTRAL-COM_FreeMemThreshold", TRUE))
      {
 	char buf[10];
-	memset(buf,sizeof(buf),0);
+	memset(buf,0,sizeof(buf));
         syscfg_get( NULL, "MinMemoryThreshold_Value", buf, sizeof(buf));
         if( buf != NULL )
         {
@@ -1662,7 +1662,7 @@ ULONG COSADmlGetMaxWindowSize()
     ULONG winSize = 0;
     char value[10];
 
-    memset(value,0,10);
+    memset(value,0,sizeof(value));
     fp = popen("cat /proc/sys/net/core/wmem_max", "r");
     if (fp == NULL)
     {
@@ -1707,7 +1707,7 @@ CosaDmlDiGetProcessorSpeed
     char *pcur;
     FILE *fp;
 
-    memset(line, 0, MAX_LINE_SIZE);
+    memset(line, 0, sizeof(line));
 
 #ifdef _COSA_BCM_ARM_
 
