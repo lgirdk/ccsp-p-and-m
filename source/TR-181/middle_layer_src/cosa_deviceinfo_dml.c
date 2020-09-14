a1393f03e42f979b11c2da7864c4e1914d7f7904
subhradeep <sunath.contractor@libertyglobal.com>
2020-09-14 09:27:32 +0200

    RDKB-4570: Serial number on UI and TR-069 does not match with HW SN.
    Device.DeviceInfo.SerialNumber parameter reflects eRouter MAC address per
    LGI requirements, but LGI wants UI to display the actual serial number.
    
    FIX DESCRIPTION:
        - Added vendor specific parameter Device.DeviceInfo.X_LGI-COM_SerialNumber
          that will always report the real serial number
        - Added TR069 Alias for Device.DeviceInfo.X_LGI-COM_SerialNumber
    
    TESTING PERFORMED:
        - Verified UI displays actual serial number
        - Verified Device.DeviceInfo.X_LGI-COM_SerialNumber appears in the LGI
          DataModel reports
    
    signed off by: subhradeep nath<sunath.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
@@ -804,6 +804,13 @@ DeviceInfo_GetParamStringValue
         return 0;
     }
 
+    /* This parameter always returns the actual serial number */
+    if (strcmp(ParamName, "X_LGI-COM_SerialNumber") == 0)
+    {
+        CosaDmlDiGetSerialNumber(NULL, pValue, pulSize);
+        return 0;
+    }
+
     if (strcmp(ParamName, "HardwareVersion") == 0)
     {
         /* collect value */
