f0d762389e222d376fc00742e9f784e24b17eb80
subhradeep <sunath.contractor@libertyglobal.com>
2020-09-14 09:07:49 +0200

    RDKB-4570: Modem had gone to reboot after soak test because "out_of_memory"
    FIX DESCRIPTION:
    1. Fixed the memory leaks caused by joinable threads
    
    TESTING PERFORMED:
    1. Verfied load build success.
    2. Verfied basic functions work.
    
    signed off by: subhradeep nath<sunath.contractor@libertyglobal.com>

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
@@ -4772,6 +4772,7 @@ static void configBridgeMode(int bEnable) {
         char primarybrp[5];
         char brpdm[50];
         char brmode[5];
+        void *handle;
         PCOSA_NOTIFY_WIFI pnotifypara = (PCOSA_NOTIFY_WIFI)AnscAllocateMemory(sizeof(COSA_NOTIFY_WIFI)); /*RDKB-6845, CID-32969, free unused resource before return */
 
         memset(pnotifypara, 0, sizeof(COSA_NOTIFY_WIFI));
@@ -4808,7 +4809,8 @@ static void configBridgeMode(int bEnable) {
         totalticket += 1;
         pnotifypara->flag = brmode[0] == '3' ? 3 : bEnable;
         pnotifypara->ticket = totalticket;
-        AnscCreateTask(bridge_mode_wifi_notifier_thread, USER_DEFAULT_TASK_STACK_SIZE, USER_DEFAULT_TASK_PRIORITY, (void *)pnotifypara, "BridgeModeWifiNotifierThread");
+        handle = AnscCreateTask(bridge_mode_wifi_notifier_thread, USER_DEFAULT_TASK_STACK_SIZE, USER_DEFAULT_TASK_PRIORITY, (void *)pnotifypara, "BridgeModeWifiNotifierThread");
+        pthread_detach((pthread_t)handle);
 }
 
 ANSC_STATUS
