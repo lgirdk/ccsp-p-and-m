d9af567471247ac5a3e9fb6169124e19dc69861c
subhradeep <sunath.contractor@libertyglobal.com>
2020-09-21 12:02:35 +0200

    add support for Device.DHCPv6.Server.Pool.{i}.X_LGI-COM_MaxClients
    RDKB 4657: Implementation of following TR-181 parameters
    
        - Device.DHCPv6.Server.Pool.{i}.X_LGI-COM_MaxClients
    
    TESTING PERFORMED:
        1. Bring up the DUT online.
        2. Ensure the connectivity to ACS.
        3. Perform GPV for the parameters and ensured its successfull.
        4. Verified data model value synchronization via ACS and dmcli

--- source/TR-181/middle_layer_src/cosa_dhcpv6_dml.c
+++ source/TR-181/middle_layer_src/cosa_dhcpv6_dml.c
@@ -4538,6 +4538,15 @@ Pool1_GetParamUlongValue
         return TRUE;
     }
 
+    if( AnscEqualString(ParamName, "X_LGI-COM_MaxClients", TRUE) )
+    {
+        /* collect value */
+        returnStatus = CosaDmlDhcpv6sMaxClient( NULL, pPool->Cfg.InstanceNumber, puLong);
+        if (returnStatus == ANSC_STATUS_SUCCESS)
+            return TRUE;
+        else
+            return FALSE;
+    }
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
 }
