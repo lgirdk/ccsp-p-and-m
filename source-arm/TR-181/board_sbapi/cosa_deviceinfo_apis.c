f7baf5b3046a6be91c508e0be7149685069cb6d7
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2020-10-09 11:47:16 +0200

    return "Docsis_Gateway" for Device.DeviceInfo.DeviceCategory
    Defining the DeviceCategory as " Docsis_Gateway"(RDKB-4771 CcspPAM Porting (LGI Objects))
    
    DESCRIPTION:
    
    Defining the DeviceCategory as " Docsis_Gateway". Currently it is returning NULL.
    
    TESTING PERFORMED:-
    
    1.Bring the modem online.
    2.Verified that Device.DeviceInfo.DeviceCategory is returning Docsis_Gateway

--- source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
@@ -394,7 +394,7 @@ CosaDmlDiGetDeviceCategory
         ULONG*                      pulSize
     )
 {
-    pValue[0] = '\0';
+    AnscCopyString(pValue, "DOCSIS_Gateway");
     *pulSize = AnscSizeOfString(pValue);
     return ANSC_STATUS_SUCCESS;
 }
