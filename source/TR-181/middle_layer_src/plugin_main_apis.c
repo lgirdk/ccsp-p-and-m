38951af4bf6424fbba7cda707809dfb7383389fe
Tarun Saha <tsaha@libertyglobal.com>
2020-10-12 12:14:26 -0700

    implement Device.DeviceInfo.TemperatureStatus. datamodel ( OFW-72 )
    Implemented Device.DeviceInfo.TemperatureStatus object.
    
    Actually reading the temperature is platform specific and stubbed out
    for now. Eventually there should be a HAL API for it?
    
    block attempts to set active notification on Device.DeviceInfo.TemperatureStatus.TemperatureSensor.1.LastUpdate ( OFW-1607 )
    
    Description:
    [Mv2+] Attempt to set active notification on Device.DeviceInfo.TemperatureStatus.TemperatureSensor.1.LastUpdate does not result in fault code.
    
    Test steps:
    1. Make sure that DUT is registered on the ACS
    2. Execute SetParameterAttributes RPC by providing ParameterName as  Device.DeviceInfo.TemperatureStatus.TemperatureSensor.1.LastUpdate,
    AccesslistChange as False,
    Notification as 2,
    NotificationChange as true,
    Access List as empty
    
    Result :
    SetParameterAttributesResponse has fault element with faultcode of 9009 and faultstring as "Notification Request Rejected".
    
    Signed-off-by: Arundhati aghose.contractor@libertyglobal.com
    
    fix GPV for Device.DeviceInfo.TemperatureStatus.TemperatureSensor.{}. ( OFW-1820 )
    
    BUG DESCRIPTION:
    
    [Mv2+] GPV for Device.DeviceInfo.TemperatureStatus.TemperatureSensor.{}. was failed due to datetime format mismatch of the parameters.
    
    FIX DESCRIPTION:
    
    Modified the datetime format as per the requirement.
    
    Signed-off-by: Cibil P <cpankiras.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/plugin_main_apis.c
+++ source/TR-181/middle_layer_src/plugin_main_apis.c
@@ -495,6 +495,8 @@ if(id != 0)
     // LGI ADD START
     pMyObject->hLgiWoL = (ANSC_HANDLE)CosaLgiWoLCreate();
     AnscTraceWarning(("  CosaLgiWoLCreate done !\n"));
+    pMyObject->hTemperatureStatus    = (ANSC_HANDLE)CosaTemperatureStatusCreate();
+    AnscTraceWarning(("  CosaTemperatureStatusCreate done!\n"));
     // LGI ADD END
 
     printf("PandM DM initialization done!\n");
@@ -779,6 +781,11 @@ CosaBackEndManagerRemove
     if (pMyObject->hLgiPlume)
     {
         CosaLgiPlumeRemove((ANSC_HANDLE)pMyObject->hLgiPlume);
+    }
+
+    if (pMyObject->hTemperatureStatus)
+    {
+        COSADmlRemoveTemperatureInfo((ANSC_HANDLE)pMyObject->hTemperatureStatus);
     }
 
     /* Remove self */
