b2c10dc8764a4a94f4fa08d7b2ff653f72226fe5
Hamza Ben Zekri <hbenzekri@libertyglobal.com>
2020-10-22 16:36:15 +0200

    Add support for X_LGI-COM_SON (ie Plume related) datamodel objects ( RDKB-4761 )
    Note that this is a partial implementation, the full functionality is not yet supported.
    
    Add support for X_LGI-COM_SON (ie Plume related) datamodel objects - Part 2 ( RDKB-4761 )
    
    Finish implementation for X_LGI-COM_SON and add HideSmartWifiSection (requirement MVXREQ-343)
    
    Restart plume agent on URL change
    
    Restart plume agent on URL change instead of just sending a notification through mesh-agent
    This is because when the plume URL fails to resolve the URL, it gets stuck trying to resolve it and doesn't process the notification
    
    only use rpcclient2 when running plume_init.sh etc for Puma6
    
    For Puma6, ccsp-p-and-m runs on the ARM and Plume runs on the Atom.
    For other platforms both run on the same CPU.

---
+++ source/TR-181/include/cosa_lgi_plume_apis.h
@@ -0,0 +1,91 @@
+/*********************************************************************
+ * Copyright 2019 ARRIS Enterprises, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************/
+
+#ifndef  _COSA_LGI_PLUME_APIS_H
+#define  _COSA_LGI_PLUME_APIS_H
+
+#include "../middle_layer_src/cosa_apis.h"
+
+/**********************************************************************
+                FUNCTION PROTOTYPES
+**********************************************************************/
+ANSC_STATUS
+CosaDmlGetPlumeUrl
+(
+    ANSC_HANDLE                 hContext,
+    char                        *pValue,
+    ULONG                       *pUlSize
+);
+
+ULONG
+CosaDmlSetPlumeUrl
+(
+    ANSC_HANDLE                 hContext,
+    char                        *pValue
+);
+
+BOOL
+CosaDmlGetPlumeAdminStatus
+(
+    ANSC_HANDLE                 hContext,
+    BOOL                        *pValue
+);
+
+BOOL
+CosaDmlSetPlumeAdminStatus
+(
+    ANSC_HANDLE                 hContext,
+    BOOL                        value
+);
+
+BOOL
+CosaDmlGetPlumeOperationalStatus
+(
+    ANSC_HANDLE                 hContext,
+    BOOL                        *pValue
+);
+
+BOOL
+CosaDmlSetPlumeOperationalStatus
+(
+    ANSC_HANDLE                 hContext,
+    BOOL                        value
+);
+BOOL
+CosaDmlGetPlumeDFSEnable
+(
+    ANSC_HANDLE                 hContext,
+    BOOL                        *pValue
+);
+BOOL
+CosaDmlSetPlumeDFSEnable
+(
+    ANSC_HANDLE                 hContext,
+    BOOL                        value
+);
+BOOL
+CosaDmlGetPlumeNativeAtmBsControl
+(
+    ANSC_HANDLE                 hContext,
+    BOOL                        *pValue
+);
+BOOL
+CosaDmlSetPlumeNativeAtmBsControl
+(
+    ANSC_HANDLE                 hContext,
+    BOOL                        value
+);
+#endif
