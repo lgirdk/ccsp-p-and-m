0ed641fc9a2bb3f0775a9424e36db414307ccbd5
Hamza Ben Zekri <hbenzekri@libertyglobal.com>
2020-10-22 16:36:15 +0200

    Add support for X_LGI-COM_SON (ie Plume related) datamodel objects ( RDKB-4761 )
    Note that this is a partial implementation, the full functionality is not yet supported.
    
    Add support for X_LGI-COM_SON (ie Plume related) datamodel objects - Part 2 ( RDKB-4761 )
    
    Finish implementation for X_LGI-COM_SON and add HideSmartWifiSection (requirement MVXREQ-343)
    
    Restart plume agent on URL change
    
    Restart plume agent on URL change instead of just sending a notification through mesh-agent
    This is because when the plume URL fails to resolve the URL, it gets stuck trying to resolve it and doesn't process the notification
    
    only use rpcclient2 when running plume_init.sh etc for Puma6
    
    For Puma6, ccsp-p-and-m runs on the ARM and Plume runs on the Atom.
    For other platforms both run on the same CPU.

--- source/TR-181/middle_layer_src/plugin_main_apis.c
+++ source/TR-181/middle_layer_src/plugin_main_apis.c
@@ -372,6 +372,8 @@ CosaBackEndManagerInitialize
 #endif
     pMyObject->hLgiCloudUi = (ANSC_HANDLE)CosaLgiCloudUiCreate();
     AnscTraceWarning(("  CosaLgiCloudUiCreate done !\n"));
+    pMyObject->hLgiPlume = (ANSC_HANDLE)CosaLgiPlumeCreate();
+    AnscTraceWarning(("  CosaArrisPlumeCreate done !\n"));
     pMyObject->hLgiGeneral = (ANSC_HANDLE)CosaLgiGeneralCreate();
     AnscTraceWarning(("  CosaLgiGeneralCreate done !\n"));
     pMyObject->hNeighdisc     = (ANSC_HANDLE)CosaNeighdiscCreate();
@@ -777,6 +779,11 @@ CosaBackEndManagerRemove
         CosaLgiWoLRemove((ANSC_HANDLE)pMyObject->hLgiWoL);
     }
     //LGI ADD END
+
+    if (pMyObject->hLgiPlume)
+    {
+        CosaLgiPlumeRemove((ANSC_HANDLE)pMyObject->hLgiPlume);
+    }
 
     /* Remove self */
     AnscFreeMemory((ANSC_HANDLE)pMyObject);
