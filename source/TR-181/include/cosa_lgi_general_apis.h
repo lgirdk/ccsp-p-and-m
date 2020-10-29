eed1b4f162c9eef8b954caf20bcdd53413b611a9
Sai Charan Sirpa <ssirpa.contractor@libertyglobal.com>
2020-10-29 14:57:48 +0100

    LG Datamodel parameter Porting - PAM ( RDKB-4877 )
    Implementation of below 3 parameters.
    
      1) Device.X_LGI-COM_General.MaxLoginAttempts
      2) Device.X_LGI-COM_General.LockoutPeriod
      3) Device.X_LGI-COM_General.MaxLockoutPeriods
    
    Testing Performed:
    
      1) Upgrade the modem with the latest firmware
      2) Able to do SPV and GPV on above parameters
    
    Signed-off-by: Sai Charan Sirpa <ssirpa.contractor@libertyglobal.com>

--- source/TR-181/include/cosa_lgi_general_apis.h
+++ source/TR-181/include/cosa_lgi_general_apis.h
@@ -139,4 +139,20 @@ CosaDmlGiSetCurrentLanguage
     char                        *pValue
 );
 
+ANSC_STATUS
+CosaDmlGiGetLoginSecurity
+(
+    ANSC_HANDLE                 hContext,
+    ULONG                       *pValue,
+    char                        *lValue
+);
+
+ULONG
+CosaDmlGiSetLoginSecurity
+(
+    ANSC_HANDLE                 hContext,
+    ULONG                       data,
+    char                        *lValue
+);
+
 #endif
