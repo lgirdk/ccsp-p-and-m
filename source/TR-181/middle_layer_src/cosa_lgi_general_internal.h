ac5c1b150e029d9430253d4f2ddc9c6f4427345d
Sai Charan Sirpa <ssirpa.contractor@libertyglobal.com>
2020-10-30 11:37:33 +0100

    add support for Device.X_LGI-COM_General.WebUISkin
    BUG DESCRIPTION:
      Implementation of WebUISkin Datamodel parameter.
    
    FIX DESCRIPTION:
      - Implemented the Device.X_LGI-COM_General.WebUISkin TR181 parameter.
      - Added the new parameter "WebUISkin" in the TR181-USGv2.XML
    
    TESTING PERFORMED:
      1. Bring up the modem.
      2. Verify the parameter can be queried using dmcli eRT getv Device.X_LGI-COM_General.WebUISkin.
      3. Set CustomerId to 20 through dmcli eRT setv Device.X_LGI-COM_General.CustomerId uint 20 .Device reboots after set query is successful.
      4. Verify the parameter value by querying it using dmcli eRT getv Device.X_LGI-COM_General.WebUISkin. Value is "ziggo".
    
    Signed-off-by: Sai Charan Sirpa <ssirpa.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_lgi_general_internal.h
+++ source/TR-181/middle_layer_src/cosa_lgi_general_internal.h
@@ -38,6 +38,7 @@ _COSA_DATAMODEL_LGI_GENERAL_CLASS_CONTENT
     BOOL                            TroubleshootWizardEnable;
     char                            DataModelVersion[32];
     BOOL                            FirstInstallWizardEnable;
+    char                            WebUISkin[16];
     ULONG                           max_failed_login_attempts;
     ULONG                           lockout_period;
     ULONG                           max_lockout_periods;
