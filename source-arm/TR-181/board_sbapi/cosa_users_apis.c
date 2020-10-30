6cb1cab9ffb474513b6e8a7e4a51649ead5d5848
Andre McCurdy <armccurdy@gmail.com>
2020-10-30 11:51:39 -0700

    force use of original code which doesn't rely on configparamgen
    Partially undo the changes introduced in:
    
      c33271f9 RDKB-28464: Remove default passwords from syscfg.db
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-arm/TR-181/board_sbapi/cosa_users_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_users_apis.c
@@ -570,7 +570,7 @@ user_validatepwd
 
    if(fromDB[0] == '\0')
    {
-     #if defined(_HUB4_PRODUCT_REQ_) || defined(_PLATFORM_RASPBERRYPI_) || defined(_PLATFORM_TURRIS_)
+     #if defined(_HUB4_PRODUCT_REQ_) || defined(_PLATFORM_RASPBERRYPI_) || defined(_PLATFORM_TURRIS_) || 1
          user_hashandsavepwd(hContext,pEntry->Password,pEntry);
      #else
          FILE *ptr;
@@ -717,7 +717,7 @@ CosaDmlUserResetPassword
    
    if(!strcmp(pEntry->Username,"admin"))
    {
-     #if defined(_HUB4_PRODUCT_REQ_) || defined(_PLATFORM_RASPBERRYPI_) || defined(_PLATFORM_TURRIS_)
+     #if defined(_HUB4_PRODUCT_REQ_) || defined(_PLATFORM_RASPBERRYPI_) || defined(_PLATFORM_TURRIS_) || 1
          //TODO: Avoid the hardcoded password.
          errno_t safec_rc = -1;
          safec_rc = strcpy_s(defPassword,sizeof(defPassword),"password");
