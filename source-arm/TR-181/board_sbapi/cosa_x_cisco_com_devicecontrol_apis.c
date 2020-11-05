05c73eccec27510e88cfe35353512165fed102ac
Andre McCurdy <armccurdy@gmail.com>
2020-11-05 00:13:03 -0800

    add support for _DISABLE_WIFI_HEALTH_STATS_TO_NVRAM_
    Allow the call to the wifivAPPercentage.sh script from within
    CosaDmlDcSaveWiFiHealthStatusintoNVRAM() to be disabled for targets
    which don't require it.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
@@ -4712,6 +4712,8 @@ void* set_mesh_disabled(void* arg)
 
 void CosaDmlDcSaveWiFiHealthStatusintoNVRAM( void  )
 {
+#if ! defined (_DISABLE_WIFI_HEALTH_STATS_TO_NVRAM_)
+
 	char acBoxType[ 16 ] = { 0 };
 
 	// Get BOX TYPE from device properties
@@ -4763,6 +4765,8 @@ void CosaDmlDcSaveWiFiHealthStatusintoNVRAM( void  )
 			v_secure_system( "sh /usr/ccsp/wifi/wifivAPPercentage.sh" );
 		}
 	}
+
+#endif
 }
 
 /* CheckAndGetDevicePropertiesEntry() */
