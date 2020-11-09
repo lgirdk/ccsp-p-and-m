efbcacbcaab703973596b41d7bc0f769b781a8a8
Sandipan Sinha <ssinha.contractor@libertyglobal.com>
2020-11-09 12:12:39 +0100

    fix Wi-Fi disabled when changing DUT from Router mode to bridge then back to Router ( RDKB-4895 )
    DESCRIPTION:
    - Wi-Fi is disabled when changing DUT from Router mode to bridge then
    back to Router.
    
    FIX DESCRIPTION:
    1.When modem changed from router to full bridge mode, it disabled WIFI
      radio and SSID in WIFI notifier thread successfully before reboot.
    2.When modem changed from full bridge to router mode, it just enabled
      WIFI radio successfully. But modem rebooted before it enabled SSID.
      So this issue occurred.
    3.A sync mechanism is added to make sure modem reboot after WIFI radio
      and SSID are set successfully.
    
    TESTING PERFORMED:
    1.Set modem to full bridge mode.
    2.WIFI radio and SSID are disabled after modem reboot.
    3.Set modem to router mode.
    4.Make sure WIFI radio and SSID(status is up and broadcasted) are enabled after modem reboot.
    
    Signed-off-by: Sandipan Sinha <ssinha.contractor@libertyglobal.com>

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
@@ -1536,14 +1536,12 @@ CosaDmlDcResetBr0(char *ip, char *sub) {
 	return ANSC_STATUS_SUCCESS;
 }
 
-#ifdef _XF3_PRODUCT_REQ_
 static int openCommonSyseventConnection() {
     if (commonSyseventFd == -1) {
         commonSyseventFd = s_sysevent_connect(&commonSyseventToken);
     }
     return 0;
 }
-#endif
 
 void* CosaDmlDcRestartRouter(void* arg)
 {
@@ -3874,6 +3872,9 @@ void* bridge_mode_wifi_notifier_thread(void* arg) {
     
     checkTicket(pNotify->ticket);
 
+    openCommonSyseventConnection();
+    sysevent_set(commonSyseventFd, commonSyseventToken, "wifi-notifier-done", "0" , 0);
+
 	/* 
 	  * Configure Bridge Static Mode Configuration 
 	  * if BridgeStaticMode then "Advanced Bridge" 2 then COSA_DML_LanMode_BridgeStatic
@@ -4113,6 +4114,9 @@ parameterValStruct_t valCommit1[] = {
 
         curticket++;
         AnscFreeMemory(arg);
+
+        sysevent_set(commonSyseventFd, commonSyseventToken, "wifi-notifier-done", "1" , 0);
+
         return NULL;
 }
 
