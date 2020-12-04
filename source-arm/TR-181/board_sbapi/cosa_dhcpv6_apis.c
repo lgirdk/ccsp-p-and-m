14b40f4a781ff3b1fd513474d8881db4662c0c71
Andre McCurdy <armccurdy@gmail.com>
2020-12-03 21:14:25 -0800

    dhcpv6 server: enforce strict compliance with rfc3315
    Add "drop-unicast" to dibbler server.conf
    
    From the dibbler user manual:
    
      In general, clients are supposed to send their messages to
      multicast, unless the server explicitly allows unicast. rfc3315
      says that packets sent to unicast should be dropped and the server
      must respond with status code set to UseMulticast. That's a bit
      harsh in author's opinion, so this behavior is not set by default.
      However, if you want strict RFC compliance, you can enable this by
      adding drop-unicast in your global scope.
    
    TESTING PERFORMED:
      1.In cdrouter setup, initiate a DHCPv6 Request from the client using the server's unicast address.
      2.Verify the that the server sends a Reply message with a status code of 5 'UseMulticast'
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
@@ -4209,6 +4209,9 @@ void __cosa_dhcpsv6_refresh_config()
     //Intel Proposed RDKB Generic Bug Fix from XB6 SDK
     fprintf(fp, "reconfigure-enabled 1\n");
 
+    //strict RFC compliance rfc3315 Section 13
+    fprintf(fp, "drop-unicast\n");
+
     if ( g_dhcpv6_server_type != DHCPV6_SERVER_TYPE_STATEFUL )
         fprintf(fp, "stateless\n");
 
@@ -5056,6 +5059,9 @@ void __cosa_dhcpsv6_refresh_config()
 
     //Intel Proposed RDKB Generic Bug Fix from XB6 SDK
     fprintf(fp, "reconfigure-enabled 1\n");
+
+    //strict RFC compliance rfc3315 Section 13
+    fprintf(fp, "drop-unicast\n");
 
     if ( g_dhcpv6_server_type != DHCPV6_SERVER_TYPE_STATEFUL )
         fprintf(fp, "stateless\n");
