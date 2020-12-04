296bcc0a01651d0304d8bac8d33b543b90493755
Andre McCurdy <armccurdy@gmail.com>
2020-12-03 19:55:04 -0800

    declaration of pf_cache etc in cosa_webconfig_api.h should be extern
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/middle_layer_src/cosa_webconfig_api.c
+++ source/TR-181/middle_layer_src/cosa_webconfig_api.c
@@ -28,6 +28,12 @@ t_cache pf_cache_bkup[PORTMAP_CACHE_SIZE];
 t_cache dmz_cache[DMZ_CACHE_SIZE];
 t_cache dmz_cache_bkup[DMZ_CACHE_SIZE];
 
+t_cache pf_cache[PORTMAP_CACHE_SIZE];
+t_cache pf_cache_bkup[PORTMAP_CACHE_SIZE];
+
+t_cache dmz_cache[DMZ_CACHE_SIZE];
+t_cache dmz_cache_bkup[DMZ_CACHE_SIZE];
+
 int pf_cache_size = 0;
 int pf_cache_size_bkup = 0;
 
