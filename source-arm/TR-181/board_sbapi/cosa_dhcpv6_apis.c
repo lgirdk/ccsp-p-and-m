b840abc6e3d42fdb2bffae9819b2927e986c964e
Tarun Saha <tsaha@libertyglobal.com>
2020-12-22 04:24:35 -0800

    fix invalid lifetimes for DHCPv6 stateful mode ( OFW-388 )
    DHCPv6 client on LAN does not receive the lifetime as acquired in the
    IA_PD on the WAN interface.

--- source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
@@ -5459,7 +5459,7 @@ void __cosa_dhcpsv6_refresh_config()
                         T1           = (sDhcpv6ServerPool[Index].Cfg.LeaseTime)/2;
                         T2           = (ULONG)((sDhcpv6ServerPool[Index].Cfg.LeaseTime)*80.0/100);
                         preferedTime = (sDhcpv6ServerPool[Index].Cfg.LeaseTime);
-                        validTime    = (sDhcpv6ServerPool[Index].Cfg.LeaseTime);
+                        validTime    = (sDhcpv6ServerPool[Index].Cfg.ValidLeaseTime);
                     }
 
                     fprintf(fp, "       T1 %lu\n", T1);
@@ -9161,7 +9161,6 @@ dhcpv6c_dbg_thrd(void * in)
                         commonSyseventSet(COSA_DML_DHCPV6C_PREF_VLDTM_SYSEVENT_NAME, iapd_vldtm);
 						}
 
-#if defined (MULTILAN_FEATURE)
                         ULONG idx = 0;
                         for ( idx=0; idx<uDhcpv6ServerPoolNum; idx++)
                         {
@@ -9170,8 +9169,9 @@ dhcpv6c_dbg_thrd(void * in)
                                   sDhcpv6ServerPool[idx].Cfg.LeaseTime = atoi(iapd_pretm);
                                   setpool_into_utopia((PUCHAR)DHCPV6S_NAME, (PUCHAR)"pool", idx, &sDhcpv6ServerPool[idx]);
                               }
+                              sDhcpv6ServerPool[idx].Cfg.ValidLeaseTime = atoi(iapd_vldtm);
                         }
-#endif
+
                         if(pString)
                         {
                             snprintf(objName, sizeof(objName)-1, "%sIPv6Prefix.", pString);
