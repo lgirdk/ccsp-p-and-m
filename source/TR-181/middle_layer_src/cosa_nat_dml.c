f53c12055e19628e27c4f4cd72b05f2db9c55584
Andre McCurdy <armccurdy@gmail.com>
2021-01-04 12:55:51 -0800

    fix initialisation of port mapping objects ( OFW-116 )
    1. Initialize the port mapping object with default values in PortMapping_AddEntry()
    2. Default value of Device.DHCPv4.Server.Pool.{i}.InternalClient is changed from "255.255.255.255" to "0.0.0.0" in macro NAT_PORTMAPPING_SET_DEFAULTVALUE()
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/middle_layer_src/cosa_nat_dml.c
+++ source/TR-181/middle_layer_src/cosa_nat_dml.c
@@ -1680,6 +1680,8 @@ PortMapping_AddEntry
         goto EXIT;
     }
 
+    NAT_PORTMAPPING_SET_DEFAULTVALUE(pNatPMapping);
+
     /* now we have this link content */
     pPMappingCxtLink->hContext = (ANSC_HANDLE)pNatPMapping;
     pPMappingCxtLink->bNew     = TRUE;
