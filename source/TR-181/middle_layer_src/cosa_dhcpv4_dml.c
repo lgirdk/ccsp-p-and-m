5b95836996a8cebfaf252bd4e5ac2998c1b13a0d
Anu Shaju Ainikkal <ashaju.contractor@libertyglobal.com>
2021-01-13 14:38:32 +0100

    add support for Device.DHCPv4.Server.Pool.{i}.X_LGI-COM_ClearLanAllowedSubnetTable ( OFW-161 )
    BUG DESCRIPTION:
      -Device.DHCPv4.Server.Pool.{i}.X_LGI-COM_ClearLanAllowedSubnetTable is not seen
    
    FIX DESCRIPTION:
      -Added implementation for X_LGI-COM_ClearLanAllowedSubnetTable.
    
    TESTING PERFORMED:
    
      a.After excecuting following dmcli commands , the table number 2,3 should be deleted from the tree.
        -dmcli eRT addtable Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.
        -dmcli eRT addtable Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.
        -dmcli eRT setv Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.2.LanAllowedSubnetIP string "192.168.0.0"
        -dmcli eRT setv Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.2.LanAllowedSubnetMask string  "255.255.255.0"
        -dmcli eRT setv Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.3.LanAllowedSubnetIP string "10.0.0.0"
        -dmcli eRT setv Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.3.LanAllowedSubnetMask string  "255.0.0.0"
        -dmcli eRT setv Device.DHCPv4.Server.Pool.1.X_LGI-COM_ClearLanAllowedSubnetTable bool true
    
      b.dmcli eRT deltable Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.1.
        should throw execution fail
    
      c.dmcli eRT addtable Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.
        dmcli eRT setv Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.4.LanAllowedSubnetIP string "192.168.0.5"
        dmcli eRT setv Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.4.LanAllowedSubnetMask string  "255.255.255.0"
        dmcli eRT deltable Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.4.
        should delete table number 4 from the tree
    
    Signed-off-by: Anu Shaju Ainikkal(ashaju.contractor@libertyglobal.com)

--- source/TR-181/middle_layer_src/cosa_dhcpv4_dml.c
+++ source/TR-181/middle_layer_src/cosa_dhcpv4_dml.c
@@ -5713,6 +5713,14 @@ Pool_GetParamBoolValue
         return TRUE;
     }
 
+    if (strcmp(ParamName, "X_LGI-COM_ClearLanAllowedSubnetTable") == 0)
+    {
+        /* collect value */
+        *pBool   =  FALSE;
+
+        return TRUE;
+    }
+
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
 }
@@ -6270,6 +6278,14 @@ Pool_SetParamBoolValue
         pPool->Cfg.DNSServersEnabled   = bValue;
 
         return TRUE;
+    }
+
+    if (strcmp(ParamName, "X_LGI-COM_ClearLanAllowedSubnetTable") == 0)
+    {
+        if (CosaDmlClearLanAllowedSubnetTable() == 0)
+        {
+            return TRUE;
+        }
     }
 
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
