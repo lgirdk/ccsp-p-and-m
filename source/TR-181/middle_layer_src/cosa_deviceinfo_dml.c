77505e860d74cd898bf81d839a2b510d4b7c2856
shashwat <shsrivastava.contractor@libertyglobal.com>
2021-01-18 18:51:11 +0100

    fix Device.Firewall. and Device.NAT. not present while going GPV for Device. ( OFW-160 )
    Bug Description:
    While doing GPV for Device. Parameters under Device.NAT. and Device.Firewall. were not visible, with individual query these parameters were returning successfully.
    
    Testing Performed:
    1.Set the DUT online.
    2.GPV on Device.
    3.Observing all the parameters under Device.NAT. and Device.Firewall.
    
    Signed-off-by: Shashwat <shsrivastava.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
@@ -18476,6 +18476,7 @@ HwHealthTestEnable_GetParamBoolValue
         }
 #else
         *pBool = FALSE;
+		return TRUE;
 #endif
     }
     return FALSE;
@@ -18606,10 +18607,10 @@ HwHealthTest_GetParamUlongValue
             //Value is not set. Hence using default value
             *puLong = DEFAULT_HWST_PTR_CPU_THRESHOLD;
         }
-        return TRUE;
 #else
         *puLong = 0;
 #endif
+        return TRUE;
     }
 
     if( AnscEqualString(ParamName, "dramThreshold", TRUE))
@@ -18621,10 +18622,10 @@ HwHealthTest_GetParamUlongValue
             //Value is not set. Hence using default value
             *puLong = DEFAULT_HWST_PTR_DRAM_THRESHOLD;
         }
-        return TRUE;
 #else
         *puLong = 0;
 #endif
+        return TRUE;
     }
     return FALSE;
 }
@@ -18791,10 +18792,10 @@ HwHealthTestPTREnable_GetParamBoolValue
 #ifdef COLUMBO_HWTEST
         PCOSA_DATAMODEL_DEVICEINFO      pMyObject = (PCOSA_DATAMODEL_DEVICEINFO)g_pCosaBEManager->hDeviceInfo;
         *pBool = pMyObject->HwHealtTestPTR.PTREnable;
-        return TRUE;
 #else
         *pBool = FALSE;
 #endif
+        return TRUE;
     }
     return FALSE;
 }
@@ -18979,10 +18980,10 @@ HwHealthTestPTRFrequency_GetParamUlongValue
             //Value is not set. Hence using default value
             *puLong = DEFAULT_HWST_PTR_FREQUENCY;
         }
-        return TRUE;
 #else
         *puLong = 0;
 #endif
+        return TRUE;
     }
     return FALSE;
 }
