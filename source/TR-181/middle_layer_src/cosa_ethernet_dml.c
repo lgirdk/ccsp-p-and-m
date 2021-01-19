8a56d2b95a7491b9a4ec64ea62e3325612cea30c
Arundhati Ghose <aghose.contractor@libertyglobal.com>
2021-01-19 10:31:18 +0100

    add support for Device.Ethernet.Interface.X.EEECapability / EEEEnable ( OFW-399 )
    Bug Description:
    Device.Ethernet.Interface.1.EEECapability and Device.Ethernet.Interface.1.EEEEnable is not supported for MNG image , same is working fine for Mv2 .
    
    Testing Performed:
    1.Bring the DUT online on ACS
    2.GPV on Device.Ethernet.Interface.1.EEECapability is successful
    3.GPV and SPV on Device.Ethernet.Interface.1.EEEEnable is successful
    4.Login to the device console.
    5.GPV on Device.Ethernet.Interface.1.EEECapability using dmcli is successful
    6.GPV and SPV on Device.Ethernet.Interface.1.EEEEnable using dmcli is successful
    
    Note: Interface Mapping(Device.Ethernet.Interface.{i}) is not done properly, so hal apis are implemented but not called.
    
    Signed-off-by: Arundhati <aghose.contractor@libertyglobal.com>
    
    fix missing ; in COSA_DML_ETH_PORT_CFG struct definition in cosa_ethernet_apis.h
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/middle_layer_src/cosa_ethernet_dml.c
+++ source/TR-181/middle_layer_src/cosa_ethernet_dml.c
@@ -491,6 +491,22 @@ Interface_GetParamBoolValue
         return TRUE;
     }
 
+    if( AnscEqualString(ParamName, "EEECapability", TRUE))
+    {
+        UINT PortIdx = getPortID(pEthernetPortFull->Cfg.InstanceNumber);
+        if (PortIdx > 0)
+        {
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    if( AnscEqualString(ParamName, "EEEEnable", TRUE))
+    {
+        /* collect value */
+        *pBool = pEthernetPortFull->Cfg.bEEEEnabled;
+        return TRUE;
+    }
 
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
@@ -839,6 +855,12 @@ Interface_SetParamBoolValue
         return TRUE;
     }
 
+    if( AnscEqualString(ParamName, "EEEEnable", TRUE))
+    {
+        /* Set value */
+        pEthernetPortFull->Cfg.bEEEEnabled = bValue;
+        return TRUE;
+    }
 
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
