79c2ea806642aaccbb10628c0f20c0cf9534c61c
Andre McCurdy <armccurdy@gmail.com>
2021-01-25 17:05:56 -0800

    check fgets() result against NULL, not character '\0' in cosa_x_cisco_com_mld_apis.c
    Plus replace hardcoded buffer size constants with sizeof().
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_mld_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_mld_apis.c
@@ -225,7 +225,7 @@ CosaDmlMldGetCfg
 
     if ( (pid_f = fopen("/var/run/ecmh.pid", "r")) != NULL )
     {
-        if(fgets(buffer, 255,pid_f) != '\0')
+        if(fgets(buffer, sizeof(buffer) ,pid_f) != NULL)
         {
             fclose(pid_f);
             pid_f = NULL;
@@ -283,7 +283,7 @@ CosaDmlMldSetCfg
 #if !defined (_COSA_BCM_MIPS_) && !defined(_ENABLE_DSL_SUPPORT_)
     if ( (pid_f = fopen("/var/run/ecmh.pid", "r")) != NULL )
     {
-        if(fgets(buffer, 255,pid_f) != '\0')
+        if(fgets(buffer, sizeof(buffer), pid_f) != NULL)
         {
             fclose(pid_f);
             pid_f = NULL;
@@ -341,7 +341,7 @@ CosaDmlMldGetInfo
 
     if ( (pid_f = fopen("/var/run/ecmh.pid", "r")) != NULL )
     {
-        if(fgets(buffer, 255,pid_f) != '\0')
+        if(fgets(buffer, sizeof(buffer), pid_f) != NULL)
         {
             fclose(pid_f);
             pid_f = NULL;
@@ -391,7 +391,7 @@ CosaDmlMldGetGroup
 
     if ( (pid_f = fopen("/var/run/ecmh.pid", "r")) != NULL )
     {
-        if(fgets(buffer, 255,pid_f) != '\0')
+        if(fgets(buffer, sizeof(buffer), pid_f) != NULL)
         {
             fclose(pid_f);
             pid_f = NULL;
@@ -419,9 +419,9 @@ CosaDmlMldGetGroup
 
     if ( (stat_f = fopen("/var/run/ecmh.dump", "r")) != NULL )
     {
-        fgets(buffer, 255, stat_f); /* Ignore the first line */
+        fgets(buffer, sizeof(buffer), stat_f); /* Ignore the first line */
         
-        while(fgets(buffer, 255,stat_f) != '\0')
+        while(fgets(buffer, sizeof(buffer), stat_f) != NULL)
         {
 			st = NULL;
             pch = strtok_r(buffer, " \t\r\n", &st);
