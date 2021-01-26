1c27a9de6ee243abfb0bd0f7778ad1c61a3dbedd
Andre McCurdy <armccurdy@gmail.com>
2021-01-25 16:03:32 -0800

    fix missing pointer dereference in CosaDmlDiGetFirmwareBuildTime()
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
@@ -880,7 +880,7 @@ CosaDmlDiGetFirmwareBuildTime
 		if (value_token != NULL) 
 		{
 			value_token = strtok_r(NULL, "\"", &st); 
-			snprintf(pValue, pulSize, "%s", value_token);
+			snprintf(pValue, *pulSize, "%s", value_token);
 			*pulSize = AnscSizeOfString(pValue);
 		
 			fclose(fp);
