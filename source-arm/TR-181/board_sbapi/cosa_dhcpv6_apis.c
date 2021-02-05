864714b53057d5b949578dd64f746dc5f2803b41
Tarun Saha <tsaha@libertyglobal.com>
2021-02-05 13:44:40 -0800

    Changes required to support MULTILAN_FEATURE

--- source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
@@ -7804,6 +7804,7 @@ void CosaDmlDhcpv6sRebootServer()
         return;
 #if defined (MULTILAN_FEATURE)
     commonSyseventSet("dhcpv6s-restart", "");
+    commonSyseventSet("zebra-restart", "");
 #else
     FILE *fp = NULL;
     int fd = 0;
@@ -9204,15 +9205,22 @@ dhcpv6c_dbg_thrd(void * in)
                         g_COSARepopulateTable(g_pDslhDmlAgent, "Device.DHCPv6.Server.Pool.1.Option.");
 
 #if defined (MULTILAN_FEATURE)
+                        /* we need save this for zebra to send RA 
+                           ipv6_prefix           // xx:xx::/yy
+                         */
+                        v_secure_system("sysevent set ipv6_prefix %s ",v6pref);
+
                         g_dhcpv6_server_prefix_ready = TRUE;
 
                         if ((v6addr_prev[0] == '\0') || ( _ansc_strcmp(v6addr_prev, v6pref ) !=0))
                         {
                             _ansc_strncpy( v6addr_prev, v6pref, sizeof(v6pref));
-                            commonSyseventSet("ipv6-restart", "1");
+			    /* In case of change in prefix, sysevent set on 'ipv6_prefix' will always trigger ipv6-restart */
+                            //commonSyseventSet("ipv6-restart", "1");
                         }
                         else
                         {
+                            commonSyseventSet("ipv6_addr-unset", "");
                             commonSyseventSet("ipv6_addr-set", "");
                         }
 #endif
@@ -9228,10 +9236,7 @@ dhcpv6c_dbg_thrd(void * in)
 			#ifdef _COSA_INTEL_XB3_ARM_
                         v_secure_system("ip -6 route add %s dev %s table erouter", v6pref, COSA_DML_DHCPV6_SERVER_IFNAME);
 			#endif
-                        /* we need save this for zebra to send RA 
-                           ipv6_prefix           // xx:xx::/yy
-                         */
-                        v_secure_system("sysevent set ipv6_prefix %s ",v6pref);
+
 #if defined(FEATURE_RDKB_WAN_MANAGER)
                         // Disabled sending ipv6 info to wan manager for comcast platforms
                         // Later we should enable if needed.
@@ -9276,6 +9281,7 @@ dhcpv6c_dbg_thrd(void * in)
                         }
 #endif
 
+#ifndef _PUMA6_ARM_
                         CosaDmlDHCPv6sTriggerRestart(FALSE);
 #if defined(_COSA_BCM_ARM_) || defined(INTEL_PUMA7)
                         CcspTraceWarning((" %s dhcpv6_assign_global_ip to brlan0 \n", __FUNCTION__));
@@ -9332,6 +9338,7 @@ dhcpv6c_dbg_thrd(void * in)
 			    CcspTraceWarning(("%s: setting lan-restart\n", __FUNCTION__));
                             commonSyseventSet("lan-restart", "1");
                         }
+#endif //_PUMA6_ARM_
 #endif
 #else
 #if defined(FEATURE_RDKB_WAN_MANAGER)
