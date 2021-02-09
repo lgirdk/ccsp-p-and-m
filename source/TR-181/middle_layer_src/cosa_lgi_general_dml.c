ef12b8c8ceaf85d29ae5aac74c4c1ed3554e3aeb
skizhakkayil <skizhakkayil.contractor@libertyglobal.com>
2021-02-09 12:43:08 +0000

    UserBridgeModeAllowed should be supported to disable Modem Mode Page in UI ( OFW-571 )
    DESCRIPTION:-
    Parameter needs to be implemented to support Disabling Modem Mode page
    
    Fix:
    Implementation done for the parameter Device.X_LGI-COM_General.UserBridgeModeAllowed
    
    TESTING PERFORMED:-
    
    Verified GPV and SPV for Device.X_LGI-COM_General.UserBridgeModeAllowed using dmcli and ACS
    
    Default Value :  True
    
    Signed-off-by: Sandhya Kizhakkayil <skizhakkayil.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_lgi_general_dml.c
+++ source/TR-181/middle_layer_src/cosa_lgi_general_dml.c
@@ -119,6 +119,11 @@ LgiGeneral_GetParamBoolValue
         CosaDmlGiGetTroubleshootWizardEnable(NULL, pBool);
         return TRUE;
     }
+    if (strcmp(ParamName, "UserBridgeModeAllowed") == 0)
+    {
+        CosaDmlGiGetUserBridgeModeAllowed(NULL, pBool);
+        return TRUE;
+    }
     // LGI ADD END
 
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
@@ -351,6 +356,11 @@ LgiGeneral_SetParamBoolValue
         pMyObject->TroubleshootWizardEnable = bValue;
         return TRUE;
     }
+    if (strcmp(ParamName, "UserBridgeModeAllowed") == 0)
+    {
+        pMyObject->UserBridgeModeAllowed = bValue;
+        return TRUE;
+    }
     return FALSE;
 }
 
@@ -483,6 +493,7 @@ LgiGeneral_Commit
     CosaDmlGiSetLoginSecurity(NULL, pMyObject->max_failed_login_attempts, "max_failed_login_attempts");
     CosaDmlGiSetLoginSecurity(NULL, pMyObject->lockout_period, "lockout_period");
     CosaDmlGiSetLoginSecurity(NULL, pMyObject->max_lockout_periods, "max_lockout_periods");
+    CosaDmlGiSetUserBridgeModeAllowed(NULL, pMyObject->UserBridgeModeAllowed);
     CosaDmlGiSaveSettings();
     return 0;
 }
