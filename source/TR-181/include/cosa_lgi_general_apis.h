3fb3ca5fed9bc7c3f7fd6cc24b42fffd63c3f220
skizhakkayil <skizhakkayil.contractor@libertyglobal.com>
2021-02-09 12:43:08 +0000

    UserBridgeModeAllowed should be supported to disable Modem Mode Page in UI ( OFW-571 )
    DESCRIPTION:-
    Parameter needs to be implemented to support Disabling Modem Mode page
    
    Fix:
    Implementation done for the parameter Device.X_LGI-COM_General.UserBridgeModeAllowed
    
    TESTING PERFORMED:-
    
    Verified GPV and SPV for Device.X_LGI-COM_General.UserBridgeModeAllowed using dmcli and ACS
    
    Default Value :  True
    
    Signed-off-by: Sandhya Kizhakkayil <skizhakkayil.contractor@libertyglobal.com>

--- source/TR-181/include/cosa_lgi_general_apis.h
+++ source/TR-181/include/cosa_lgi_general_apis.h
@@ -179,4 +179,19 @@ CosaDmlGiGetDefaultAdminPassword
     char                        *pValue,
     ULONG                       *pUlSize
 );
+
+ULONG
+CosaDmlGiGetUserBridgeModeAllowed
+(
+    ANSC_HANDLE                 hContext,
+    BOOL                        *pValue
+);
+
+ULONG
+CosaDmlGiSetUserBridgeModeAllowed
+(
+    ANSC_HANDLE                 hContext,
+    BOOL                        bValue
+);
+
 #endif
