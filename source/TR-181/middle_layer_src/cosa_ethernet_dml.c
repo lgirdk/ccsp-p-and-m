646cbc106adcf9eb046e7bf18405902007c24bd6
Andre McCurdy <armccurdy@gmail.com>
2021-02-17 20:59:50 -0800

    fix handling of EEECapability in Interface_GetParamBoolValue()
    Fix missing stats related parameters under Device.Ethernet.Interface.5. tree ( OFW-634 )
    
    If Interface_GetParamBoolValue() returns FALSE for
    "Device.Ethernet.Interface.5." and "Device.Ethernet.Interface.6."
    EEECapability, further calls in the object tree are skipped and
    Stats_GetParamUlongValue() does not get called at all for object tree
    "Device.Ethernet.Interface.5." and "Device.Ethernet.Interface.6."
    
    Test done: Verified that GPV of "Device.Ethernet.Interface.5" has
    .Stats returned.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/middle_layer_src/cosa_ethernet_dml.c
+++ source/TR-181/middle_layer_src/cosa_ethernet_dml.c
@@ -496,9 +496,13 @@ Interface_GetParamBoolValue
         UINT PortIdx = getPortID(pEthernetPortFull->Cfg.InstanceNumber);
         if (PortIdx > 0)
         {
-            return TRUE;
+            *pBool = TRUE;
         }
-        return FALSE;
+        else
+        {
+            *pBool = FALSE;
+        }
+        return TRUE;
     }
 
     if( AnscEqualString(ParamName, "EEEEnable", TRUE))
