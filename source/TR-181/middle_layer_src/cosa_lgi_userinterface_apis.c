9ade9aa1e706ab2ba76da91f3a5378b60b924665
Ajay Vishnu Bhat <abhat@libertyglobal.com>
2021-03-03 20:35:31 +0100

    changes to the start_lighttpd, set/unset the mgmt_wan_access and firewall rules to handle remote access settings for CSR login ( OFW-238 OFW-761 )
    FIX DESCRIPTION:
    The lighttpd was not bound to any interface and was listening on
    all interfaces. lighttpd.conf is now being regenerated from
    start_lighttpd.sh script and the firewall rule changes are also
    done.
    
    DM implementation for RemoteAccess object is in now in place and
    corresponding changes for TR069 is also in place.
    
    TESTING PERFORMED:
    1. Ensure the DUT is online.
    2. Try to access the DUT UI on wan0 IP.
    3. The UI should not be accessible.
    4. Set Device.UserInterface.RemoteAccess.Enable parameter to true.
    5. DUT UI should be accessible over wan0 IP.
    
    Signed-off-by: Ajay Vishnu Bhat <abhat@libertyglobal.com>
    
    reverted changes to start_lighttpd script to bind to all interfaces and handle wan0 traffic with iptable rules ( OFW-822 )
    
    FIX DESCRIPTION:
    The changes to lighttpd.conf needs to be considered if necessary.
    
    TESTING PERFORMED:
    1. Set the DUT to bridge mode.
    2. DUT UI should be accessible over 192.168.100.1
    
    Signed-off-by: Ajay Vishnu Bhat <abhat@libertyglobal.com>
    
    fix default values of Device.UserInterface.RemoteAccess. ( OFW-1365 )
    
    Fix Description: Modified those parameters as non writable and changed defaults as per Req
    
    Signed-off-by: Sarveswaran Nagarajan <sanagarajan.contractor@libertyglobal.com>
    
    fix default values of Device.UserInterface.RemoteAccess. ( OFW-1692 )
    
    DESCRIPTION:
    Device.UserInterface.RemoteAccess. shows Port as 443 and Supported
    Protocols as HTTPS but the UI is accessible on port 80 with HTTP.
    
    Expected default values for Device.UserInterface.RemoteAccess. are:
    
      Port                  readOnly, 80
      Supported Protocols   readOnly, HTTP
      Protocol              readOnly, HTTP
    
    TESTING PERFORMED:-
    Checked with GPV of Device.UserInterface.RemoteAccess. through ACS and the terminal.

---
+++ source/TR-181/middle_layer_src/cosa_lgi_userinterface_apis.c
@@ -0,0 +1,124 @@
+/*********************************************************************
+ * Copyright 2017-2019 ARRIS Enterprises, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************/
+
+
+#include "cosa_userinterface_apis.h"
+#include "cosa_lgi_userinterface_apis.h"
+#include <syscfg/syscfg.h>
+
+/**************************************************************************
+
+    module: cosa_lgi_userinterface_apis.c
+
+        For COSA Data Model Library Development
+
+    -------------------------------------------------------------------
+
+    description:
+
+        This file implements back-end apis for the COSA Data Model Library
+
+        *  CosaDmlStdRaInit
+        *  CosaDmlStdRaSetCfg
+        *  CosaDmlStdRaGetCfg
+    -------------------------------------------------------------------
+
+**************************************************************************/
+
+ANSC_STATUS
+CosaDmlStdRaInit
+    (
+        ANSC_HANDLE                 hDml,
+        PANSC_HANDLE                phContext
+    )
+{
+    return ANSC_STATUS_SUCCESS;
+}
+
+ANSC_STATUS
+CosaDmlStdRaSetCfg
+    (
+        ANSC_HANDLE                 hContext,
+        PCOSA_DML_STD_RA_CFG        pCfg
+    )
+{
+    const char* gui_session;
+    gui_session = "/var/tmp/gui/";
+    struct stat stats;
+
+    /* HTTP/HTTPS enable */
+    if (pCfg->bEnabled)
+    {
+       /* enable WAN access */
+       if (syscfg_set(NULL, "mgmt_wan_access", "1") != 0)
+       {
+           fprintf(stderr, "Failed to enable WAN access\n");
+           return ANSC_STATUS_FAILURE;
+       }
+    }
+    else
+    {
+       /* enable WAN access */
+       if (syscfg_set(NULL, "mgmt_wan_access", "0") != 0)
+       {
+           fprintf(stderr, "Failed to enable WAN access\n");
+           return ANSC_STATUS_FAILURE;
+       }
+    }
+
+    /* remove the GUI session file when Enabling or Disabling Remote Access*/
+    if (stat(gui_session, &stats) == 0 && S_ISDIR(stats.st_mode)) {
+        system("rm -rf /var/tmp/gui/session_*");
+    }
+
+    /* commit the change to persistent storage */
+    syscfg_commit();
+
+    /* Restarting Firewall */
+    if (system("sysevent set firewall-restart") != 0)
+    {
+        fprintf(stderr, "%s: fail to restart firewall\n", __FUNCTION__);
+        return ANSC_STATUS_FAILURE;
+    }
+
+    /* Restarting Lighttpd */
+    system("/bin/sh /etc/start_lighttpd.sh restart");
+
+    fprintf(stderr, "%s: OK !!!!\n", __FUNCTION__);
+    return ANSC_STATUS_SUCCESS;
+}
+
+ANSC_STATUS
+CosaDmlStdRaGetCfg
+    (
+        ANSC_HANDLE                 hContext,
+        PCOSA_DML_STD_RA_CFG        pCfg
+    )
+{
+    /* Enable flag - default to FALSE */
+    pCfg->bEnabled = FALSE;
+
+    /* Supported Protocols */
+    snprintf(pCfg->SupportedProtocols, sizeof(pCfg->SupportedProtocols), "HTTP");
+
+    /* Port - default to HTTPS */
+    pCfg->Port = HTTP_INTERNAL_PORT;
+
+    /* Protocol - default to HTTPS */
+    snprintf(pCfg->Protocol, sizeof(pCfg->Protocol), "HTTP");
+
+    return ANSC_STATUS_SUCCESS;
+}
