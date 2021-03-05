e37ea852e787d3fbc3468d2b8fbb4c40490834ba
Andre McCurdy <armccurdy@gmail.com>
2021-03-05 15:17:37 -0800

    drop unnecessary zeroing of buffers passed to _get_shell_output() etc
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
@@ -2424,7 +2424,7 @@ static int _dibbler_client_operation(char * arg)
 {
 #if defined(INTEL_PUMA7) || defined(_COSA_BCM_ARM_)
     FILE *fp = NULL;
-    char out[256] = {0};
+    char out[256];
 #endif
 #if defined (INTEL_PUMA7)
     int watchdog = NO_OF_RETRY;
@@ -6199,7 +6199,7 @@ CosaDmlDhcpv6sGetPoolInfo
 {
     UNREFERENCED_PARAMETER(hContext);
     FILE *fp = NULL;
-    char out[256] = {0};
+    char out[256];
 
     ULONG                           Index = 0;
 
@@ -7133,7 +7133,7 @@ int dhcpv6_assign_global_ip(char * prefix, char * intfName, char * ipAddr)
     unsigned int    i                = 0;
     unsigned int    j                = 0;
     unsigned int    k                = 0;
-    char            out[256]         = {0};
+    char            out[256];
     char            tmp[8]           = {0};
     FILE *fp = NULL;
     errno_t         rc               = -1;
@@ -7302,7 +7302,7 @@ void CosaDmlDhcpv6sRebootServer()
     if (fd < 0) {
 #endif
         BOOL isBridgeMode = FALSE;
-        char out[128] = {0};
+        char out[128];
 
 /* dibbler-server process start fix for HUB4 and ADA */
 #if defined (_HUB4_PRODUCT_REQ_)
@@ -7552,10 +7552,12 @@ void enable_IPv6(char* if_name)
 
     	CcspTraceInfo(("%s : Enabling ipv6 on iface %s\n",__FUNCTION__,if_name));
 
-    	char tbuff[100] = {0} , ipv6_addr[128] = {0} , cmd[128] = {0} ;
+    	char tbuff[100], ipv6_addr[128] = {0} , cmd[128] = {0} ;
     	errno_t rc = -1;
-	    fp = v_secure_popen("r","sysctl net.ipv6.conf.%s.autoconf",if_name);
-	    _get_shell_output(fp, tbuff, sizeof(tbuff));
+
+	fp = v_secure_popen("r","sysctl net.ipv6.conf.%s.autoconf",if_name);
+	_get_shell_output(fp, tbuff, sizeof(tbuff));
+
     	if(tbuff[strlen(tbuff)-1] == '0')
     	{
             v_secure_system("sysctl -w net.ipv6.conf.%s.autoconf=1",if_name);
@@ -7825,7 +7827,6 @@ static void *InterfaceEventHandler_thrd(void *data)
         retPsmGet = PSM_Get_Record_Value2(bus_handle,g_Subsystem, "dmsb.l2net.2.Port.1.Name", NULL, &Inf_name);
         if (retPsmGet == CCSP_SUCCESS)
         {
-            memset(tbuff,0,sizeof(tbuff));
             fp = v_secure_popen("r","sysctl net.ipv6.conf.%s.autoconf",Inf_name);
             _get_shell_output(fp, tbuff, sizeof(tbuff));
             if(tbuff[strlen(tbuff)-1] == '0')
@@ -7848,8 +7849,6 @@ static void *InterfaceEventHandler_thrd(void *data)
 
     if(strcmp((const char*)buf, "ready") == 0)
     {
-
-        memset(tbuff,0,sizeof(tbuff));
         fp = v_secure_popen("r","sysctl net.ipv6.conf.br106.autoconf");
         _get_shell_output(fp, tbuff, sizeof(tbuff));
         if(tbuff[strlen(tbuff)-1] == '0')
@@ -8763,7 +8762,6 @@ dhcpv6c_dbg_thrd(void * in)
                             
 			if(pref_len < 64)
 			{
-			    memset(out,0,sizeof(out));
 			    memset(out1,0,sizeof(out1));
                             fp = v_secure_popen("r","syscfg get IPv6subPrefix");
                             _get_shell_output(fp, out, sizeof(out));
@@ -8771,7 +8769,6 @@ dhcpv6c_dbg_thrd(void * in)
 				{
                                 static int first = 0;
 
-				memset(out,0,sizeof(out));
                                 fp = v_secure_popen("r","syscfg get IPv6_Interface");
                                 _get_shell_output(fp, out, sizeof(out));
 				pt = out;
