d6a5127cc32a5fb1f79459cd190d25a034b65e32
sarveswaran.n <sanagarajan.contractor@libertyglobal.com>
2021-03-10 16:45:51 +0100

    fix LAN ip is not changing to Default after removing the entry from LanAllowedSubnetIP table ( OFW-16 )
    Fix Description : Added the logic to switch back to default LAN settings in case of AllowedSubnet table deleted
    
    Testing Performed:
    
      1) Added the following values on the AllowedSubnet table:
    
         dmcli eRT setv Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.2.LanAllowedSubnetIP string "10.0.0.0"
         dmcli eRT setv Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.2.LanAllowedSubnetMask string  "255.0.0.0"
         dmcli eRT setv Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.3.LanAllowedSubnetIP string "172.16.0.0"
         dmcli eRT setv Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.3.LanAllowedSubnetMask string  "255.240.0.0"
    
      2) Add LANIp , Max & Min address within the LAN Allowed subnet Range
      3) Delete the LANAllowed table entries
      4) Reboot the DUT
      5) Ensure LAN settings switched in to Default LAN values
    
    Signed-off-by: sarveswaran.n <sanagarajan.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_dhcpv4_dml.c
+++ source/TR-181/middle_layer_src/cosa_dhcpv4_dml.c
@@ -10888,6 +10888,7 @@ LanAllowedSubnetTable_DelEntry
         CosaDmlLAN_Allowed_Subnet_DelEntry(pLinkObj->InstanceNumber);
     }
 
+    CosaDmlLAN_Validate_ModifyLanIP(pLanAllowedSubnet, pLinkObj->InstanceNumber);
     AnscFreeMemory(pLanAllowedSubnet);
     AnscFreeMemory(pLinkObj);
     return ANSC_STATUS_SUCCESS;
