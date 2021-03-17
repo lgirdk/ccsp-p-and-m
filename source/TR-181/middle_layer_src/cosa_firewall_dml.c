390e5b83313048ceb209776d8a84baba53a67867
Andy Carvalho <acarvalho.contractor@libertyglobal.com>
2021-03-17 15:27:32 +0100

    add support for Device.Firewall.X_RDKCENTRAL-COM_Security.V4.ICMPFloodDetect etc ( OFW-529 )
    BUG DESCRIPTION: V4_ICMPFloodDetect is not supported
    
    FIX DESCRIPTION:
        - Implemented TR181 parameters.
    	i) Device.Firewall.X_RDKCENTRAL-COM_Security.V4.ICMPFloodDetect
            ii) Device.Firewall.X_RDKCENTRAL-COM_Security.V4.ICMPFloodDetectRate
        - Added new parameters "ICMPFloodDetect" & "ICMPFloodDetectRate" in the TR181-USGv2.XML
    
    TESTING PERFORMED:
        1. Bring up the modem.
        2. Verify the parameters can be queried using dmcli.
        3. Set "Device.Firewall.X_RDKCENTRAL-COM_Security.V4.ICMPFloodDetect" to true, dmcli eRT setv Device.Firewall.X_RDKCENTRAL-COM_Security.V4.ICMPFloodDetect bool true
        4. Set "Device.Firewall.X_RDKCENTRAL-COM_Security.V4.ICMPFloodDetectRate", dmcli eRT setv Device.Firewall.X_RDKCENTRAL-COM_Security.V4.ICMPFloodDetectRate uint 20
        5. Verify the parameter values by querying it.

--- source/TR-181/middle_layer_src/cosa_firewall_dml.c
+++ source/TR-181/middle_layer_src/cosa_firewall_dml.c
@@ -738,6 +738,11 @@ V4_GetParamBoolValue
       CosaDmlGatewayV4GetIPFloodDetect(pBool);
       return TRUE;
     }
+    else if (AnscEqualString(ParamName, "ICMPFloodDetect", TRUE)) {
+
+      CosaDmlGatewayV4GetICMPFloodDetect(pBool);
+      return TRUE;
+    }
     else if (AnscEqualString(ParamName, "Enable", TRUE)) {
 
       CosaDmlGatewayV4GetFwEnable(pBool);
@@ -801,6 +806,11 @@ V4_SetParamBoolValue
       CosaDmlGatewayV4SetIPFloodDetect(bValue);
       return TRUE;
     }
+    else if (AnscEqualString(ParamName, "ICMPFloodDetect", TRUE)) {
+
+      CosaDmlGatewayV4SetICMPFloodDetect(bValue);
+      return TRUE;
+    }
     else if (AnscEqualString(ParamName, "Enable", TRUE)) {
 
       CosaDmlGatewayV4SetFwEnable(bValue);
@@ -820,6 +830,14 @@ BOOL V4_GetParamUlongValue ( ANSC_HANDLE hInsContext, char* ParamName, ULONG* pu
         }
     }
 
+    if (AnscEqualString(ParamName, "ICMPFloodDetectRate", TRUE))
+    {
+        if(ANSC_STATUS_SUCCESS == CosaDmlGatewayV4GetICMPFloodDetectRate(puLong))
+        {
+            return TRUE;
+        }
+    }
+
     return FALSE;
 }
 
@@ -835,6 +853,14 @@ BOOL V4_SetParamUlongValue ( ANSC_HANDLE hInsContext, char* ParamName, ULONG ulV
         }
 
         pCosaDMFirewall->V4DayOfWeekBlockTimeBitMaskType = ulValue;
+    }
+
+    if (AnscEqualString(ParamName, "ICMPFloodDetectRate", TRUE))
+    {
+        if(ANSC_STATUS_SUCCESS != CosaDmlGatewayV4SetICMPFloodDetectRate(ulValue))
+        {
+            return FALSE;
+        }
     }
 
     return TRUE;
