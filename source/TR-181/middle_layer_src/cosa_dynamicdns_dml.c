b9afbb12ff395a680bec62085f03ed7490547777
Siddalingesh <sihiregoudar@libertyglobal.com>
2021-03-22 12:13:54 +0100

    update Device.DynamicDNS.Server.[i].Enable type from bool to string ( OFW-831 )
    [Issue]
    The below dm Parameter is bool type in stack but the ACS expects it
    to be string:
    
      Device.DynamicDNS.Server.1.Enable
    
    [Solution]
    Change the Type of parameter to string.
    
    Signed-off-by: Sidda(sihiregoudar@libertyglobal.com)

--- source/TR-181/middle_layer_src/cosa_dynamicdns_dml.c
+++ source/TR-181/middle_layer_src/cosa_dynamicdns_dml.c
@@ -1020,10 +1020,8 @@ DDNSHostname_Rollback
     *  DDNSServer_GetEntry
     *  DDNSServer_AddEntry
     *  DDNSServer_DelEntry
-    *  DDNSServer_GetParamBoolValue
     *  DDNSServer_GetParamUlongValue
     *  DDNSServer_GetParamStringValue
-    *  DDNSServer_SetParamBoolValue
     *  DDNSServer_SetParamStringValue
     *  DDNSServer_Validate
     *  DDNSServer_Commit
@@ -1170,35 +1168,6 @@ DDNSServer_DelEntry
 }
 
 BOOL
-DDNSServer_GetParamBoolValue
-    (
-        ANSC_HANDLE                 hInsContext,
-        char*                       ParamName,
-        BOOL*                       pBool
-    )
-{
-#if defined(DDNS_BROADBANDFORUM)
-    PCOSA_CONTEXT_LINK_OBJECT     pLinkObj     = (PCOSA_CONTEXT_LINK_OBJECT)hInsContext;
-    COSA_DML_DDNS_SERVER          *pDDNSServer = (COSA_DML_DDNS_SERVER*)pLinkObj->hContext;
-
-    CosaDmlDynamicDns_Server_GetConf(pDDNSServer->InstanceNumber,pDDNSServer);
-
-    if (AnscEqualString(ParamName, "Enable", TRUE))
-    {
-        *pBool = pDDNSServer->Enable;
-        return TRUE;
-    }
-    return FALSE;
-#endif
-#if !defined(DDNS_BROADBANDFORUM)
-    UNREFERENCED_PARAMETER(hInsContext);
-    UNREFERENCED_PARAMETER(ParamName);
-    UNREFERENCED_PARAMETER(pBool);
-    return TRUE;
-#endif
-}
-
-BOOL
 DDNSServer_GetParamUlongValue
     (
         ANSC_HANDLE                 hInsContext,
@@ -1211,6 +1180,13 @@ DDNSServer_GetParamUlongValue
     PCOSA_CONTEXT_LINK_OBJECT    pLinkObj      = (PCOSA_CONTEXT_LINK_OBJECT)hInsContext;
     COSA_DML_DDNS_SERVER         *pServerEntry = (COSA_DML_DDNS_SERVER *)pLinkObj->hContext;
 
+    /* check the parameter name and return the corresponding value */
+    if( AnscEqualString(ParamName, "Enable", TRUE))
+    {
+        /* collect value */
+        *puLong = (pServerEntry->Enable) ? 1 : 0;
+        return TRUE;
+    }
     /* check the parameter name and return the corresponding value */
     if( AnscEqualString(ParamName, "CheckInterval", TRUE))
     {
@@ -1296,33 +1272,6 @@ DDNSServer_GetParamStringValue
 }
 
 BOOL
-DDNSServer_SetParamBoolValue
-    (
-        ANSC_HANDLE                 hInsContext,
-        char*                       ParamName,
-        BOOL                        pBool
-    )
-{
-#if defined(DDNS_BROADBANDFORUM)
-    PCOSA_CONTEXT_LINK_OBJECT       pLinkObj    = (PCOSA_CONTEXT_LINK_OBJECT)hInsContext;
-    COSA_DML_DDNS_SERVER          *pDDNSServer  = (COSA_DML_DDNS_SERVER*)pLinkObj->hContext;
-
-    if (AnscEqualString(ParamName, "Enable", TRUE))
-    {
-        pDDNSServer->Enable = pBool;
-        return TRUE;
-    }
-    return FALSE;
-#endif
-#if !defined(DDNS_BROADBANDFORUM)
-    UNREFERENCED_PARAMETER(hInsContext);
-    UNREFERENCED_PARAMETER(ParamName);
-    UNREFERENCED_PARAMETER(pBool);
-    return TRUE;
-#endif
-}
-
-BOOL
 DDNSServer_SetParamStringValue
     (
         ANSC_HANDLE                 hInsContext,
@@ -1381,6 +1330,11 @@ DDNSServer_SetParamUlongValue
     PCOSA_CONTEXT_LINK_OBJECT    pLinkObj      = (PCOSA_CONTEXT_LINK_OBJECT)hInsContext;
     COSA_DML_DDNS_SERVER         *pServerEntry = (COSA_DML_DDNS_SERVER *)pLinkObj->hContext;
 
+    if( AnscEqualString(ParamName, "Enable", TRUE))
+    {
+        pServerEntry->Enable = (uValue) ? TRUE : FALSE;
+        return TRUE;
+    }
     if( AnscEqualString(ParamName, "CheckInterval", TRUE))
     {
         pServerEntry->CheckInterval = uValue;
