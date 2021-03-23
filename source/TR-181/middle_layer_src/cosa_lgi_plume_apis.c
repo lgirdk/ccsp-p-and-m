6579706fc8247ad28e02fe47e066e630f2ff79cc
Karthik Shanbhag <kshanbhag@libertyglobal.com>
2021-03-23 14:24:28 +0100

    add support for Device.X_LGI-COM_SON.SONLogpullEnable ( OWF-773 )
    [Description]
    Device.X_LGI-COM_SON.SONLogpullEnable parameter is not available
    This is new datamodel to enable or disable the log pull functionality
    If the datamodel is set to true, log should be uploaded to server.
    If set to false, logupload should not happen.
    
    [Test Description]:
    On Factory reset, Device.X_LGI-COM_SON.SONLogpullEnable should be false.
    If log-upload is called from the server, log-pull should not happen.
    Set Device.X_LGI-COM_SON.SONLogpullEnable to true, do log pull from the server. Logs should be uploaded to server.
    
    Signed-off-by: Karthik Shanbhag(kshanbhag@libertyglobal.com)

--- source/TR-181/middle_layer_src/cosa_lgi_plume_apis.c
+++ source/TR-181/middle_layer_src/cosa_lgi_plume_apis.c
@@ -208,3 +208,24 @@ BOOL CosaDmlSetPlumeNativeAtmBsControl ( ANSC_HANDLE hContext, BOOL value )
 
     return FALSE;
 }
+
+BOOL CosaDmlGetPlumeLogpullEnable ( ANSC_HANDLE hContext, BOOL *pValue )
+{
+    char buf[8];
+
+    syscfg_get (NULL, "son_logpull_enable", buf, sizeof(buf));
+
+    *pValue = (strcmp (buf, "1") == 0);
+
+    return TRUE;
+}
+
+BOOL CosaDmlSetPlumeLogpullEnable ( ANSC_HANDLE hContext, BOOL value )
+{
+    if (syscfg_set_commit (NULL, "son_logpull_enable", value ? "1" : "0") == 0)
+    {
+        return TRUE;
+    }
+
+    return FALSE;
+}
