8631280b0953961719cf1c60723c2b083e581270
Karthik Shanbhag <kshanbhag@libertyglobal.com>
2021-03-23 14:24:28 +0100

    add support for Device.X_LGI-COM_SON.SONLogpullEnable ( OWF-773 )
    [Description]
    Device.X_LGI-COM_SON.SONLogpullEnable parameter is not available
    This is new datamodel to enable or disable the log pull functionality
    If the datamodel is set to true, log should be uploaded to server.
    If set to false, logupload should not happen.
    
    [Test Description]:
    On Factory reset, Device.X_LGI-COM_SON.SONLogpullEnable should be false.
    If log-upload is called from the server, log-pull should not happen.
    Set Device.X_LGI-COM_SON.SONLogpullEnable to true, do log pull from the server. Logs should be uploaded to server.
    
    Signed-off-by: Karthik Shanbhag(kshanbhag@libertyglobal.com)

--- source/TR-181/middle_layer_src/cosa_lgi_plume_dml.c
+++ source/TR-181/middle_layer_src/cosa_lgi_plume_dml.c
@@ -62,6 +62,11 @@ LgiPlume_GetParamBoolValue
     {
         return CosaDmlGetPlumeNativeAtmBsControl(NULL, pValue);
     }
+    if (strcmp(ParamName, "SONLogpullEnable") == 0)
+    {
+        return CosaDmlGetPlumeLogpullEnable(NULL, pValue);
+    }
+
     return -1;
 }
 
@@ -101,6 +106,11 @@ LgiPlume_SetParamBoolValue
     {
         pMyObject->plumeNativeAtmBsControl = value;
         pMyObject->bPlumeNativeAtmBsControlChanged = 1;
+        return TRUE;
+    }
+    if (strcmp(ParamName, "SONLogpullEnable") == 0)
+    {
+        pMyObject->plumeLogpullEnable = value;
         return TRUE;
     }
 
@@ -174,6 +184,7 @@ LgiPlume_Commit
     CosaDmlSetPlumeAdminStatus(NULL, pMyObject->plumeAdminStatus);
     CosaDmlSetPlumeOperationalStatus(NULL, pMyObject->plumeOperationalStatus);
     CosaDmlSetPlumeDFSEnable(NULL, pMyObject->plumeDFSEnable);
+    CosaDmlSetPlumeLogpullEnable(NULL, pMyObject->plumeLogpullEnable);
     if (pMyObject->bPlumeNativeAtmBsControlChanged) {
         CosaDmlSetPlumeNativeAtmBsControl(NULL, pMyObject->plumeNativeAtmBsControl);
     }
