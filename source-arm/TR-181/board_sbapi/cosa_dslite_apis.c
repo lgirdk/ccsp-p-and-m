bdab57d9daaf5678d91aad715de86b33305f4c68
djakkula.contractor@libertyglobal.com <djakkula.contractor@libertyglobal.com>
2021-03-24 06:25:22 +0100

    fix erouter0 is losing ip after enabling Device.DSLite. in ipv4 Mode ( OFW-882 )
    Handling the DSlite enabling to stop, if the device mode is in IPV4 or Dualstack
    Applied condition check at InterfaceSetting as this parameter (Device.DSLite.InterfaceSetting.1.Enable)is also used to set DSLite
    
    Test description:
    
      Set dmcli eRT setv Device.DSLite.Enable  bool true
      Monitor ifconfig erouter0
    
    Signed-off-by: XXXX <XXXX@YYYY.com>
    
    fix DSLite not being enabled when device is in ipv6Only mode ( OFW-1040 )
    
    Bug Description - DSLite is not enabling When device is in ipv6Only Mode . this issue is caused by OFW-882
    
    Testing Performed - Tried enabling DSLite
    		    Tested the same on all the 3 modes
    
    Signed-off-by: Sarveswaran Nagarajan <sanagarajan.contractor@libertyglobal.com>

--- source-arm/TR-181/board_sbapi/cosa_dslite_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dslite_apis.c
@@ -18,6 +18,7 @@
 #include <utapi.h>
 #include "cosa_dslite_apis.h"
 #include <utapi/utapi_dslite.h>
+#include "cosa_x_cisco_com_devicecontrol_internal.h"
 
 ANSC_STATUS
 CosaDmlDsliteInit
@@ -69,6 +70,14 @@ CosaDmlSetDsliteEnable
     UtopiaContext ctx;
     boolean_t read_dslite_enable;
     BOOL dns_override = FALSE;
+    ULONG   deviceMode;
+
+   if (CosaDmlDcGetDeviceMode(NULL, &deviceMode) == ANSC_STATUS_SUCCESS) {
+       if((deviceMode == COSA_DML_DEVICE_MODE_Ipv4 ) || (deviceMode == COSA_DML_DEVICE_MODE_Dualstack )) {
+           CcspTraceWarning(("Cannot set DSLite, the device mode is ipv4 or dualstack \n" ));
+           return ANSC_STATUS_FAILURE;
+       }
+   }
 
     if(!Utopia_Init(&ctx))
         return ANSC_STATUS_FAILURE;
