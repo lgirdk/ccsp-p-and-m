4552d033e3922f6a26baec24d9c9c24d5be8fae6
anil shetty <adshetty@libertyglobal.com>
2021-03-31 12:22:15 -0700

    don't support empty string for Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.{i}.Alias ( OFW-1058 )

--- source/TR-181/middle_layer_src/cosa_dhcpv4_dml.c
+++ source/TR-181/middle_layer_src/cosa_dhcpv4_dml.c
@@ -10957,8 +10957,11 @@ LanAllowedSubnetTable_SetParamStringValue
     }
     if (AnscEqualString(ParamName, "Alias", TRUE))
     {
-        _ansc_snprintf(pLanAllowedSubnet->Alias, sizeof(pLanAllowedSubnet->Alias), "%s", pString);
-        return TRUE;
+        if (strcmp (pString, "") != 0)
+        {
+            _ansc_snprintf(pLanAllowedSubnet->Alias, sizeof(pLanAllowedSubnet->Alias), "%s", pString);
+            return TRUE;
+        }
     }
 
     return FALSE;
