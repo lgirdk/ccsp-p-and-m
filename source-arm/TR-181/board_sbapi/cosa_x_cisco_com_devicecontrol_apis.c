0a7cd2ba519f019efa31a4342c945305c7806362
Tarun Saha <tsaha@libertyglobal.com>
2021-04-12 13:02:40 -0700

    replace igmpproxy with mcproxy

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
@@ -2815,7 +2815,7 @@ CosaDmlDcGetIGMPProxyEnable
     )
 {
     UNREFERENCED_PARAMETER(hContext);
-    if ( detect_process("igmpproxy") == 0 )
+    if ( detect_process("mcproxy_v4") == 0 )
     {
         *pFlag = FALSE;
     }
@@ -2847,14 +2847,14 @@ CosaDmlDcSetIGMPProxyEnable
 
     if (pFlag)
     {
-        if ( detect_process("igmpproxy") == 0 )
+        if ( detect_process("mcproxy_v4") == 0 )
         {
-            system("igmpproxy &");
+            system("mcproxy_v4 &");
         }
     }
     else
     {
-        system("killall igmpproxy");
+        system("killall mcproxy_v4");
     }
     return ANSC_STATUS_SUCCESS;
 }
