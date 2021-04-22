251344c6fd891f53769d2e5dc68801268df0e878
Andre McCurdy <armccurdy@gmail.com>
2021-04-22 14:38:19 -0700

    drop Device.DeviceInfo.CustomDataModelEnabled data model object
    Revert "RDKB-26623, RDKBDEV-55: Adding Custom_Mapper functionality"
    
    Drop support for controlling Alias Manager functionality via the
    Device.DeviceInfo.CustomDataModelEnabled data model object. Since
    Alias Manager is expected to be either always enabled or disabled
    by a build time option, the run time control via the data model is
    not required.
    
    This reverts commit 213913410072b7b5754b75d6fb292e797b649736.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
@@ -527,12 +527,6 @@ DeviceInfo_GetParamBoolValue
         return TRUE;
     }
 
-    if (strcmp(ParamName, "CustomDataModelEnabled") == 0)
-    {
-        CosaDmlGiGetCustomDataModelEnabled(NULL, pBool);
-        return TRUE;
-    }
-
   bReturnValue =
         DeviceInfo_GetParamBoolValue_Custom
             (
@@ -1086,7 +1080,6 @@ DeviceInfo_SetParamBoolValue
         BOOL                        bValue
     )
 {
-    PCOSA_DATAMODEL_DEVICEINFO      pMyObject = (PCOSA_DATAMODEL_DEVICEINFO)g_pCosaBEManager->hDeviceInfo;
     BOOL                            bReturnValue;
 #if defined(_PLATFORM_RASPBERRYPI_)
     id =getuid();    
@@ -1128,12 +1121,6 @@ DeviceInfo_SetParamBoolValue
         {
             v_secure_system("/rdklogger/onboardLogUpload.sh delete &");
         }
-        return TRUE;
-    }
-
-    if (strcmp(ParamName, "CustomDataModelEnabled") == 0)
-    {
-        pMyObject->CustomDataModelEnabled = bValue;
         return TRUE;
     }
 
@@ -2130,14 +2117,6 @@ DeviceInfo_Commit
     PCOSA_DATAMODEL_DEVICEINFO      pMyObject = (PCOSA_DATAMODEL_DEVICEINFO)g_pCosaBEManager->hDeviceInfo;
 
     CosaDmlDiSetProvisioningCode(NULL, (char *)pMyObject->ProvisioningCode);
-
-    //RDKB-33819: TR069 restart is not required for BWG platforms
-#if !defined _CBR_PRODUCT_REQ_ && !defined _BWG_PRODUCT_REQ_
-    if(pMyObject->CustomDataModelEnabled)
-    {
-        CosaDmlGiSetCustomDataModelEnabled(NULL, pMyObject->CustomDataModelEnabled);
-    }
-#endif
 
     return 0;
 }
