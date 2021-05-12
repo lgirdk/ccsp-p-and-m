b1d1fc72880952d2805d37886d39300f88d28063
jyoti <jjyoti@libertyglobal.com>
2021-05-12 19:37:34 +0200

    use new "ipv6_addr-renew" sysevent ( OFW-1104 )
    Fix brlanX losing IPv6 in dslite/dualstack mode during renewal.
    
    A new sysevent "ipv6_addr-renew" has been added to handle the case
    where wan-prefix doesn't change during renewal but need to extend the
    valid and preferred lifetime. Update code to use it.
    
    Signed-off-by: jyoti <jjyoti@libertyglobal.com>

--- source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
@@ -9996,8 +9996,7 @@ dhcpv6c_dbg_thrd(void * in)
                         }
                         else
                         {
-                            commonSyseventSet("ipv6_addr-unset", "");
-                            commonSyseventSet("ipv6_addr-set", "");
+                            commonSyseventSet("ipv6_addr-renew", "");
                         }
 #endif
 
