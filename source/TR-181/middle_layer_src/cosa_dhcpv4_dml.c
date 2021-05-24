2cd41969c903f9000c233abfcca479acc353db98
Andy Carvalho <acarvalho.contractor@libertyglobal.com>
2021-05-24 10:21:37 +0200

    fix LAN client is not receiving IP from the default subnet 192.168.0.1/24 when active subnet is removed from LanAllowedSubnetTable ( OFW-1249 )
    BUG DESCRIPTION:
        LAN client is not receiving IP from the default subnet 192.168.0.1/24 when active subnet is removed from LanAllowedSubnetTable
    
    FIX DESCRIPTION:
        - Included a sysevent call to allow brlan0 and LAN client to update with proper ip.
        - Included changes for "Pool.1.MinAddress" & "Pool.1.MaxAddress" DML paramters to update with proper ip.
    
    TESTING PERFORMED:
        1. Make the device up after factory reset.
        2. Add the following entries into the LanAllowedSubetTable using dmcli,
            - Add table to "Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable."
            - Set "Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.{i}.LanAllowedSubnetIP" to "172.16.0.0"
            - Set "Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.{i}.LanAllowedSubnetMask" to  "255.255.0.0"
        3. Change Lan management entry to Lan allowed subnet,
            - Set "Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanIPAddress" to "172.16.0.1"
            - Set "Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanSubnetMask" to "255.255.0.0"
            - Set "Pool.1.MinAddress" & "Pool.1.MaxAddress" to "172.16.0.2" & "172.16.0.253" respectively.
        4. Verify that brlan0 IP is changing/ wait till gw refresh is completed.
        5. Delete the active subnet from LanAllowedSubnetTable.
        6. Verify brlan0 IP & Lan client has updated with IP from default range/ wait till gw refresh is completed.
    
    Signed-off-by: Andy Carvalho <acarvalho.contractor@libertyglobal.com>
    
    fix LAN client is not receiving IP from the default subnet 192.168.0.1/24 when active subnet is removed from LanAllowedSubnetTable ( OFW-1249 )
    
    BUG DESCRIPTION:
        LAN client is not receiving IP from the default subnet 192.168.0.1/24 when active subnet is removed from LanAllowedSubnetTable
    
    FIX DESCRIPTION:
        - Issue fixed for IP to get back to default after deleting active subnet.
        - LanIPAddress parameter update in dml.
    
    TESTING PERFORMED:
        1. Make the device up after factory reset.
        2. Add the following entries into the LanAllowedSubetTable using dmcli,
            - Add table to "Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable."
            - Set "Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.2.LanAllowedSubnetIP" to "172.16.0.0"
            - Set "Device.DHCPv4.Server.Pool.1.X_LGI-COM_LanAllowedSubnetTable.2.LanAllowedSubnetMask" to  "255.255.0.0"
        3. Change Lan management entry to Lan allowed subnet,
            - Set "Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanIPAddress" to "172.16.0.1"
            - Set "Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanSubnetMask" to "255.255.0.0"
            - Set "Pool.1.MinAddress" & "Pool.1.MaxAddress" to "172.16.0.2" & "172.16.0.253" respectively.
        4. Verify that brlan0 IP is changing/ wait till gw refresh is completed.
        5. Delete the active subnet from LanAllowedSubnetTable.
        6. Verify brlan0 IP & Lan client has updated with IP from default range/ wait till gw refresh is completed.
        7. Perform GPV to verify "LanIPAddress" is updated.
    
    Signed-off-by: Andy Carvalho <acarvalho.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_dhcpv4_dml.c
+++ source/TR-181/middle_layer_src/cosa_dhcpv4_dml.c
@@ -5855,7 +5855,8 @@ Pool_GetParamUlongValue
     if (strcmp(ParamName, "MinAddress") == 0)
     {
         /* collect value */
-	*puLong  = pPool->Cfg.MinAddress.Value;
+        CosaDmlDhcpsGetPoolCfg(NULL,&poolTemp.Cfg);
+        *puLong  = poolTemp.Cfg.MinAddress.Value;
 
         return TRUE;
     }
@@ -5863,7 +5864,8 @@ Pool_GetParamUlongValue
     if (strcmp(ParamName, "MaxAddress") == 0)
     {
         /* collect value */
-	*puLong  = pPool->Cfg.MaxAddress.Value;
+        CosaDmlDhcpsGetPoolCfg(NULL,&poolTemp.Cfg);
+        *puLong  = poolTemp.Cfg.MaxAddress.Value;
 
         return TRUE;
     }
