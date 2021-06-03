68b244b11e44bbbc4c2a7252574a70d8775ce868
anil shetty <adshetty@libertyglobal.com>
2021-06-02 17:13:01 -0700

    fix buffer size for syscfg get of CaptivePortal_Enable
    The expected values are "true" or "false", so the buffer should be at
    least 6 chars to avoid truncation.
    
    Also fix typo in related macro ( EANBLE -> ENABLE ).

--- custom/comcast/source/TR-181/custom_sbapi/cosa_deviceinfo_apis_custom.c
+++ custom/comcast/source/TR-181/custom_sbapi/cosa_deviceinfo_apis_custom.c
@@ -85,7 +85,7 @@
 
 #define _ERROR_ "NOT SUPPORTED"
 
-#define CAPTIVEPORTAL_EANBLE     "CaptivePortal_Enable"
+#define CAPTIVEPORTAL_ENABLE     "CaptivePortal_Enable"
 
 extern void* g_pDslhDmlAgent;
 
@@ -536,9 +536,9 @@ CosaDmlGetCaptivePortalEnable
         BOOL *pValue
     )
 {
-	char buf[5];
+	char buf[8];
         /* CID: 58774 Array compared against 0*/
-        if(!syscfg_get( NULL, CAPTIVEPORTAL_EANBLE , buf, sizeof(buf)))
+        if(!syscfg_get( NULL, CAPTIVEPORTAL_ENABLE , buf, sizeof(buf)))
 	{
     		if (strcmp(buf,"true") == 0)
 		{
@@ -577,7 +577,7 @@ CosaDmlSetCaptivePortalEnable
 		CcspTraceWarning(("CaptivePortal: Disabling Captive Portal switch ...\n"));		
 		strcpy(buf,"false");
 	}
-	if (syscfg_set(NULL, CAPTIVEPORTAL_EANBLE , buf) != 0) {
+	if (syscfg_set(NULL, CAPTIVEPORTAL_ENABLE , buf) != 0) {
                      CcspTraceWarning(("syscfg_set failed to enable/disable captive portal\n"));
 		     return ANSC_STATUS_FAILURE;
              } else {
