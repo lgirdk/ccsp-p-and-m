8b94ce6fdec58a20e489b72bec47139ecae36dfc
Andre McCurdy <armccurdy@gmail.com>
2021-07-01 13:49:46 -0700

    fix invalid string passed to Utopia_RawSet() in CosaDmlRaIfSetCfg()
    Ensure that the strings passed to Utopia_RawSet() in
    CosaDmlRaIfSetCfg() are correctly nul terminated (it's not safe to
    construct a string by writing a single char to the first location in
    a buffer and hoping that the second location is a nul...).
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-arm/TR-181/board_sbapi/cosa_ra_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_ra_apis.c
@@ -512,11 +512,8 @@ CosaDmlRaIfSetCfg
                     return ANSC_STATUS_FAILURE;
                 }
 
-		out[0] = pCfg->bAdvManagedFlag?'1':'0';
-		Utopia_RawSet(&utctx,NULL,"router_managed_flag",out);
-		
-		out[0] = pCfg->bAdvOtherConfigFlag?'1':'0';
-		Utopia_RawSet(&utctx,NULL,"router_other_flag",out);
+		Utopia_RawSet(&utctx, NULL, "router_managed_flag", pCfg->bAdvManagedFlag ? "1" : "0");
+		Utopia_RawSet(&utctx, NULL, "router_other_flag", pCfg->bAdvOtherConfigFlag ? "1" : "0");
 
 		snprintf(out, sizeof(out), "%u", pCfg->MinRtrAdvInterval);
 		Utopia_RawSet(&utctx,NULL,"ra_interval",out);
