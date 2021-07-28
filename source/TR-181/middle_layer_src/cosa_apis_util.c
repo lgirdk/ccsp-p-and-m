7764207c0e6a8d00298f9ed57c032845bb599b5d
Suresh Putta <suputta@libertyglobal.com>
2021-07-28 12:28:12 -0700

    disable logging every system call to console

--- source/TR-181/middle_layer_src/cosa_apis_util.c
+++ source/TR-181/middle_layer_src/cosa_apis_util.c
@@ -2105,7 +2105,7 @@ int vsystem(const char *fmt, ...)
     if (n >= sizeof(cmd))
         return -1;
 
-    fprintf(stderr, "%s: %s\n", __FUNCTION__, cmd);
+    //fprintf(stderr, "%s: %s\n", __FUNCTION__, cmd);
     return system(cmd);
 }
 
