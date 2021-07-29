b9a438d7816ab5f0bae9a688fd0c706da3846d09
Sandipan Sinha <ssinha.contractor@libertyglobal.com>
2021-07-29 08:09:05 +0200

    start_lighttpd.sh: create lighttpd.conf (and set server.document-root) dynamically ( OFW-1514 )
    Fix for troubleshoot Page not loading after RF disconnect.
    
    DESCRIPTION:
    
    - Troubleshoot Page not loading after RF disconnect in case of
      Local or Cloud UI.
    
    FIX PERFORMED:
    
    - Made changes in the script starting the lighttpd process.
    
    - The script will read that whether Cloud UI is enabled or not and
      accordingly configure the lighttpd.conf file in the location
      /var/lighttpd.conf
    
    - The "server.document-root" is being made dynamic for lighttpd.conf.
    
    Signed-off-by: Sandipan Sinha <ssinha.contractor@libertyglobal.com>
    
    optimise dynamic creation of lighttpd.conf
    
    Only create an alternative lighttpd.conf if the Cloud UI is enabled
    and the default lighttpd.conf can't be used.
    
    Also avoid creating the alternative lighttpd.conf if it already
    exists (ie it's been created before at some point since booting).
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- arch/intel_usg/boards/arm_shared/scripts/start_lighttpd.sh
+++ arch/intel_usg/boards/arm_shared/scripts/start_lighttpd.sh
@@ -5,12 +5,37 @@ LOCAL_UI_ENABLE="$(syscfg get local_ui_enable)"
 # Fixme: Calling the init script only works for non-systemd builds...
 #        Need to add support for starting via systemd too.
 
+lighttpd_start()
+{
+	LIGHTTPD_PID=$(pidof lighttpd)
+
+	if [ -n "${LIGHTTPD_PID}" ]
+	then
+		/bin/kill ${LIGHTTPD_PID}
+	fi
+
+	if [ "$(syscfg get cloud_enable_flag)" = "1" ]
+	then
+		if [ ! -f /var/lighttpd.conf ]
+		then
+			sed 's|^server\.document-root .*|server.document-root = "/www/cloud/"|' < /etc/lighttpd/lighttpd.conf > /var/lighttpd.conf
+			ln -sf /etc/lighttpd/gateway.conf /var/gateway.conf
+		fi
+		CONF_FILE="/var/lighttpd.conf"
+	else
+		CONF_FILE="/etc/lighttpd/lighttpd.conf"
+	fi
+
+	if [ "${LOCAL_UI_ENABLE}" = "true" ]
+	then
+		/usr/sbin/lighttpd -f ${CONF_FILE}
+	fi
+}
+
 case "$1"
 in
 	start)
-		if [ "${LOCAL_UI_ENABLE}" = "true" ] ; then
-			/etc/init.d/lighttpd start
-		fi
+		lighttpd_start
 		;;
 
 	stop)
@@ -18,11 +43,10 @@ in
 		;;
 
 	restart)
-		if [ "${LOCAL_UI_ENABLE}" = "true" ] ; then
-			/etc/init.d/lighttpd restart
-		else
-			/etc/init.d/lighttpd stop
+		/etc/init.d/lighttpd stop
+		if [ "${LOCAL_UI_ENABLE}" = "true" ]
+		then
+			lighttpd_start
 		fi
 		;;
-
 esac
