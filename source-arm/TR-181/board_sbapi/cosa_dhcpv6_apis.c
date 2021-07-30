f34c25cfc857ff5381c6e8ae5b646ec63456d645
Tarun Saha <tsaha@libertyglobal.com>
2021-07-30 08:09:06 -0700

    fix dibbler-server restart on each renew ( OFW-1674 )
    For same delegated prefix, do not generate dhcpv6s-restart event.

--- source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
@@ -8740,6 +8740,8 @@ dhcpv6c_dbg_thrd(void * in)
 /* Service IPv6 will assign IP address and prefix allocation,
    for all lan interfaces.
 */
+#if 0 /* Not required for MVx platforms with MULTILAN feature enabled */
+
 #if !defined(_COSA_INTEL_XB3_ARM_)
                         // not the best place to add route, just to make it work
                         // delegated prefix need to route to LAN interface
@@ -8747,7 +8749,6 @@ dhcpv6c_dbg_thrd(void * in)
 			#ifdef _COSA_INTEL_XB3_ARM_
                         v_secure_system("ip -6 route add %s dev %s table erouter", v6pref, COSA_DML_DHCPV6_SERVER_IFNAME);
 			#endif
-#ifndef _PUMA6_ARM_
                         CosaDmlDHCPv6sTriggerRestart(FALSE);
 #if defined(_COSA_BCM_ARM_) || defined(INTEL_PUMA7)
                         CcspTraceWarning((" %s dhcpv6_assign_global_ip to brlan0 \n", __FUNCTION__));
@@ -8804,7 +8805,7 @@ dhcpv6c_dbg_thrd(void * in)
 			    CcspTraceWarning(("%s: setting lan-restart\n", __FUNCTION__));
                             commonSyseventSet("lan-restart", "1");
                         }
-#endif //_PUMA6_ARM_
+#endif
 #endif
 #else
 #if defined(FEATURE_RDKB_WAN_MANAGER)
