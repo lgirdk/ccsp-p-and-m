19b7bb5c2faad8b84799ea997dd60fc0b6173634
qlei <rlei@libertyglobal.com>
2021-08-24 14:57:51 -0700

    set pEntry->Interface with the string in right format (ifname instead of path name)

--- source/TR-181/middle_layer_src/cosa_routing_dml.c
+++ source/TR-181/middle_layer_src/cosa_routing_dml.c
@@ -3856,6 +3856,10 @@ IPv6Forwarding_GetParamStringValue
         char                            * pString      = NULL;
 
         /* collect value */
+#if defined(USE_TR181_PATH)
+        AnscCopyString(pValue, pRouterForward->Interface);
+        return 0;
+#else
         pString = (char*)CosaUtilGetFullPathNameByKeyword
             (
                 (PUCHAR)"Device.IP.Interface.",
@@ -3891,6 +3895,7 @@ IPv6Forwarding_GetParamStringValue
         {
             return 0;
         }
+#endif
     }
 
     if (strcmp(ParamName, "ExpirationTime") == 0)
@@ -4107,6 +4112,9 @@ IPv6Forwarding_SetParamUlongValue
     return:     TRUE if succeeded.
 
 **********************************************************************/
+#ifndef USE_TR181_PATH
+extern void* g_pDslhDmlAgent;
+#endif
 BOOL
 IPv6Forwarding_SetParamStringValue
     (
@@ -4176,12 +4184,25 @@ IPv6Forwarding_SetParamStringValue
             return FALSE;
 
         /* save update to backup */
+#if defined(USE_TR181_PATH)
         rc = STRCPY_S_NOCLOBBER(pRouterForward->Interface, sizeof(pRouterForward->Interface), wrapped_inputparam);
         if(rc != EOK)
         {
             ERR_CHK(rc);
             return FALSE;
-        }
+        }		
+#else
+{
+        int len;
+        char key[128];
+        snprintf(key, sizeof(key), "%sName", pString);
+        len = sizeof(pRouterForward->Interface);
+        rc = g_GetParamValueString(g_pDslhDmlAgent, key, pRouterForward->Interface, &len);
+		if(rc != 0)
+			return FALSE;
+}
+#endif
+
         return TRUE;
     }
 
