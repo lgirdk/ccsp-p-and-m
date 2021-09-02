6144a28c74f22b8404deee943394d5b8ed42a29a
Tarun Saha <tsaha@libertyglobal.com>
2021-09-01 17:31:38 -0700

    add support for Device.X_LGI-COM_General_Internal.SKUName ( OFW-1871 OFW-1905 )
    Added a new CWMP parameter for Device.X_LGI-COM_General_Internal.SKUName
    
    OFW-1905: Added defined SKU names for MV2+ device
    
    update Customer ID 1003 -> 51 in CosaDmlGiGetSKU()
    
    update the SKUName for Slovakia ( OFW-2754 )
    
    [Description]
    SKUName for SK is to be updated from "Connect Box" to
    "Giga Connect Box 6"
    
    [Solution]
    Update the SKUName for SK customer ID.
    
    [Testing Performed]
    1. Set the SK customer ID.
    2. Validate the SKUName in the DM.
    3. Check the login page of the UI.
    
    Upstream Candidate: LGI specific fix.
    
    Signed-off-by: Ajay Vishnu Bhat <abhat@libertyglobal.com>

--- source-arm/TR-181/board_sbapi/cosa_lgi_general_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_lgi_general_apis.c
@@ -115,6 +115,67 @@ CosaDmlGiGetTroubleshootWizardEnable
     return ANSC_STATUS_SUCCESS;
 }
 
+ANSC_STATUS CosaDmlGiGetSKU (ANSC_HANDLE hContext, char *pValue, ULONG ulSize)
+{
+    char buf[20];
+    char *sku = NULL;
+    int customerId = 0;
+    ULONG len;
+
+    if (syscfg_get (NULL, "Customer_Index", buf, sizeof(buf)) == 0)
+    {
+        customerId = atoi(buf);
+    }
+
+    buf[0] = 0;
+    len = sizeof(buf);
+    CosaDmlDiGetProductClass (hContext, buf, &len);
+
+    if (strcmp (buf, "CH7465LG") == 0)
+    {
+        if (customerId == 6)
+        {
+            sku = "Nextgen WiFi";
+        }
+        else if (customerId == 8)
+        {
+            sku = "Hub 3";
+        }
+    }
+    else if (strcmp (buf, "MERCV2P") == 0)
+    {
+        if (customerId == 8)
+        {
+            sku = "Hub 5";
+        }
+        else if (customerId == 20)
+        {
+            sku = "SmartWifi modem";
+        }
+        else if (customerId == 41)
+        {
+            sku = "Virgin Media Hub 6";
+        }
+        else if (customerId == 51)
+        {
+            sku = "Connect Box 3";
+        }
+        else if (customerId == 53)
+        {
+            sku = "Giga Connect Box 6";
+        }
+    }
+
+    if (sku == NULL)
+    {
+        sku = "Connect Box";
+    }
+
+    snprintf (pValue, ulSize, sku);
+
+    return ANSC_STATUS_SUCCESS;
+}
+
 ANSC_STATUS
 CosaDmlGiGetCustomerId
     (
