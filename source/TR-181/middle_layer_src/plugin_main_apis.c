ea8bf7b58d70156283dc99fbec9206b6b152b43c
Sarveswaran Nagarajan <sanagarajan.contractor@libertyglobal.com>
2021-09-30 20:56:05 +0200

    add support for Device.X_LGI-COM_Applications.Speedtest.SamKnows.Enable etc ( OFW-1841 )
    Implemented the below TR-181 Data Model parameters.
    
    1. Device.X_LGI-COM_Applications.Speedtest.SamKnows.Enable
    2. Device.X_LGI-COM_Applications.Speedtest.SamKnows.Property
    
    TESTING PERFORMED:
    
    1. Bring up the DUT online.
    2. Perform GPV/SPV for the parameters via dmcli
    3. Perform GPV/SPV for the parameters via tr069
    
    Signed-off-by: Sarveswaran Nagarajan <sanagarajan.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/plugin_main_apis.c
+++ source/TR-181/middle_layer_src/plugin_main_apis.c
@@ -159,6 +159,9 @@
 #include "cosa_lgi_wol_internal.h"
 // LGI ADD END
 
+#include "cosa_lgi_applications_apis.h"
+#include "cosa_lgi_applications_internal.h"
+
 static void CheckAndSetRebootReason();
 
 #if defined(_PLATFORM_RASPBERRYPI_)
@@ -482,6 +485,10 @@ if(id != 0)
     AnscTraceWarning(("  CosaTemperatureStatusCreate done!\n"));
     // LGI ADD END
 
+ 
+    pMyObject->hLgiApplications = (ANSC_HANDLE)CosaLgiApplicationsCreate();
+    AnscTraceWarning(("  CosaLgiApplicationsCreate done !\n"));
+
     printf("PandM DM initialization done!\n");
     CcspTraceWarning(("RDKB_SYSTEM_BOOT_UP_LOG : PandM DM initialization done!\n"));
     //Unknown Reboot Reason 
@@ -764,6 +771,11 @@ CosaBackEndManagerRemove
         CosaLgiWoLRemove((ANSC_HANDLE)pMyObject->hLgiWoL);
     }
     //LGI ADD END
+    
+    if (pMyObject->hLgiApplications)
+    {
+        CosaLgiApplicationsRemove((ANSC_HANDLE)pMyObject->hLgiApplications);
+    }
 
     if (pMyObject->hLgiPlume)
     {
