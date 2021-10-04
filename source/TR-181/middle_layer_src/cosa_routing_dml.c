2ad5d28e5d64356f66887864dc2d59a2c459af45
Cibil P <cpankiras.contractor@libertyglobal.com>
2021-10-04 14:46:46 +0200

    RIPv2: Support to enable/disable RIPv2 etc
    The basic RIPv2 feature comes from RDK. Made the following changes to comply with MVXREQ.
    
    MVXREQ-1776: Support to enable/disable RIPv2
        i)   Changed ripd.conf file location to /var from /etc. /etc directory is mounted as readonly
        ii)  Start ripd daemon during bootup if RIPv2 feature is enabled.
        iii) Restart firewall to allow/block ripv2 multicast packets accordingly
        iv)  Listen VTY on localhost
    
    MVXREQ-1779: Specifying the supported modes for RIP
        i)  Changed the default value of Device.Routing.RIP.SupportedModes to "both"
    
    MVXREQ-1781: Support for configuring RIP route advertisment intervals on the enabled interfaces
        i)   Created a datamodel Device.Routing.RIP.InterfaceSetting.{i}.X_LGI-COM_UpdateInterval to
             configure periodic route update.
        ii)  Changed the update interval logic from X_CISCO_COM_UpdateInterval to the
             new data model X_LGI-COM_UpdateInterval.
        iii) If X_LGI-COM_UpdateInterval is zero, then device should not send any periodic route update.
    
    MVXREQ-1777: Support for RIPv2 in different provisioning modes
        i)  Made changes to support RIPv2 feature only in IPv4 or dual stack mode.
        ii) Made changes to start ripd daemon during bootup if device is in IPv4 or dual stack mode.
    
    MVXREQ-1783: Support for accepting RIP routes on enabled interfaces
        i) As per MVXREQ, default value is true. As per MVXREQ, the default value is true.
           Made changes to read the value from struct irrespective of "RIP.Enable" value
    
    MVXREQ-1782: Support to send RIP route advertisements on enabled interfaces
        i) As per MVXREQ, default value is true. As per MVXREQ, the default value is true.
           Made changes to read the value from struct irrespective of "RIP.Enable" value
    
    MVXREQ-1780: Configuring the IP interface for send/receiving RIP messages
        i)  Change the logic to get/set Device.Routing.RIP.InterfaceSetting.{i}.Interface
            dml directly without relying on Device.Routing.RIP.InterfaceSetting.{i}.Alias.
        ii) RIPv2 request/response should only go through erouter0 interface. Changed the ripd.conf accordingly.
    
    Note:
    Enabled vty password in ripd daemon for debugging.
    
    Signed-off-by: Cibil <cpankiras.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_routing_dml.c
+++ source/TR-181/middle_layer_src/cosa_routing_dml.c
@@ -4557,7 +4557,7 @@ RIP_SetParamBoolValue
     PCOSA_DATAMODEL_ROUTING         pMyObject     = (PCOSA_DATAMODEL_ROUTING)g_pCosaBEManager->hRouting; 
     PCOSA_DML_RIP_CFG               pCfg          = &(pMyObject->RIPCfg);
     
-    BRIDGE_MODE_JUDGEMENT_IFTRUE_RETURNFALSE
+    BRIDGE_AND_IPv6_MODE_JUDGEMENT_IFTRUE_RETURNFALSE
 
     /* check the parameter name and set the corresponding value */
     if (strcmp(ParamName, "Enable") == 0)
@@ -4663,7 +4663,7 @@ RIP_SetParamUlongValue
     PCOSA_DATAMODEL_ROUTING         pMyObject     = (PCOSA_DATAMODEL_ROUTING)g_pCosaBEManager->hRouting; 
     PCOSA_DML_RIP_CFG               pCfg          = &(pMyObject->RIPCfg);
 
-    BRIDGE_MODE_JUDGEMENT_IFTRUE_RETURNFALSE
+    BRIDGE_AND_IPv6_MODE_JUDGEMENT_IFTRUE_RETURNFALSE
     #define MIN 1
     #define MAX 15
     
@@ -5100,21 +5100,14 @@ InterfaceSetting_GetParamBoolValue
     if (strcmp(ParamName, "AcceptRA") == 0)
     {
         /* collect value */
-        if(!pCfg->Enable) 
-            *pBool = FALSE;
-        else
-            *pBool = pRipIF->AcceptRA;
-
+        *pBool = pRipIF->AcceptRA;
         return TRUE;
     }
 
     if (strcmp(ParamName, "SendRA") == 0)
     {
         /* collect value */
-        if(!pCfg->Enable) 
-            *pBool = FALSE;
-        else
-            *pBool = pRipIF->SendRA;        
+        *pBool = pRipIF->SendRA;
 
         return TRUE;
     }
@@ -5217,6 +5210,13 @@ InterfaceSetting_GetParamUlongValue
     {
         /* collect value */        
         *puLong = pRipIF->Status;  
+        return TRUE;
+    }
+
+    if (strcmp(ParamName, "X_LGI-COM_UpdateInterval") == 0)
+    {
+        /* collect value */
+        *puLong  = pRipIF->X_LGI_COM_UpdateInterval;
         return TRUE;
     }
 
@@ -5328,12 +5328,7 @@ InterfaceSetting_GetParamStringValue
     if (strcmp(ParamName, "Interface") == 0)
     {
         /* collect value */
-        if ((_ansc_strcmp(pRipIF->Alias, COSA_RIPD_IF1_NAME ) == 0 ) ||
-            (_ansc_strcmp(pRipIF->Alias, "cpe" ) == 0 ) ||
-            (_ansc_strcmp(pRipIF->Alias, "Ethernet" ) == 0 ) )
-            AnscCopyString(pValue, "Ethernet");
-        else
-            AnscCopyString(pValue, "Cable");
+        AnscCopyString(pValue, pRipIF->Interface);
 
         return 0;
     }
@@ -5397,7 +5392,7 @@ InterfaceSetting_SetParamBoolValue
     PCOSA_CONTEXT_LINK_OBJECT       pCosaContext   = (PCOSA_CONTEXT_LINK_OBJECT)hInsContext;
     PCOSA_DML_RIP_IF_CFG           pRipIF         = (PCOSA_DML_RIP_IF_CFG)pCosaContext->hContext;
 
-    BRIDGE_MODE_JUDGEMENT_IFTRUE_RETURNFALSE
+    BRIDGE_AND_IPv6_MODE_JUDGEMENT_IFTRUE_RETURNFALSE
 
     /* check the parameter name and set the corresponding value */
     if (strcmp(ParamName, "Enable") == 0)
@@ -5514,7 +5509,13 @@ InterfaceSetting_SetParamUlongValue
     PCOSA_CONTEXT_LINK_OBJECT       pCosaContext   = (PCOSA_CONTEXT_LINK_OBJECT)hInsContext;
     PCOSA_DML_RIP_IF_CFG           pRipIF         = (PCOSA_DML_RIP_IF_CFG)pCosaContext->hContext;
 
-    BRIDGE_MODE_JUDGEMENT_IFTRUE_RETURNFALSE
+    BRIDGE_AND_IPv6_MODE_JUDGEMENT_IFTRUE_RETURNFALSE
+
+    if (strcmp(ParamName, "X_LGI-COM_UpdateInterval") == 0)
+    {
+        pRipIF->X_LGI_COM_UpdateInterval  = uValue;
+        return TRUE;
+    }
 
     /* check the parameter name and set the corresponding value */
     if (strcmp(ParamName, "X_CISCO_COM_SendVersion") == 0)
@@ -5601,7 +5602,7 @@ InterfaceSetting_SetParamStringValue
     errno_t                         rc             = -1;
     int                             ind            = -1;
 
-    BRIDGE_MODE_JUDGEMENT_IFTRUE_RETURNFALSE
+    BRIDGE_AND_IPv6_MODE_JUDGEMENT_IFTRUE_RETURNFALSE
 
     /* check if pString doesn't hold null or whitespaces */
     if(AnscValidStringCheck((PUCHAR)pString) != TRUE)
@@ -5658,7 +5659,7 @@ InterfaceSetting_SetParamStringValue
     if((rc == EOK) && (!ind))
     {
         /* save update to backup */
-        rc = STRCPY_S_NOCLOBBER(pRipIF->Alias, sizeof(pRipIF->Alias), pString);
+        rc = STRCPY_S_NOCLOBBER(pRipIF->Interface, sizeof(pRipIF->Interface), pString);
         if(rc != EOK)
         {
             ERR_CHK(rc);
