7993619ecd620351ec3d8944c57f6428683cf673
Siddalingesh <sihiregoudar@libertyglobal.com>
2021-11-08 22:43:02 +0100

    add support for refresh-plume sysevent ( OFW-2022 )
    Fix devices not showing online in Plume NOC After DHCP Subnet Change.
    
    Signed-off-by: Sidda(sihiregoudar@libertyglobal.com)

--- source-arm/TR-181/board_sbapi/cosa_dhcpv4_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dhcpv4_apis.c
@@ -2533,6 +2533,7 @@ CosaDmlDhcpsSetPoolCfg
         rc = Utopia_SetDhcpV4ServerPoolCfg(&ctx, pCfg);
 
         commonSyseventSet("refresh-switch", "true"); 
+        commonSyseventSet("refresh-plume", "true");
 
         /* Free Utopia Context */
         Utopia_Free(&ctx,!rc);
