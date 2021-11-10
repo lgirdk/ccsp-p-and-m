64759e51a592d177a432b0b314a0d32699a3223e
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2021-11-10 13:06:17 +0100

    add support for Device.X_LGI-COM_Multicast.Enable and Device.X_LGI-COM_Multicast.SnoopingEnable ( OFW-2015 )
    Implemented the below TR-181 Data Model parameters as Read-Only as per the MVXREQ-2079 and default value is TRUE.
    
      Device.X_LGI-COM_Multicast.Enable
      Device.X_LGI-COM_Multicast.SnoopingEnable
    
    TESTING PERFORMED:
    
      1. Bring up the DUT online.
      2. Perform GPV for the parameters via dmcli.
      3. Perform GPV for the parameters via tr069.
      4. Tested and Verified that Multicast traffic is being forwarded.
    
    Signed-off-by: BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/plugin_main_apis.c
+++ source/TR-181/middle_layer_src/plugin_main_apis.c
@@ -159,6 +159,8 @@
 #include "cosa_lgi_wol_internal.h"
 #include "cosa_lgi_applications_apis.h"
 #include "cosa_lgi_applications_internal.h"
+#include "cosa_lgi_multicast_apis.h"
+#include "cosa_lgi_multicast_internal.h"
 // LGI ADD END
 
 #if  defined  (WAN_FAILOVER_SUPPORTED) || defined(RDKB_EXTENDER_ENABLED) ||  defined(RBUS_BUILD_FLAG_ENABLE) || defined (_HUB4_PRODUCT_REQ_)
@@ -509,6 +511,9 @@ if(id != 0)
     pMyObject->hLgiApplications = (ANSC_HANDLE)CosaLgiApplicationsCreate();
     AnscTraceWarning(("  CosaLgiApplicationsCreate done !\n"));
 
+    pMyObject->hLgiMulticast = (ANSC_HANDLE)CosaLgiMulticastCreate();
+    AnscTraceWarning((" CosaLgiMulticastCreate done !\n"));
+
     printf("PandM DM initialization done!\n");
     CcspTraceWarning(("RDKB_SYSTEM_BOOT_UP_LOG : PandM DM initialization done!\n"));
     //Unknown Reboot Reason 
@@ -795,6 +800,11 @@ CosaBackEndManagerRemove
     if (pMyObject->hLgiApplications)
     {
         CosaLgiApplicationsRemove((ANSC_HANDLE)pMyObject->hLgiApplications);
+    }
+
+    if (pMyObject->hLgiMulticast)
+    {
+        CosaLgiMulticastRemove((ANSC_HANDLE)pMyObject->hLgiMulticast);
     }
 
     if (pMyObject->hLgiPlume)
