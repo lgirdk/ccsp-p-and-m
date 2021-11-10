2974e86542c47c83e108b765d7125e31e6e85583
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2021-11-10 13:06:17 +0100

    add support for Device.X_LGI-COM_Multicast.Enable and Device.X_LGI-COM_Multicast.SnoopingEnable ( OFW-2015 )
    Implemented the below TR-181 Data Model parameters as Read-Only as per the MVXREQ-2079 and default value is TRUE.
    
      Device.X_LGI-COM_Multicast.Enable
      Device.X_LGI-COM_Multicast.SnoopingEnable
    
    TESTING PERFORMED:
    
      1. Bring up the DUT online.
      2. Perform GPV for the parameters via dmcli.
      3. Perform GPV for the parameters via tr069.
      4. Tested and Verified that Multicast traffic is being forwarded.
    
    Signed-off-by: BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>

---
+++ source/TR-181/middle_layer_src/cosa_lgi_multicast_apis.c
@@ -0,0 +1,54 @@
+/*********************************************************************************
+* Copyright 2019 Liberty Global B.V.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*********************************************************************************/
+
+#include "cosa_lgi_multicast_internal.h"
+#include "cosa_lgi_multicast_apis.h"
+#include <syscfg/syscfg.h>
+#include "cosa_drg_common.h"
+
+ULONG CosaDmlMulticastGetEnable ( ANSC_HANDLE hContext, BOOL *pValue )
+{
+    char buf[8];
+
+    syscfg_get (NULL, "multicast_enable", buf, sizeof(buf));
+    *pValue = (strcmp(buf, "1") == 0);
+
+    return ANSC_STATUS_SUCCESS;
+}
+
+ULONG CosaDmlMulticastSetEnable ( ANSC_HANDLE hContext, BOOL bValue )
+{
+    syscfg_set_commit (NULL, "multicast_enable", bValue ? "1" : "0");
+
+    return ANSC_STATUS_SUCCESS;
+}
+
+ULONG CosaDmlMulticastGetSnoopingEnable ( ANSC_HANDLE hContext, BOOL *pValue )
+{
+    char buf[8];
+
+    syscfg_get (NULL, "multicast_snooping_enable", buf, sizeof(buf));
+    *pValue = (strcmp(buf, "1") == 0);
+
+    return ANSC_STATUS_SUCCESS;
+}
+
+ULONG CosaDmlMulticastSetSnoopingEnable ( ANSC_HANDLE hContext, BOOL bValue )
+{
+    syscfg_set_commit (NULL, "multicast_snooping_enable", bValue ? "1" : "0");
+
+    return ANSC_STATUS_SUCCESS;
+}
