0f1b11d0e01d9844d0fcadc38f77b9588ebc8bc5
Cibil <cpankiras.contractor@libertyglobal.com>
2021-11-24 12:52:36 +0100

    RIPv2: support to advertise /29 or larger assigned subnet over RIPv2 ( MVXREQ-1809 )
    [Description]
    
    The gateway must advertise the /29 or larger static IP subnet via RIPv2 over the eRouter WAN interface to the CMTS.
    RIPv2 advertisements to the WAN must be sourced from the primary eRouter WAN address.
    
    [Testing Performed]
    
    1. Enable RIP feautre
    dmcli eRT setv Device.Routing.RIP.Enable bool true
    
    2. Set Static IP to brlan0
    dmcli eRT setv Device.IP.Interface.4.IPv4Address.1.IPAddress string  "62.179.115.33"
    dmcli eRT setv Device.IP.Interface.4.IPv4Address.1.SubnetMask string "255.255.255.252"
    
    3. The Static IP should be added to the cmts routing table
    
    Signed-off-by: Cibil <cpankiras.contractor@libertyglobal.com>

--- source-arm/TR-181/board_sbapi/cosa_ip_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_ip_apis.c
@@ -3032,6 +3032,7 @@ CosaDmlIpIfSetV4Addr
             Utopia_Free(&utctx, 1);
 
             commonSyseventSet("dhcp_server-restart", "");
+            RestartRipd();
 
             return returnStatus;
         }
