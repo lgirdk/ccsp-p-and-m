6224b8fd0998c3c482ad22dc7a53b6db2b64ec53
anil shetty <adshetty@libertyglobal.com>
2021-11-30 12:05:55 -0800

    skip adding time offset when current local time is querried

--- source-arm/TR-181/board_sbapi/cosa_time_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_time_apis.c
@@ -851,11 +851,7 @@ CosaDmlTimeGetLocalTime
     time_t t;
 #if defined(UTC_ENABLE) && !defined(_XF3_PRODUCT_REQ_)
 struct tm *pLcltime, temp;
-struct tm *pLcloffset, tempoffset;
-time_t t_offset;
    time(&t);
-   t_offset = getOffset();
-   t = t + t_offset;
 #if 0
    /* Fixme: review... should this version be used? */
    gmtime_r(&t, &temp); // already adjusted for TZ with offset
@@ -864,20 +860,13 @@ time_t t_offset;
 #endif
    pLcltime = &temp;
 
-   localtime_r(&t_offset, &tempoffset);
-   pLcloffset = &tempoffset;
-
-    _ansc_sprintf(pCurrLocalTime, "%.4u-%.2u-%.2uT%.2u:%.2u:%.2u%c%.2u:%.2u",
+   _ansc_sprintf(pCurrLocalTime, "%.4u-%.2u-%.2uT%.2u:%.2u:%.2u",
             (pLcltime->tm_year)+1900,
             (pLcltime->tm_mon)+1,
             pLcltime->tm_mday,
             pLcltime->tm_hour,
             pLcltime->tm_min,
-            pLcltime->tm_sec,
-            (t_offset?'+':'-'),
-            pLcloffset->tm_hour,
-            pLcloffset->tm_min);
-
+            pLcltime->tm_sec);
 #else
     struct tm *pLcltime;
 #ifdef _XF3_PRODUCT_REQ_
