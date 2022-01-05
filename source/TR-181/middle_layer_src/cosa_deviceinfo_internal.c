781240c22e731fb75cc6b16077d015fb1d785dd2
Andre McCurdy <armccurdy@gmail.com>
2022-01-05 13:08:52 -0800

    drop dependency on Partner ID and related json config files
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/middle_layer_src/cosa_deviceinfo_internal.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_internal.c
@@ -67,6 +67,10 @@
         01/11/2011    initial revision.
 
 **************************************************************************/
+/*
+   Define USE_PARTNER_ID to use partner ID to access some parameters
+*/
+//#define USE_PARTNER_ID
 
 #include "cosa_deviceinfo_internal.h"
 #include "cosa_deviceinfo_apis_custom.h"
@@ -187,7 +191,12 @@ CosaDeviceInfoInitialize
 	CosaDmlDiGetSyndicationTR69CertLocation( (ANSC_HANDLE)pMyObject,
 											  pMyObject->TR69CertLocation.ActiveValue );
 	CosaDeriveSyndicationPartnerID(pMyObject->PartnerID);
+
+#ifdef USE_PARTNER_ID
+    // UI Branding is not supported
     CosaDmlDiUiBrandingInit((ANSC_HANDLE)pMyObject, &pMyObject->UiBrand);
+#endif
+
 	CosaDmlDiWiFiTelemetryInit(&pMyObject->WiFi_Telemetry);
 	CosaDmlDiUniqueTelemetryIdInit(&pMyObject->UniqueTelemetryId);
     CosaDmlDiSyndicationFlowControlInit(&pMyObject->SyndicatonFlowControl);
