b05e9d35324b18cdcf14237bc9c6d5fbadc347b5
Andre McCurdy <armccurdy@gmail.com>
2022-01-05 13:08:52 -0800

    drop dependency on Partner ID and related json config files
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
@@ -90,6 +90,11 @@
         01/11/2011    initial revision.
 
 **************************************************************************/
+/*
+   Define USE_PARTNER_ID to use partner ID to access some parameters
+*/
+//#define USE_PARTNER_ID
+
 #define _GNU_SOURCE
 #include <string.h>
 #include <stdlib.h>
@@ -103,8 +108,10 @@
 #include <syscfg/syscfg.h>
 #include "safec_lib_common.h"
 #define DEVICE_PROPERTIES    "/etc/device.properties"
+#ifdef USE_PARTNER_ID
 #define PARTNERS_INFO_FILE              "/nvram/partners_defaults.json"
 #define BOOTSTRAP_INFO_FILE		"/nvram/bootstrap.json"
+#endif
 #define RFC_DEFAULTS_FILE       "/etc/rfcDefaults.json"
 #define RFC_STORE_FILE       "/opt/secure/RFC/tr181store.json"
 #define MAX_TIME_FORMAT     5
@@ -3613,6 +3620,8 @@ void UniqueTelemetryCronJob(BOOL enable, INT timeInterval, char* tagString) {
         }
 }
 
+#ifdef USE_PARTNER_ID
+
 ANSC_STATUS
 CosaDmlDiUiBrandingInit
   (
@@ -4632,6 +4641,24 @@ ANSC_STATUS UpdateJsonParam
 	 return ANSC_STATUS_SUCCESS;
 }
 
+#else
+
+ANSC_STATUS CosaDmlDiUiBrandingInit ( ANSC_HANDLE hContext, PCOSA_DATAMODEL_RDKB_UIBRANDING PUiBrand, PCOSA_DATAMODEL_RDKB_CDLDM PCdlDM )
+{
+    CcspTraceWarning(("%s-%d : UI Branding is not supported... \n" , __FUNCTION__, __LINE__ ));
+
+    return ANSC_STATUS_FAILURE;
+}
+
+ANSC_STATUS UpdateJsonParam ( char *pKey, char *PartnerId, char *pValue, char *pSource, char *pCurrentTime )
+{
+    CcspTraceWarning(("%s-%d : Skip updating to Partner JSON file... \n" , __FUNCTION__, __LINE__));
+
+    return ANSC_STATUS_FAILURE;
+}
+
+#endif // #ifdef USE_PARTNER_ID
+
 static int writeToJson(char *data, char *file)
 {
     if (file == NULL || data == NULL)
@@ -5443,6 +5470,8 @@ EndRfcProcessing(cJSON **pRfcStore)
    return ANSC_STATUS_SUCCESS;
 }
 
+#ifdef USE_PARTNER_ID
+
 #define MAX_NTP_SERVER 5
 
 ANSC_STATUS
@@ -5564,6 +5593,9 @@ ApplyNTPPartnerDefaults()
       free(data);
     return ANSC_STATUS_FAILURE;
 }
+
+#endif // #ifdef USE_PARTNER_ID
+
 ANSC_STATUS
 CosaDmlSetnewNTPEnable(BOOL bValue)
 {
@@ -5575,8 +5607,10 @@ CosaDmlSetnewNTPEnable(BOOL bValue)
              AnscTraceWarning(("syscfg_set failed for new_ntp_enabled\n"));
              return ANSC_STATUS_FAILURE;
          } 
+#ifdef USE_PARTNER_ID
          if( ANSC_STATUS_SUCCESS != ApplyNTPPartnerDefaults() )
              return ANSC_STATUS_FAILURE;
+#endif
      }
      else
      {
