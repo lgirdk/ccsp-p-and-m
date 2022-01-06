ff823ee40f70d4337c0a5dd69a3918fe6c9ce56b
SreejaSKumar <srekumar.contractor@libertyglobal.com>
2022-01-06 11:33:56 +0530

    drop unused Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.FWUpdate.AutoExcluded. objects ( OFW-2256 )
    [PERFORMANCE] Remove unused datamodel from PAM
    
    BUG DESCRIPTION:
    Remove the unused datamodel from tr069 XML, dml files and default values.
    
    Parameters Removed:
    
      Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.FWUpdate.AutoExcluded.Enable
      Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.FWUpdate.AutoExcluded.XconfUrl
    
    FIX DESCRIPTION:
    Removed the unused parameter from the tr069 XML dml files and default values.
    
    TESTING PERFORMED:
    Checked the parameters by checking dmcli and also in ACS.
    
    Signed-off-by: Sreeja S Kumar (srekumar.contractor@libertyglobal.com)

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.h
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.h
@@ -1545,51 +1545,6 @@ MTLS_SetParamBoolValue
     );
 
 /***********************************************************************
- *
- APIs for Object:
-
-    Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.FWUpdate.AutoExcluded
-
-    *  AutoExcluded_GetParamBoolValue
-    *  AutoExcluded_SetParamBoolValue
-    *  AutoExcluded_GetParamStringValue
-    *  AutoExcluded_SetParamStringValue
-***********************************************************************/
-BOOL
-AutoExcluded_GetParamBoolValue
-
-    (
-        ANSC_HANDLE                 hInsContext,
-        char*                       ParamName,
-        BOOL*                       pBool
-    );
-
-BOOL
-AutoExcluded_SetParamBoolValue
-    (
-        ANSC_HANDLE                 hInsContext,
-        char*                       ParamName,
-        BOOL                        bValue
-    );
-
-ULONG
-AutoExcluded_GetParamStringValue
-(
- ANSC_HANDLE                 hInsContext,
- char*                       ParamName,
- char*                       pValue,
- ULONG*                      pUlSize
-);
-
-BOOL
-AutoExcluded_SetParamStringValue
-(
- ANSC_HANDLE                 hInsContext,
- char*                       ParamName,
- char*                       pString
-);
-
-/***********************************************************************
 
  APIs for Object:
 
