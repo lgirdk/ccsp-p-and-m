9fafdd664f2fc9a4ddc7e28553637ede3bbff41c
parth <parth.gulati@infosys.com>
2022-01-07 22:59:49 +0530

    update Device.GRE.Tunnel.1.DefaultDSCPMark data type integer -> unsigned integer ( OFW-2081 )
    Fix SPV on WiFi params fail with FaultString "Invalid Parameter Value"
    
    BUG DESCRIPTION
    Due to wrong data type for  parameter, SPV was not working
    
    FIX DESCRIPTION
    Data type was changed from integer to unsigned integer.
    
    TESTING PERFORMED
    console testing
    dmcli eRT setv Device.GRE.Tunnel.1.DefaultDSCPMark uint 32
    dmcli eRT getv Device.GRE.Tunnel.1.DefaultDSCPMark
    checked in ACS as well

--- custom/comcast/source/TR-181/custom_ml/cosa_x_comcast-com_gre_dml.c
+++ custom/comcast/source/TR-181/custom_ml/cosa_x_comcast-com_gre_dml.c
@@ -269,6 +269,11 @@ BOOL GreTunnel_GetParamUlongValue ( ANSC_HANDLE hInsContext, char* ParamName, UL
     COSA_DML_GRE_TUNNEL                 *pGreTu      = (COSA_DML_GRE_TUNNEL *)hInsContext;
     ULONG                           ins = pGreTu->InstanceNumber;
 
+    if (strcmp(ParamName, "DSCPMarkPolicy") == 0)
+    {
+        *pUlong = pGreTu->DSCPMarkPolicy;
+        return TRUE;
+    }
     if (strcmp(ParamName, "Status") == 0)
     {
         if (CosaDml_GreTunnelGetStatus(ins, (COSA_DML_GRE_STATUS *)pUlong) != ANSC_STATUS_SUCCESS)
@@ -454,12 +459,6 @@ BOOL GreTunnel_GetParamIntValue ( ANSC_HANDLE hInsContext, char* ParamName, int*
     COSA_DML_GRE_TUNNEL                 *pGreTu      = (COSA_DML_GRE_TUNNEL *)hInsContext;
     //ULONG                           ins = pGreTu->InstanceNumber;
 
-    if (strcmp(ParamName, "DSCPMarkPolicy") == 0)
-    {
-        *pInt = pGreTu->DSCPMarkPolicy;
-        return TRUE;
-    }   
-
     return FALSE;
 }
 
@@ -550,6 +549,15 @@ BOOL GreTunnelIf_SetParamBoolValue ( ANSC_HANDLE hInsContext, char* ParamName, B
 BOOL GreTunnel_SetParamUlongValue ( ANSC_HANDLE  hInsContext, char* ParamName, ULONG  uValue) {
 	COSA_DML_GRE_TUNNEL                 *pGreTu      = (COSA_DML_GRE_TUNNEL *)hInsContext;
 
+    if (strcmp(ParamName, "DSCPMarkPolicy") == 0)
+    {
+        if(pGreTu->DSCPMarkPolicy == uValue)
+            return TRUE;
+
+        pGreTu->DSCPMarkPolicy = uValue;
+        pGreTu->ChangeFlag |= GRETU_CF_DSCP;
+        return TRUE;
+    }
     if (strcmp(ParamName, "KeyIdentifierGenerationPolicy") == 0)
     {
         pGreTu->KeyIdentifierGenerationPolicy = uValue;
@@ -739,16 +747,6 @@ BOOL GreTunnelIf_SetParamStringValue ( ANSC_HANDLE hInsContext, char*  ParamName
 BOOL GreTunnel_SetParamIntValue ( ANSC_HANDLE hInsContext, char* ParamName, int value ) {
 	COSA_DML_GRE_TUNNEL                 *pGreTu      = (COSA_DML_GRE_TUNNEL *)hInsContext;
 
-    if (strcmp(ParamName, "DSCPMarkPolicy") == 0)
-    {
-        if(pGreTu->DSCPMarkPolicy == value)
-            return TRUE;
-
-        pGreTu->DSCPMarkPolicy = value;
-        pGreTu->ChangeFlag |= GRETU_CF_DSCP;
-        return TRUE;
-    }
-    
     return FALSE;
 }
 
