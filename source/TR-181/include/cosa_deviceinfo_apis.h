16243b7cb2a624a5a57449a3951472b7baadddb3
SreejaSKumar <srekumar.contractor@libertyglobal.com>
2022-01-20 19:30:34 +0530

    drop unused Device.DeviceInfo.X_RDKCENTRAL-COM.Ops. etc objects ( OFW-2311 )
    [PERFORMANCE] Remove unused datamodel from PAM
    
    BUG DESCRIPTION:
    Remove the unused datamodel from tr069 XML, dml files and default values.
    
    Parameters Removed:
    
      Device.DeviceInfo.X_RDKCENTRAL-COM.Ops.LogsUploadStatus
      Device.DeviceInfo.X_RDKCENTRAL-COM.Ops.UploadLogsNow
      Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.DFSatBootUp.Enable
      Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.IPv6onMoCA.Enable
    
    FIX DESCRIPTION:
    Removed the unused parameter from the tr069 XML dml files and default values.
    
    TESTING PERFORMED:
    Checked the parameters by checking dmcli and also in ACS.
    
    Signed-off-by: Sreeja S Kumar (srekumar.contractor@libertyglobal.com)

--- source/TR-181/include/cosa_deviceinfo_apis.h
+++ source/TR-181/include/cosa_deviceinfo_apis.h
@@ -689,21 +689,6 @@ CosaDmlSetnewNTPEnable(BOOL bValue);
 
 BOOL CosaDmlGetInternetStatus();
 
-ANSC_STATUS
-COSADmlUploadLogsStatus
-    (
-        ANSC_HANDLE                 Context,
-        char*   pValue,
-        ULONG*  pUlSize
-    );
-
-ANSC_STATUS
-COSADmlUploadLogsNow
-        (
-                ANSC_HANDLE                 hContext,
-                BOOL                        bEnable
-        );
-
 void CosaDmlPresenceEnable(BOOL enable);
 
 
@@ -787,6 +772,5 @@ CosaDmlSetNativeHostapdState
 #endif //FEATURE_HOSTAP_AUTHENTICATOR
 
 BOOL CosaDmlSetDFS(BOOL bValue);
-BOOL CosaDmlSetDFSatBootUp(BOOL bValue);
 
 #endif
