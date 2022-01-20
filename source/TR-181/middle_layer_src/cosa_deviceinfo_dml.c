6ff4f24d71928ed845dbededc71d0d81735735e5
Ajay Vishnu Bhat <abhat@libertyglobal.com>
2022-01-20 03:15:51 +0100

    implement Device.DeviceInfo.X_LGI-COM_ProvisioningCodeSource ( OFW-2297 F3896SI-698 )
    Implement MVXREQ-2289 for ProvisioningCode.
    
    [Issue]
    The DM Device.DeviceInfo.ProvisioningCode needs to be based on X_LGI-COM_ProvisioningCodeSource based on CR080.
    
    [Description]
    Currently the ProvisioningCode DM is set to SerialNumber of the device if nothing is set and also accepts string values but doesn't hold values based on X_LGI-COM_ProvisioningCodeSource. The X_LGI-COM_ProvisioningCodeSource is not defined. It should also accept a direct SPV and retain that value unless either of X_LGI-COM_ProvisioningCodeSource or ProvisioningCode is set.
    
    [Solution]
    Implement the DM X_LGI-COM_ProvisioningCodeSource and also define ProvisioningCode based on CR080. Also default the value of ProvisioningCode to CM MAC for Sunrise SKU.
    
    [Testing Performed]
    1. Query the Device.DeviceInfo.ProvisioningCode DM.
    2. Verify that the value is the SerialNumber of the gateway.
    3. Change the value of Device.DeviceInfo.ProvisioningCode.
    4. Reboot the gateway.
    5. Ensure that the DM retains its value.
    6. Change the value of Device.DeviceInfo.X_LGI-COM_ProvisioningCodeSource.
    7. Make sure the value of Device.DeviceInfo.ProvisioningCode DM is updated accordingly.
    8. Reboot the gateway.
    9. The value of the DMs should be retained.
    
    Signed-off-by: Ajay Vishnu Bhat <abhat@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
@@ -639,6 +639,7 @@ DeviceInfo_GetParamUlongValue
         ULONG*                      puLong
     )
 {
+    PCOSA_DATAMODEL_DEVICEINFO      pMyObject = (PCOSA_DATAMODEL_DEVICEINFO)g_pCosaBEManager->hDeviceInfo;
     UNREFERENCED_PARAMETER(hInsContext);
     /* check the parameter name and return the corresponding value */
     if (strcmp(ParamName, "UpTime") == 0)
@@ -690,7 +691,13 @@ DeviceInfo_GetParamUlongValue
         CosaDmlDiGetFactoryResetCount(NULL,puLong);
         return TRUE;
     }
-	
+
+    if(strcmp(ParamName, "X_LGI-COM_ProvisioningCodeSource") == 0)
+    {
+        *puLong = pMyObject->ProvisioningCodeSource;
+        return TRUE;
+    }
+
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
 }
@@ -1319,6 +1326,7 @@ DeviceInfo_SetParamUlongValue
         ULONG                       uValue
     )
 {
+    PCOSA_DATAMODEL_DEVICEINFO      pMyObject = (PCOSA_DATAMODEL_DEVICEINFO)g_pCosaBEManager->hDeviceInfo;
     UNREFERENCED_PARAMETER(hInsContext);
     /* check the parameter name and set the corresponding value */
     if (strcmp(ParamName, "X_RDKCENTRAL-COM_ConfigureDocsicPollTime") == 0)
@@ -1338,6 +1346,14 @@ DeviceInfo_SetParamUlongValue
  	   fclose(fp);
 	   return TRUE;
     } 
+
+    if (strcmp(ParamName, "X_LGI-COM_ProvisioningCodeSource") == 0)
+    {
+        pMyObject->ProvisioningCodeSource = uValue;
+        CosaDmlDiSetProvisioningCodeSource(NULL, pMyObject->ProvisioningCodeSource);
+        return TRUE;
+    }
+
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
 }
@@ -1879,6 +1895,8 @@ DeviceInfo_SetParamStringValue
               ERR_CHK(rc);
                return FALSE;
           }
+
+        CosaDmlDiSetProvisioningCode(NULL, (char *)pMyObject->ProvisioningCode);
         return TRUE;
     }
 #ifdef CONFIG_INTERNET2P0
@@ -2271,10 +2289,6 @@ DeviceInfo_Commit
     )
 {
     UNREFERENCED_PARAMETER(hInsContext);
-    PCOSA_DATAMODEL_DEVICEINFO      pMyObject = (PCOSA_DATAMODEL_DEVICEINFO)g_pCosaBEManager->hDeviceInfo;
-
-    CosaDmlDiSetProvisioningCode(NULL, (char *)pMyObject->ProvisioningCode);
-
     return 0;
 }
 
@@ -2312,6 +2326,7 @@ DeviceInfo_Rollback
     ULONG pulSize = 0;
     /*CID:78739 Out-of-bounds access - updted the ProvisioningCode with 256 in the declaration*/
     CosaDmlDiGetProvisioningCode(NULL,(char *)pMyObject->ProvisioningCode, &pulSize);
+    CosaDmlDiGetProvisioningCodeSource(NULL, &pMyObject->ProvisioningCodeSource);
     
     return 0;
 }
