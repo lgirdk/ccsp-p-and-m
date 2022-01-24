63bd6ee5534f4cc66dcdbc08e4b6de32c83a04a0
SreejaSKumar <srekumar.contractor@libertyglobal.com>
2022-01-24 16:35:09 +0530

    drop unused Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SyndicationFlowControl. objects ( OFW-2311 )
    [PERFORMANCE] Remove unused datamodel from PAM
    
    BUG DESCRIPTION:
    Remove the unused datamodel from tr069 XML, dml files and default values.
    
    Parameters Removed:
    
      Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SyndicationFlowControl.Enable
      Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SyndicationFlowControl.InitialOutputMark
      Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SyndicationFlowControl.InitialForwardedMark
    
    FIX DESCRIPTION:
    Removed the unused parameter from the tr069 XML dml files and default values.
    
    TESTING PERFORMED:
    Checked the parameters by checking dmcli and also in ACS.
    
    Signed-off-by: Sreeja S Kumar (srekumar.contractor@libertyglobal.com)

--- source/TR-181/include/cosa_deviceinfo_apis.h
+++ source/TR-181/include/cosa_deviceinfo_apis.h
@@ -227,16 +227,6 @@ _COSA_DATAMODEL_RDKB_LATENCYMEASURE_TCP_IPV6_CLASS_CONTENT
 COSA_DATAMODEL_RDKB_LATENCYMEASURE_TCP_IPV6, *PCOSA_DATAMODEL_RDKB_LATENCYMEASURE_TCP_IPV6;
 
 typedef  struct
-_COSA_DATAMODEL_RDKB_SYNDICATIONFLOWCONTROL_CLASS_CONTENT
-{
-    COSA_BOOTSTRAP_BOOL    Enable;
-    COSA_BOOTSTRAP_STR    InitialForwardedMark;
-    COSA_BOOTSTRAP_STR    InitialOutputMark;
-}
-COSA_DATAMODEL_RDKB_SYNDICATIONFLOWCONTROL, *PCOSA_DATAMODEL_RDKB_SYNDICATIONFLOWCONTROL;
-
-
-typedef  struct
     _COSA_DATAMODEL_KICKSTARTTABLE
 {
     UINT    SecurityNumberLen;
@@ -692,24 +682,6 @@ BOOL CosaDmlGetInternetStatus();
 void CosaDmlPresenceEnable(BOOL enable);
 
 
-ANSC_STATUS
-CosaDmlDiSet_SyndicationFlowControl_Enable
-    (
-        char bValue
-    );
-
-ANSC_STATUS
-CosaDmlDiSet_SyndicationFlowControl_InitialForwardedMark
-    (
-        char *pString
-    );
-
-ANSC_STATUS
-CosaDmlDiSet_SyndicationFlowControl_InitialOutputMark
-    (
-        char *pString
-    );
-
 ANSC_STATUS 
 setTempPartnerId
     (
@@ -727,12 +699,6 @@ ANSC_STATUS
 CosaDeriveSyndicationPartnerID
     (
         char *Partner_ID
-    );
-
-ANSC_STATUS
-CosaDmlDiSyndicationFlowControlInit
-    (
-        PCOSA_DATAMODEL_RDKB_SYNDICATIONFLOWCONTROL pSyndicatonFlowControl
     );
 
 ANSC_STATUS
