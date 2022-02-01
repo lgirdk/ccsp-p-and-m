7463cc3ef381f65ffa13fc9e83f9c2f40e7ce4c8
Andre McCurdy <armccurdy@gmail.com>
2022-01-31 22:06:10 -0800

    drop references to EnableTR69Binary syscfg value ( tr069 is always enabled )
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/PandMSsp/ssp_main.c
+++ source/PandMSsp/ssp_main.c
@@ -759,16 +759,6 @@ if(id != 0)
     }
 #endif
 
-#if !defined(_COSA_INTEL_XB3_ARM_)
-    char buf[64] = {'\0'};
-    if(syscfg_get( NULL, "EnableTR69Binary", buf, sizeof(buf))==0)
-    {
-         if (strncmp(buf, "false", 5) == 0)
-             /*check this file during Tr069 service start*/
-             v_secure_system("touch /tmp/disableTr069");
-    }
-#endif
-	
     if ( bRunAsDaemon )
     {
        if (bDebugSlowChildProcess)
