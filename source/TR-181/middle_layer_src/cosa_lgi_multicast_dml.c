fe020829290ca14308bd5251ce88ccba2eac1893
Devin Mungan <dmungan@libertyglobal.com>
2022-02-21 11:27:11 +0100

    add data model support for Device.X_LGI-COM_Multicast.IGMPv3ProxyEnable ( OFW-2446 )
    [Issue]
    Multicast submodule of PAM should be enhanced to support set/commit/validate/rollback
    capabilities.
    
    [Solution]
    - add cache mechanism for the multicast config
    - add set/commit/validate/rollback functions
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)
    
    [OFW-2446] Add data model - Device.X_LGI-COM_Multicast.IGMPv3ProxyEnable
    
    [Issue]
    Implement set/get for IGMPv3ProxyEnable and take the necessary actions if the
    parameter changes (enable/disable mcproxy_v4)
    
    [Solution]
    - add cache mechanism for the multicast config
    - add set/commit/validate/rollback functions
    
    [Testing Performed]
    1. Enable DSLite and verify that mcproxy_v4 is not running
    2. Disable DSLite and verify that mcproxy_v4 is running
    3. verify that mcproxy_v4 is running when erouter mode is "ipv4Only" or "ipv4AndIpv6"
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)

--- source/TR-181/middle_layer_src/cosa_lgi_multicast_dml.c
+++ source/TR-181/middle_layer_src/cosa_lgi_multicast_dml.c
@@ -23,19 +23,81 @@
 #include "cosa_drg_common.h"
 #include <syscfg/syscfg.h>
 
-BOOL LgiMulticast_GetParamBoolValue ( ANSC_HANDLE hInsContext, char* ParamName, BOOL* pBool )
+BOOL LgiMulticast_GetParamBoolValue ( ANSC_HANDLE hInsContext, char* ParamName, BOOL* pValue )
 {
+    PCOSA_DATAMODEL_LGI_MULTICAST pMyObject = (PCOSA_DATAMODEL_LGI_MULTICAST) g_pCosaBEManager->hLgiMulticast;
+
     if (strcmp (ParamName, "Enable") == 0)
     {
-        CosaDmlMulticastGetEnable(NULL, pBool);
+        CosaDmlMulticastGetEnable(NULL, &pMyObject->Cfg.bEnable);
+        *pValue = pMyObject->Cfg.bEnable;
         return TRUE;
     }
 
     if (strcmp(ParamName, "SnoopingEnable") == 0)
     {
-        CosaDmlMulticastGetSnoopingEnable(NULL, pBool);
+        CosaDmlMulticastGetSnoopingEnable(NULL, &pMyObject->Cfg.bSnoopingEnable);
+        *pValue = pMyObject->Cfg.bSnoopingEnable;
+        return TRUE;
+    }
+
+    if (strcmp(ParamName, "IGMPv3ProxyEnable") == 0)
+    {
+        CosaDmlMulticastGetIGMPv3ProxyEnable(NULL, &pMyObject->Cfg.bIGMPv3ProxyEnable);
+        *pValue = pMyObject->Cfg.bIGMPv3ProxyEnable;
         return TRUE;
     }
 
     return FALSE;
+}
+
+BOOL LgiMulticast_SetParamBoolValue ( ANSC_HANDLE hInsContext, char* ParamName, BOOL bValue )
+{
+    PCOSA_DATAMODEL_LGI_MULTICAST pMyObject = (PCOSA_DATAMODEL_LGI_MULTICAST) g_pCosaBEManager->hLgiMulticast;
+
+    if (strcmp(ParamName, "IGMPv3ProxyEnable") == 0)
+    {
+        pMyObject->Cfg.bIGMPv3ProxyEnable = bValue;
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+BOOL LgiMulticast_Validate ( ANSC_HANDLE hInsContext, char* pReturnParamName, ULONG* puLength )
+{
+    return TRUE;
+}
+
+ULONG LgiMulticast_Commit ( ANSC_HANDLE hInsContext )
+{
+    PCOSA_DATAMODEL_LGI_MULTICAST pMyObject = (PCOSA_DATAMODEL_LGI_MULTICAST) g_pCosaBEManager->hLgiMulticast;
+
+    if (IS_PARAM_CHANGED(pMyObject, bIGMPv3ProxyEnable))
+    {
+        CosaDmlMulticastSetIGMPv3ProxyEnable(NULL, pMyObject->Cfg.bIGMPv3ProxyEnable);
+    }
+    if (IS_IPV4PROXY_RESTART_NEEDED(pMyObject))
+    {
+        system("/etc/utopia/service.d/service_mcastproxy.sh mcastproxy-restart &");
+    }
+    /* commit the changes and cache the new config */
+    if (IS_CFG_CHANGED(pMyObject))
+    {
+        syscfg_commit();
+        CACHE_CFG(pMyObject);
+    }
+
+    return 0;
+}
+
+ULONG LgiMulticast_Rollback ( ANSC_HANDLE hInsContext )
+{
+    PCOSA_DATAMODEL_LGI_MULTICAST pMyObject = (PCOSA_DATAMODEL_LGI_MULTICAST) g_pCosaBEManager->hLgiMulticast;
+
+    CosaDmlMulticastGetEnable(NULL, &pMyObject->Cfg.bEnable);
+    CosaDmlMulticastGetSnoopingEnable(NULL, &pMyObject->Cfg.bSnoopingEnable);
+    CosaDmlMulticastGetIGMPv3ProxyEnable(NULL, &pMyObject->Cfg.bIGMPv3ProxyEnable);
+
+    return 0;
 }
