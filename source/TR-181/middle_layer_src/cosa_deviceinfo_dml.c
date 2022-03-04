30abedc947be1e6e41fc13f4f319c7e37133cdfb
mkamat <mkamate@libertyglobal.com>
2022-03-04 10:45:17 -0800

    If XXX_GetParamStringValue() returns success(0) then it should return a valid pValue buffer

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
@@ -748,6 +748,7 @@ DeviceInfo_GetParamStringValue
     ULONG                           ReturnValue;
     errno_t                         rc        = -1;
     
+     strcpy(pValue, "");
     /* check the parameter name and return the corresponding value */
 
     if (strcmp(ParamName, "DeviceCategory") == 0)
@@ -3621,6 +3622,8 @@ VendorConfigFile_GetParamStringValue
     UNREFERENCED_PARAMETER(hInsContext);
     UNREFERENCED_PARAMETER(pValue);
     UNREFERENCED_PARAMETER(pUlSize);
+    
+    strcpy(pValue, "");
     /* check the parameter name and return the corresponding value */
     if (strcmp(ParamName, "Alias") == 0)
     {
@@ -4239,6 +4242,8 @@ SupportedDataModel_GetParamStringValue
     UNREFERENCED_PARAMETER(hInsContext);
     UNREFERENCED_PARAMETER(pValue);
     UNREFERENCED_PARAMETER(pUlSize);
+    
+    strcpy(pValue, "");
     /* check the parameter name and return the corresponding value */
     if (strcmp(ParamName, "URL") == 0)
     {
