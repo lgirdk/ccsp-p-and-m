d495a9bec57f8dd44c24d409d5e719c2b0f1b68e
mkamat <mkamate@libertyglobal.com>
2022-03-04 10:45:17 -0800

    If XXX_GetParamStringValue() returns success(0) then it should return a valid pValue buffer

--- source/TR-181/middle_layer_src/cosa_bridging_dml.c
+++ source/TR-181/middle_layer_src/cosa_bridging_dml.c
@@ -5872,6 +5872,8 @@ Filter_GetParamStringValue
     UNREFERENCED_PARAMETER(hInsContext);
     UNREFERENCED_PARAMETER(pValue);
     UNREFERENCED_PARAMETER(pUlSize);
+
+    strcpy(pValue, "");
     if (strcmp(ParamName, "Alias") == 0)
     {
         /* collect value */
