06d969f7aadbc7a093102ead74b6927cd0b7820a
Sandipan Sinha <ssinha.contractor@libertyglobal.com>
2022-03-17 08:12:47 +0100

    start_lighttpd.sh: add url redirection when Captive Portal is enabled ( OFW-1775 )
    [Issue]
    When Device.X_LGI-COM_General.FirstInstallRedirectAll is true, http
    traffic not redirecting to local UI.
    
    [Solution]
    - In script start_lighttpd.sh, introducing condition for redirection of http traffic
      to Local UI using mod-redirect of lighttpd, whenever captive portal is enabled.
    
    [Testing Performed]
    - If Device.X_LGI-COM_General.FirstInstallRedirectAll is true, http traffic redirects to local UI.
    
    Signed-off-by: Sandipan Sinha <ssinha.contractor@libertyglobal.com>
    
    start_lighttpd.sh: drop support for creating gateway.conf symlink
    
    gateway.conf is now included directly within lighttpd.conf and is no
    longer a separate file.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- arch/intel_usg/boards/arm_shared/scripts/start_lighttpd.sh
+++ arch/intel_usg/boards/arm_shared/scripts/start_lighttpd.sh
@@ -14,16 +14,42 @@ lighttpd_start()
 		/bin/kill ${LIGHTTPD_PID}
 	fi
 
+	CAPTIVE_PORTAL_ENABLE=$(syscfg get CaptivePortal_Enable)
+
 	if [ "$(syscfg get cloud_enable_flag)" = "1" ]
 	then
-		if [ ! -f /var/lighttpd.conf ]
-		then
-			sed 's|^server\.document-root .*|server.document-root = "/www/cloud/"|' < /etc/lighttpd/lighttpd.conf > /var/lighttpd.conf
-			ln -sf /etc/lighttpd/gateway.conf /var/gateway.conf
-		fi
+		sed 's|^server\.document-root .*|server.document-root = "/www/cloud/"|' < /etc/lighttpd/lighttpd.conf > /var/lighttpd.conf
 		CONF_FILE="/var/lighttpd.conf"
+
+	elif [ "$CAPTIVE_PORTAL_ENABLE" = "true" ]
+	then
+		cp /etc/lighttpd/lighttpd.conf /var/lighttpd.conf
+		CONF_FILE="/var/lighttpd.conf"
+
 	else
 		CONF_FILE="/etc/lighttpd/lighttpd.conf"
+	fi
+
+	if [ "$CONF_FILE" = "/var/lighttpd.conf" ]
+	then
+		if [ "$CAPTIVE_PORTAL_ENABLE" = "true" ]
+		then
+			LAN_IP=$(syscfg get lan_ipaddr)
+
+			{
+			echo
+			echo '$HTTP["scheme"] == "http" {'
+			echo '  $HTTP["host"] == "'"$LAN_IP"'" {'
+			echo '    $HTTP["url"] =~ "'"$LAN_IP"'" {'
+			echo '    }'
+			echo '  }'
+			echo '  else $HTTP["url"] !~ "'"$LAN_IP"'" {'
+			echo '    url.redirect = ("" => "http://'"$LAN_IP"'/")'
+			echo '    url.redirect-code = 302'
+			echo '  }'
+			echo '}'
+			} >> $CONF_FILE
+		fi
 	fi
 
 	if [ "${LOCAL_UI_ENABLE}" = "true" ]
