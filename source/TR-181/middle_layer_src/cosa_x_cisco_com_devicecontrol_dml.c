18f75abbf0bb268a3bf40de87ff6bc5d986fcdc3
Saikiran Vanamala <svanamala@libertyglobal.com>
2022-03-31 15:29:33 +0200

    implement Device.X_LGI-COM_DeviceControl.SSH.Enable and Device.X_LGI-COM_DeviceControl.SSH.SessionTimeout ( OFW-2569 )
    Specifying SSH session timeout and Support to disable SSH when not used for 24 hours after activation.
    
    Description:
    Added neccessary changes for service_sshd.sh script
        If SessionTimeout is set as infinite then DM should get disable after 24 hours if no ssh connection.If ssh connection is established then after 24 hours, DM should get deactivate.
        If SessionTimeout is set as any integer then DM should deactivate after mentioned timeout.
    
    Testing performed.
    1. Enable SSH through Device.X_LGI-COM_DeviceControl.SSH.Enable
    2. Set SessionTimeout as 1800.
    3. Check dropbear process within 30 mins and atttempt SSH connection on erouterip. SSH connection should be established . After timeout,Dropbear process should get killed and unable to attempt ssh.
    4. Set SessionTimeout as 0
    5. Check dropbear process within 24 hours and attempt SSH connection on erouterip. SSH connection should be established . After timeout,Dropbear process should get killed and unable to attempt ssh.
    
    Signed-off-by: Saikiran Vanamala <svanamala@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_x_cisco_com_devicecontrol_dml.c
+++ source/TR-181/middle_layer_src/cosa_x_cisco_com_devicecontrol_dml.c
@@ -306,6 +306,8 @@ X_CISCO_COM_DeviceControl_GetParamBoolValue
      */
     if (strcmp(ParamName, "Enable") == 0)
     {      
+        if (CosaDmlDcGetSSHEnable(NULL, pBool) != ANSC_STATUS_SUCCESS)
+            return FALSE;
         return TRUE;
     }
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
@@ -531,6 +533,14 @@ X_CISCO_COM_DeviceControl_GetParamUlongValue
         /* collect value */
         *puLong = CosaDmlGetMocaHardwareStatus(NULL);
         return TRUE;
+    }
+
+    if (strcmp(ParamName, "SessionTimeout") == 0)
+    {
+        if (CosaDmlDcGetSSHSessionTimeout(NULL,puLong) != ANSC_STATUS_SUCCESS)
+            return FALSE;
+
+       return TRUE;
     }
 
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
@@ -913,8 +923,13 @@ X_CISCO_COM_DeviceControl_SetParamBoolValue
      */    
     if (strcmp(ParamName, "Enable") == 0)
     {
-      fprintf(stderr,"\n %s %d  *** TODO ***",__func__,__LINE__);
-      return TRUE;
+       pMyObject->Enable = bValue;
+
+       retStatus = CosaDmlDcSetSSHEnable(NULL, pMyObject->Enable);
+       if (retStatus != ANSC_STATUS_SUCCESS)
+           return FALSE;
+
+       return TRUE;
     }
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
@@ -1170,6 +1185,17 @@ X_CISCO_COM_DeviceControl_SetParamUlongValue
         return TRUE;
     }
  
+    if (strcmp(ParamName, "SessionTimeout") == 0)
+    {
+        pMyObject->SessionTimeout = uValue;
+
+        retStatus = CosaDmlDcSetSSHSessionTimeout(NULL, pMyObject->SessionTimeout);
+        if (retStatus != ANSC_STATUS_SUCCESS)
+            return FALSE;
+
+        return TRUE;
+    }
+
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return FALSE;
 }
