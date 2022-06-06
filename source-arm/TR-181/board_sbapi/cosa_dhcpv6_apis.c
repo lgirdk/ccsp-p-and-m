9b640f553c1b4f2c52f8e6e9f88947a4a63f072c
Dhanya <dravindran.contractor@libertyglobal.com>
2022-06-06 04:08:17 +0200

    fix Preferred and Valid Lifetimes are invalid for DHCPv6 datamodel objects ( OFW-2773 F3896SI-1066 )
    [Description]
    In the dibbler code, time stamp set using CLOCK_MONOTONIC clock,
    which doesn't give the actual time but gives the monotonic time since
    some unspecified starting point.
    
    [Solution]
    Added code to convert monotonic time to actual time.
    
    [Testing Performed]
    Connected ipv6 lanclent to the device and verifed that following
    parameters have valid values:
    
      Device.DHCPv6.Server.Pool.1.Client.1.IPv6Address.1.PreferredLifetime
      Device.DHCPv6.Server.Pool.1.Client.1.IPv6Address.1.ValidLifetime
    
    Signed-off-by: Dhanya Ravindran<dravindran.contractor@libertyglobal.com>

--- source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
@@ -6917,6 +6917,8 @@ void _cosa_dhcpsv6_get_client()
     PCOSA_DML_DHCPSV6_CLIENT_OPTION          pOption1 = NULL;
     PCOSA_DML_DHCPSV6_CLIENT_IPV6ADDRESS     pIPv6Address1 = NULL;
     PCOSA_DML_DHCPSV6_CLIENT_IPV6PREFIX      pIPv6Prefix1  = NULL;
+    struct timespec monotonic_time;
+    time_t monotonic_offset;
 
     /* When read error, we keep current configuration */
     if ( fp == NULL )
@@ -6945,6 +6947,9 @@ void _cosa_dhcpsv6_get_client()
         
         g_dhcps6v_client_num = 0;
     }
+
+    clock_gettime(CLOCK_MONOTONIC, &monotonic_time);
+    monotonic_offset = time(NULL) - monotonic_time.tv_sec;
 
     /* Get all from file again */
     while ( fgets(oneLine, sizeof(oneLine), fp ) != NULL )
@@ -7135,6 +7140,7 @@ void _cosa_dhcpsv6_get_client()
                 }
                 else
                 {
+                    t1 += monotonic_offset;
                     localtime_r(&t1, &t2);
                     snprintf(buf, sizeof(buf), "%d-%d-%dT%02d:%02d:%02dZ%c", 
                          1900+t2.tm_year, t2.tm_mon+1, t2.tm_mday,
@@ -7159,6 +7165,7 @@ void _cosa_dhcpsv6_get_client()
                 }
                 else
                 {
+                    t1 += monotonic_offset;
                     localtime_r(&t1, &t2);
                     snprintf(buf, sizeof(buf), "%d-%d-%dT%02d:%02d:%02dZ%c", 
                          1900+t2.tm_year, t2.tm_mon+1, t2.tm_mday,
@@ -7214,6 +7221,7 @@ void _cosa_dhcpsv6_get_client()
                 }
                 else
                 {
+                    t1 += monotonic_offset;
                     localtime_r(&t1, &t2);
                     snprintf(buf, sizeof(buf), "%d-%d-%dT%02d:%02d:%02dZ%c", 
                          1900+t2.tm_year, t2.tm_mon+1, t2.tm_mday,
@@ -7238,6 +7246,7 @@ void _cosa_dhcpsv6_get_client()
                 }
                 else
                 {
+                    t1 += monotonic_offset;
                     localtime_r(&t1, &t2);
                     snprintf(buf, sizeof(buf), "%d-%d-%dT%02d:%02d:%02dZ%c", 
                          1900+t2.tm_year, t2.tm_mon+1, t2.tm_mday,
