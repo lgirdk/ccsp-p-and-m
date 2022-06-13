b82f01d8c1122c9c916b1bda7af2dd35c1a6a35c
Cibil <cpankiras.contractor@libertyglobal.com>
2022-06-13 14:29:17 +0200

    ddns: remove easydns ddns service provider
    As per MVXREQ-942, below are the supported ddns service providers
    
    The supported DDNS providers MUST the one from the list below:
    www.noip.com/www.no-ip.com
    www.dyn.com (previously www.dyndns.org)
    www.duckdns.org
    www.freedns.afraid.org
    www.changeip.com
    
    Upstream Candidate: No. LGI-specific DDNS requirement MVXREQ-942.
    
    Signed-off-by: Cibil <cpankiras.contractor@libertyglobal.com>

--- source-arm/TR-181/board_sbapi/cosa_dynamicdns_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dynamicdns_apis.c
@@ -95,13 +95,6 @@ DDNS_SERVICE gDdnsServices[] =
         "www.freedns.afraid.org"
     },
     {
-        "easydns",
-        "EasyDNS.com",
-        "HTTPS",
-        "HTTPS",
-        "www.easydns.com"
-    },
-    {
         "changeip",
         "ChangeIP.com",
         "HTTP",
