504264d5ab3c4e673a098b7fb517afc13eee401a
Tarun Saha <tsaha@libertyglobal.com>
2022-06-30 16:29:36 -0700

    drop dependency on platform_hal_GetSSHEnable() / platform_hal_SetSSHEnable() ( OFW-2925 )
    Fix unable to set enable/disable SSH functionality.
    
    Removed redundant HAL API call to set/get SSH status, relying on syscfg
    data only.

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
@@ -2723,10 +2723,8 @@ CosaDmlDcGetSSHEnable
         else
             *pFlag = 0;
     }
-    if (platform_hal_GetSSHEnable(pFlag) != RETURN_OK )
-        return ANSC_STATUS_FAILURE;
-    else
-        return ANSC_STATUS_SUCCESS;
+
+    return ANSC_STATUS_SUCCESS;
 }
 
 ANSC_STATUS
@@ -2784,9 +2782,6 @@ CosaDmlDcSetSSHEnable
                             "sysevent set sshd-stop ; "
                             "killall dropbear");
         }
-
-        if (platform_hal_SetSSHEnable(flag) == RETURN_ERR )
-            return ANSC_STATUS_FAILURE;
     }   
     return ANSC_STATUS_SUCCESS;
 }
