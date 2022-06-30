2fbcbf30a52786326178f4118910cb58ebbb8f82
Andre McCurdy <armccurdy@gmail.com>
2022-06-30 16:38:36 -0700

    drop unnecessary CosaDmlDiGetHardwareVersion() -> platform_hal_GetHardwareVersion() abstraction
    Call platform_hal_GetHardwareVersion() directly from
    DeviceInfo_GetParamStringValue().
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
@@ -833,8 +833,20 @@ DeviceInfo_GetParamStringValue
 
     if (strcmp(ParamName, "HardwareVersion") == 0)
     {
-        CosaDmlDiGetHardwareVersion(NULL, pValue, pulSize);
+        if (*pulSize <= 64) {
+            *pulSize = 64 + 1;
+            return 1;
+        }
+
+        if (platform_hal_GetHardwareVersion(pValue) != RETURN_OK)
+            return -1;
+
         return 0;
+    }
+
+    if (strcmp(ParamName, "AdditionalHardwareVersion") == 0)
+    {
+        return update_pValue (pValue, pulSize, "");
     }
 
     if (strcmp(ParamName, "SoftwareVersion") == 0)
@@ -847,12 +859,6 @@ DeviceInfo_GetParamStringValue
         if (platform_hal_GetSoftwareVersion(pValue, *pulSize) != RETURN_OK)
             return -1;
 
-        return 0;
-    }
-
-    if (strcmp(ParamName, "AdditionalHardwareVersion") == 0)
-    {
-        CosaDmlDiGetAdditionalHardwareVersion(NULL, pValue, pulSize);
         return 0;
     }
 
