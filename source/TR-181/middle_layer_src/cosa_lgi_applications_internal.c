89347f2c5e03ba368a97a6eca0f8eb0038a40d6f
Ajay Vishnu Bhat <abhat@libertyglobal.com>
2022-07-13 00:10:33 +0200

    samknows: Plugin changes for MV3 to enable and disable SamKnows ( OFW-3030 )
    [Description]
    With the engineering release for SamKnows, the start and stop
    functionality has changed. The toggle is now via task_runner
    process to start and stop SamKnows.
    
    [Solution]
    The changes has been handled with a initial custom samknows_ispmon
    script and monitoring specific path for agent initialization.
    
    [Testing Performed]
    1. Enable samknows via the DM.
    2. Ensure the task_runner process is running in daemon mode.
    3. Disable samknows via the DM.
    4. Ensure the task_runner is killed.
    
    Upstream Candidate: LGI specific fix.
    
    Signed-off-by: Ajay Vishnu Bhat <abhat@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_lgi_applications_internal.c
+++ source/TR-181/middle_layer_src/cosa_lgi_applications_internal.c
@@ -50,14 +50,22 @@ ANSC_STATUS CosaLgiApplicationsInitialize ( ANSC_HANDLE hThisObject )
 
     ANSC_STATUS                   returnStatus = ANSC_STATUS_SUCCESS;
     PCOSA_DATAMODEL_LGI_APPLICATIONS pMyObject = (PCOSA_DATAMODEL_LGI_APPLICATIONS)hThisObject;
+    char *unitid;
 
     CosaDmlApplicationsSamKnowsGetEnabled(NULL, &pMyObject->SamKnowsEnable);
 
     ULONG propertyStrLen = SAMKNOWS_PROPERTY_STRING_LEN;
     CosaDmlApplicationsSamKnowsGetProperty(NULL, pMyObject->SamKnowsProperty, &propertyStrLen);
 
-    if (pMyObject->SamKnowsEnable == TRUE && (access( "/tmp/samknows/unitid", F_OK ) != 0)) {
-      system("/etc/init.d/samknows_ispmon start &");
+#if defined (_LG_MV3_)
+    unitid = "/var/run/opt/samknows/router_agent/unitid";
+#else
+    unitid = "/tmp/samknows/unitid";
+#endif
+
+    if ((pMyObject->SamKnowsEnable == TRUE) && (access(unitid, F_OK) != 0))
+    {
+        system("/etc/init.d/samknows_ispmon start &");
     }
 
     return returnStatus;
