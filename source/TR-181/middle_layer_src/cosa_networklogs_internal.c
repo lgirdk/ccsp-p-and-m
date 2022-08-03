7dbf83f06ca070475f9acd1b50c60878ad3279a8
akashdhanabal <adhanabal.ext@libertyglobal.com>
2022-08-03 17:10:31 +0530

    implement Device.NetworkLogs.Logs.{i}. data model ( OFW-3097 ) - Part 2
    Datamodel Object for storing Network Logs For GUI
    
    [BUG DESCRIPTION]
    New datamodel implementation to hold network log information
    
    1) Device.NetworkLogs.LogsNumberOfEntries
    2) Device.NetworkLogs.Logs.{i}.EventID
    3) Device.NetworkLogs.Logs.{i}.EventLevel
    4) Device.NetworkLogs.Logs.{i}.Time
    5) Device.NetworkLogs.Logs.{i}.Description
    
    [DESCRIPTION]
    Implementation of Network Logs datamodel for GUI.
    
    Functionality for accessing the below datamodels are implemented and initialized.
    1) Device.NetworkLogs.LogsNumberOfEntries
    2) Device.NetworkLogs.Logs.{i}.EventID
    3) Device.NetworkLogs.Logs.{i}.EventLevel
    4) Device.NetworkLogs.Logs.{i}.Time
    5) Device.NetworkLogs.Logs.{i}.Description
    
    [TESTING PERFORMED]
    1) GPV of Device.NetworkLogs. returns proper values.
    2) DML is also verified in Device. tree when executed GPV of Device.
    
    Note:
    This data model is only for Non Docsis Gateways.
    It is equivalent of Device.X_CISCO_COM_CableModem.DocsisLog.{i}
    
    Upstream Candidate: No. LGI specific data model.
    
    Signed-off-by: akashdhanabal <adhanabal.ext@libertyglobal.com>

---
+++ source/TR-181/middle_layer_src/cosa_networklogs_internal.c
@@ -0,0 +1,137 @@
+/*****************************************************************************
+ * Copyright 2022 Liberty Global B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+#ifdef FEATURE_RDKB_WAN_MANAGER
+
+#include "cosa_networklogs_internal.h"
+
+/**********************************************************************
+
+    Caller:     Owner of the object
+
+    Prototype:
+
+        ANSC_HANDLE
+        CosaNetworkLogsCreate
+            (
+                VOID
+            );
+
+    Description:
+
+        This function creates NetworkLogs object and return handle.
+
+    Argument:  
+
+    Return:    created NetworkLogs object.
+
+**********************************************************************/
+ANSC_HANDLE
+CosaNetworkLogsCreate
+    (
+        VOID
+    )
+{
+    PCOSA_DML_NETWORKLOGS pMyObject = (PCOSA_DML_NETWORKLOGS)NULL;
+
+    pMyObject = (PCOSA_DML_NETWORKLOGS)AnscAllocateMemory(sizeof(COSA_DML_NETWORKLOGS));
+
+    if(pMyObject == NULL)
+    {
+        return (ANSC_HANDLE)NULL;
+    }
+
+    pMyObject->Oid        = COSA_DATAMODEL_LGI_NETWORKLOGS_OID;
+    pMyObject->Create     = CosaNetworkLogsCreate;
+    pMyObject->Remove     = CosaNetworkLogsRemove;
+    pMyObject->Initialize = CosaNetworkLogsInitialize;
+
+    pMyObject->Initialize((ANSC_HANDLE)pMyObject);
+
+    return (ANSC_HANDLE)pMyObject;     
+}
+
+/**********************************************************************
+
+    Caller:     Self
+
+    Prototype:
+
+        ANSC_STATUS
+        CosaNetworkLogsInitialize
+            (
+                ANSC_HANDLE                 hThisObject
+            );
+
+    Description:
+
+        This function initiate  NetworkLogs object and return status.
+
+    Argument:	ANSC_HANDLE                 hThisObject
+            This handle is actually the pointer of this object itself.
+
+    Return:     operation status.
+
+**********************************************************************/
+ANSC_STATUS
+CosaNetworkLogsInitialize
+    (
+        ANSC_HANDLE                 hThisObject
+    )
+{
+    UNREFERENCED_PARAMETER(hThisObject);
+    PCOSA_DML_NETWORKLOGS pMyObject = (PCOSA_DML_NETWORKLOGS) hThisObject;
+    
+    //Initialization of NetworkLogs.Logs. object take place during Logs_Synchronize
+
+    return ANSC_STATUS_SUCCESS;
+}
+
+/**********************************************************************
+
+    Caller:     self
+
+    Prototype:
+
+        ANSC_STATUS
+        CosaNetworkLogsRemove
+            (
+                ANSC_HANDLE                 hThisObject
+            );
+
+    Description:
+
+        This function free memory of NetworkLogs object and return the status
+
+    Argument:   ANSC_HANDLE                 hThisObject
+            This handle is actually the pointer of this object itself.
+
+    Return:     operation status.
+
+**********************************************************************/
+ANSC_STATUS
+CosaNetworkLogsRemove
+    (
+        ANSC_HANDLE                 hThisObject
+    )
+{
+    PCOSA_DML_NETWORKLOGS pMyObject = (PCOSA_DML_NETWORKLOGS)hThisObject;
+
+    AnscFreeMemory((ANSC_HANDLE)pMyObject);
+
+    return ANSC_STATUS_SUCCESS;
+}
+
+#endif
