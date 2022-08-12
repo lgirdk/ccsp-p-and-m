f088d67d0881be961870eef0134615cc39348833
mkamat <mkamat@libertyglobal.com>
2022-08-12 11:52:20 -0700

    reject attempts to set Device.DeviceInfo.TemperatureStatus.TemperatureSensor.{i}.HighAlarmValue above certain limit ( OFW-3195 )
    Also add support for a new object to report whether the high alarm
    value has been reached.

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
@@ -5631,6 +5631,12 @@ TemperatureSensor_GetParamBoolValue
         return TRUE;
     }
 
+    if (strcmp(ParamName, "X_LGI-COM_CutOutTempExceeded") == 0)
+    {
+        *pBool = PTempSensorEntry->CutOutTempExceeded;
+        return TRUE;
+    }
+
     return FALSE;
 }
 
@@ -6037,13 +6043,19 @@ TemperatureSensor_SetParamIntValue
     /* check the parameter name and set the corresponding value */
     if (strcmp(ParamName, "LowAlarmValue") == 0)
     {
-        CosaTemperatureSensorSetLowAlarm(bValue, pTempSensorEntry);
+        if (CosaTemperatureSensorSetLowAlarm(bValue, pTempSensorEntry) != ANSC_STATUS_SUCCESS)
+        {
+            return FALSE;
+        }
         return TRUE;
     }
 
     if (strcmp(ParamName, "HighAlarmValue") == 0)
     {
-        CosaTemperatureSensorSetHighAlarm(bValue, pTempSensorEntry);
+        if (CosaTemperatureSensorSetHighAlarm(bValue, pTempSensorEntry) != ANSC_STATUS_SUCCESS)
+        {
+            return FALSE;
+        }
         return TRUE;
     }
 
