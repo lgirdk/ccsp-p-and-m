1e0e001febde16550a03966e4c369d2a8a55b419
Andre McCurdy <armccurdy@gmail.com>
2022-08-18 19:09:26 -0700

    reduce usage of legacy vsystem() API
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/middle_layer_src/cosa_dslite_dml.c
+++ source/TR-181/middle_layer_src/cosa_dslite_dml.c
@@ -103,7 +103,7 @@ DSLite_SetParamBoolValue
         {
               return FALSE;
         }
-        rc = v_secure_system("/bin/sh /etc/utopia/service.d/set_resolv_conf.sh");
+        rc = system("/bin/sh /etc/utopia/service.d/set_resolv_conf.sh");
         return TRUE;
     }
     return FALSE;
