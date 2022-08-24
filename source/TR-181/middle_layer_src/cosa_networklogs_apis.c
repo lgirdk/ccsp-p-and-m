35de49a4908b38242bc89f82194daf8a7dd99704
akashdhanabal <adhanabal.ext@libertyglobal.com>
2022-08-24 16:06:39 +0530

    remove sample NetworkLogs entry ( OFW-3292 )
    [Mv3] The Time parameter for Device.NetworkLogs. throws error due to the time format
    
    [BUG DESCRIPTION]:
    Due to the wrong time format, the Network log page in UI remains in loading state.
    
    [FIX DESCRIPTION]:
    The test object created for the datamodel population for reference and developing UI were removed. This framework will be replaced with the implementation to read logs.
    
    [TESTING PERFORMED]:
    1) Network Logs UI page loads properly.
    2) GPV of Device.NetworkLogs.LogsNumberOfEntries is 0.
    
    Upstream Candidate: No. LGI specific data model.
    
    Signed-off-by: akashdhanabal <adhanabal.ext@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_networklogs_apis.c
+++ source/TR-181/middle_layer_src/cosa_networklogs_apis.c
@@ -27,51 +27,7 @@ CosaDmlGetNetworkLogs
         PCOSA_DML_NETWORKLOGS_LOG   *ppConf
     )
 {
-    UNREFERENCED_PARAMETER(hContext);
-    PCOSA_DML_NETWORKLOGS_LOG pMyLog = (PCOSA_DML_NETWORKLOGS_LOG)NULL;
-
-    //Initialize values for datamodel population
-    char *description = "";
-    char *time        = "00:00:00";
-    errno_t rc        = -1;
-    int no_of_logs    = 1;
-    int count         = 0;
-
-    pMyLog = (PCOSA_DML_NETWORKLOGS_LOG)AnscAllocateMemory(no_of_logs * sizeof(COSA_DML_NETWORKLOGS_LOG));
-
-    if(pMyLog == NULL)
-    {
-        return ANSC_STATUS_FAILURE;
-    }
-
-    for(int i = 0; i < no_of_logs; i++)
-    {
-        pMyLog[i].Index      = 0;
-        pMyLog[i].EventID    = 1000;
-        pMyLog[i].EventLevel = 0;
-
-        rc = memcpy_s(pMyLog[i].Description, sizeof(pMyLog[i].Description), description, sizeof(pMyLog[i].Description));
-        if(rc != EOK)
-        {
-            ERR_CHK(rc);
-            free(pMyLog);
-
-            return ANSC_STATUS_FAILURE;
-        }   
-
-        rc = memcpy_s(pMyLog[i].Time, sizeof(pMyLog[i].Time), time, sizeof(pMyLog[i].Time));
-        if(rc != EOK)
-        {
-            ERR_CHK(rc);
-            free(pMyLog);
-
-            return ANSC_STATUS_FAILURE;
-        }
-        count++;
-    }
-
-    *pulCount = count;
-    *ppConf   = pMyLog;
+    //This is a framework and will be replaced by the implementation to get logs.
 
     return ANSC_STATUS_SUCCESS;
 }
