a18bd8fadf0fbdd9e3e1c8db87294837d2d57072
qlei <rlei@libertyglobal.com>
2022-08-26 13:33:43 -0700

    OFW-3272: Save and read back Device.NAT.PortMapping.{i}.Interface to/from utopia

--- source-arm/TR-181/board_sbapi/cosa_nat_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_nat_apis.c
@@ -1763,6 +1763,7 @@ ANSC_STATUS _AddPortMapping(
         strncpy(singleInfo.name, pEntry->Description, sizeof(singleInfo.name));
         snprintf(singleInfo.rule_source, sizeof(singleInfo.rule_source), (strlen(pEntry->RuleSource) != 0) ? pEntry->RuleSource : "Manual");
         strncpy(singleInfo.dest_ipv6, pEntry->X_CISCO_COM_InternalClientV6, sizeof(singleInfo.dest_ipv6));
+        strncpy(singleInfo.interface, pEntry->Interface, sizeof(singleInfo.interface));
         
         rc = Utopia_AddPortForwarding(pCtx, &singleInfo);
         if ( rc != SUCCESS )
@@ -1808,6 +1809,7 @@ ANSC_STATUS _AddPortMapping(
         snprintf(rangeInfo.rule_source, sizeof(rangeInfo.rule_source), (strlen(pEntry->RuleSource) != 0) ? pEntry->RuleSource : "Manual");
         /* CID 163037 fix */
 	strncpy(rangeInfo.dest_ipv6, pEntry->X_CISCO_COM_InternalClientV6, (sizeof(rangeInfo.dest_ipv6)-1));
+        strncpy(rangeInfo.interface, pEntry->Interface, sizeof(rangeInfo.interface));
 
         rc = Utopia_AddPortForwardingRange(pCtx, &rangeInfo);
         if ( rc != SUCCESS )
@@ -2576,6 +2578,8 @@ CosaDmlNatGetPortMappings
             pNatPMapping[ulIndex].InternalClient.Value = inet_addr(singleInfo[i].dest_ip);
             pNatPMapping[ulIndex].InstanceNumber = singleInfo[i].rule_id;
             pNatPMapping[ulIndex].X_CISCO_COM_Origin = COSA_DML_NAT_PMAPPING_Origin_Static;
+            safec_rc = strcpy_s(pNatPMapping[ulIndex].Interface,sizeof(pNatPMapping[ulIndex].Interface), singleInfo[i].interface);
+            ERR_CHK(safec_rc);
             safec_rc = strcpy_s(pNatPMapping[ulIndex].Description,sizeof(pNatPMapping[ulIndex].Description), singleInfo[i].name);
             ERR_CHK(safec_rc);
             safec_rc = strcpy_s(pNatPMapping[ulIndex].X_CISCO_COM_InternalClientV6,sizeof(pNatPMapping[ulIndex].X_CISCO_COM_InternalClientV6), singleInfo[i].dest_ipv6);
@@ -2605,6 +2609,8 @@ CosaDmlNatGetPortMappings
             pNatPMapping[ulIndex].InternalClient.Value = inet_addr(rangeInfo[i].dest_ip);
             pNatPMapping[ulIndex].InstanceNumber = rangeInfo[i].rule_id;
             pNatPMapping[ulIndex].X_CISCO_COM_Origin = COSA_DML_NAT_PMAPPING_Origin_Static;
+            safec_rc = strcpy_s(pNatPMapping[ulIndex].Interface,sizeof(pNatPMapping[ulIndex].Interface), rangeInfo[i].interface);
+            ERR_CHK(safec_rc);
             safec_rc = strcpy_s(pNatPMapping[ulIndex].Description,sizeof(pNatPMapping[ulIndex].Description), rangeInfo[i].name);
             ERR_CHK(safec_rc);
             safec_rc = strcpy_s(pNatPMapping[ulIndex].X_CISCO_COM_InternalClientV6,sizeof(pNatPMapping[ulIndex].X_CISCO_COM_InternalClientV6), rangeInfo[i].dest_ipv6);
