60a4b78b87a9a1719310ade8ae3c6c0821910ddb
Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>
2022-09-02 15:34:53 +0530

    implement LGI-COM parameters for Tunneled static IP service ( OFW-3288 )
    BUG DESCRIPTION:
    Implementation of Device.X_LGI-COM_TunneledStaticIPService. Datamodel parameters as per MVXREQ-2340
    
    FIX DESCRIPTION:
    Implemented the DML parameters for Device.X_LGI-COM_TunneledStaticIPService. tree as a prototype.
    Functional changes will be handled later.
    
    TESTING PERFORMED:
    1. Perform dmcli getv of Device.X_LGI-COM_TunneledStaticIPService.
    2. Perform dmcli setv of all the param under Device.X_LGI-COM_TunneledStaticIPService. tree.
    
    Upstream Candidate: No. LGI specific feature.
    
    Signed-off-by: Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>

---
+++ source/TR-181/middle_layer_src/cosa_lgi_tunneledstaticip_internal.h
@@ -0,0 +1,71 @@
+/*****************************************************************************
+ * Copyright 2022 Liberty Global B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ****************************************************************************/
+
+
+#ifndef  _COSA_LGI_TUNNELEDSTATICIP_INTERNAL_H
+#define  _COSA_LGI_TUNNELEDSTATICIP_INTERNAL_H
+
+#include "cosa_apis.h"
+#include "plugin_main_apis.h"
+#include "cosa_lgi_tunneledstaticip_apis.h"
+
+
+
+#define  COSA_DATAMODEL_LGI_TUNNELEDSTATICIP_CLASS_CONTENT    \
+    /* duplication of the base object class content */ \
+    COSA_BASE_CONTENT                                  \
+
+typedef  struct
+_COSA_DATAMODEL_LGI_TUNNELEDSTATICIP_CLASS_CONTENT
+{
+    COSA_DATAMODEL_LGI_TUNNELEDSTATICIP_CLASS_CONTENT
+    /* start of LGITUNNELEDSTATICIP object class content */
+    BOOL     Enable;
+    char     Username[256];
+    char     Password[256];
+    char     RadiusInterface;
+    char     RadiusAuthServerIPAddr[45];
+    char     RadiusAuthSecret[45];
+    int      RadiusAuthServerPort;
+    char     NASIdentifier[45];
+    char     RadiusAccServerIPAddr[45];
+    char     RadiusAccSecret[45];
+    int      RadiusAccServerPort;
+}
+COSA_DATAMODEL_LGI_TUNNELEDSTATICIP, *PCOSA_DATAMODEL_LGI_TUNNELEDSTATICIP;
+
+
+
+ANSC_HANDLE
+CosaLgiTunneledStaticIPCreate
+    (
+        VOID
+    );
+
+ANSC_STATUS
+CosaLgiTunneledStaticIPInitialize
+    (
+        ANSC_HANDLE                 hThisObject
+    );
+
+ANSC_STATUS
+CosaLgiTunneledStaticIPRemove
+    (
+        ANSC_HANDLE                 hThisObject
+    );
+
+#endif
+
