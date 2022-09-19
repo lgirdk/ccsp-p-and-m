974c3d2366333d70942e4ed8c3f9ad9a6ab13683
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2022-09-19 13:09:49 +0200

    fix 5G down when change GW from bridge mode to router mode via UI/SNMP/DML ( F3896SI-1345 OFW-3407 )
    [BUG DESCRIPTION]
    5G down when change GW from bridge mode to router mode via UI/SNMP/DML.
    WiFi Force Disable/Enable does depends upon the destination erouter mode. Sometimes gateway goes for reboot before the nvram DB is updated with settings change.
    
    [FIX DESCRIPTION]
    Broadcom wldm API's needs call of wifi_apply to update nvram DB with the settings changes via hal wifi_setRadioEnable.
    Calling Applysettings from ccsp-p-and-m during erouter mode change is not needed with this HAL call.
    Verified that in PUMA6 ATOM no need of calling Applysettings for updating the wifi DB.
    
    [TESTING PERFORMED]
    1. Verified that 5G/2.4G radio statuses are upto date during mode switches.
    
    Upstream Candidate: Maybe. Based upon TLV202 implementation. TLV202 is not there in rdk-next.
    
    Signed-off-by: BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
@@ -3967,8 +3967,13 @@ parameterValStruct_t valCommit1[] = {
                     faultParam = NULL;
                 }  
 #endif
+
+        char rebootevent[8];
+        sysevent_get(commonSyseventFd, commonSyseventToken, "reboot-triggered", rebootevent, sizeof(rebootevent));
+        if( atoi(rebootevent) != 1 )
+        {
                
-      ret = CcspBaseIf_setParameterValues
+            ret = CcspBaseIf_setParameterValues
                         (
                                 bus_handle,
                                 ppComponents[0]->componentName,
@@ -3986,7 +3991,7 @@ parameterValStruct_t valCommit1[] = {
                     faultParam = NULL;
                 }  
   #ifdef _XF3_PRODUCT_REQ_
-      ret = CcspBaseIf_setParameterValues
+            ret = CcspBaseIf_setParameterValues
                         (
                                 bus_handle,
                                 ppComponents[0]->componentName,
@@ -3997,16 +4002,16 @@ parameterValStruct_t valCommit1[] = {
                                 TRUE,   /* no commit */
                                 &faultParam
                         );
-        if (ret != CCSP_SUCCESS && faultParam)
-        {
-            AnscTraceError(("Error:Failed to SetValue for param '%s'\n", faultParam));
-            bus_info->freefunc(faultParam);
-            faultParam = NULL;
-        } 
+            if (ret != CCSP_SUCCESS && faultParam)
+            {
+                AnscTraceError(("Error:Failed to SetValue for param '%s'\n", faultParam));
+                bus_info->freefunc(faultParam);
+                faultParam = NULL;
+            } 
  #endif
 
 	// All the cases Radio should get update since transition will happen during full - psedo - router
-        {
+           {
                 parameterValStruct_t resetRadio[] = {{"Device.WiFi.X_CISCO_COM_ResetRadios", "true", ccsp_boolean}};
 
                 ret = CcspBaseIf_setParameterValues
@@ -4027,6 +4032,7 @@ parameterValStruct_t valCommit1[] = {
                     bus_info->freefunc(faultParam);
                     faultParam = NULL;
                 }
+            }
         }
 
         curticket++;
