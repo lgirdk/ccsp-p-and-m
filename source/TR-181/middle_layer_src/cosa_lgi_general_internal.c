ab616ed20d7cb43b89536f97404be27458fab064
Kunapareddi Ravi Kumar <kravikumar.contractor@libertyglobal.com>
2022-11-09 17:50:07 +0530

    update DataModelSpecification & DataModelVersion to 10.0.9 ( OFW-3538 CH7465SI-485 )
    [Issue]:
    Device.X_LGI-COM_General.CPE.DataModelSpecification doesn't reflect the actual datamodel version.
    
    [Fix Description]:
    Modified the DataModelSpecification and DataModelVersion strings
    
    [Testing Performed]:
    1. Bring up the DUT.
    2. Perform GPV on "Device.X_LGI-COM_General.CPE.DataModelSpecification" and it should display actual datamodel version.
    3. Perform GPV on "Device.X_LGI-COM_General.CPE.DataModelVersion" and it should display actual datamodel version.
    
    Signed-off-by: Ravi Kumar <kravikumar.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_lgi_general_internal.c
+++ source/TR-181/middle_layer_src/cosa_lgi_general_internal.c
@@ -141,8 +141,8 @@ CosaLgiGeneralInitialize
 
 
     /* Further implementation of this parameter is still under design discussion*/
-    AnscCopyString(pMyObject->DataModelVersion, "2.1.0");
-    AnscCopyString(pMyObject->DataModelSpecification, "3.0.0");
+    AnscCopyString(pMyObject->DataModelVersion, "10.0.9");
+    AnscCopyString(pMyObject->DataModelSpecification, "10.0.9");
 
     return returnStatus;
 }
