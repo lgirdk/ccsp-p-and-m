d4f788bc0880a23efe3650d16a2c1c829911bf7b
Devin Mungan <dmungan@libertyglobal.com>
2022-11-17 12:56:13 +0100

    ReverseSSH: update data type of xOpsReverseSshTrigger & hide ReverseSSH arguments ( OFW-3700 )
    Data model paths should be compatible with LGI specifications.
    
    [Issue]
    Data model paths should be compatible with LGI specifications
    
    [Description]
    NA
    
    [Solution]
    - Make "ReverseSSH.Arguments" unreadable
    - Change input type of "ReverseSSH.xOpsReverseSshTrigger" from string to bool
    
    [Testing Performed]
    1) Set RSSH arguments via below command
    	dmcli eRT setv Device.DeviceInfo.X_LGI-COM_DeviceMgmt.ReverseSSH.Arguments string
    		'host=<IP>;user=<user>;passwd=<password>;revsshport=<port>'
    2) Set Session timeout
    	dmcli eRT setv Device.DeviceInfo.X_LGI-COM_DeviceMgmt.ReverseSSH.SessionTimeout uint <timeout>
    3) start RSSH feature
    	dmcli eRT setv Device.DeviceInfo.X_LGI-COM_DeviceMgmt.ReverseSSH.Enable bool true
    4) Query tunnel status and confirm it's ACTIVE
    	dmcli eRT getv Device.DeviceInfo.X_LGI-COM_DeviceMgmt.ReverseSSH.TunnelStatus
    5) Observe that both SSH client and server is running and RSSH is successful from the Jump host side.
    
    Upstream Candidate: No, LG specific.
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
@@ -11413,7 +11413,13 @@ ReverseSSH_GetParamStringValue
     /* check the parameter name and return the corresponding value */
     if (strcmp(ParamName, "xOpsReverseSshArgs") == 0)
     {
-        getXOpsReverseSshArgs(NULL, pValue, pulSize);
+        rc = strcpy_s(pValue, *pulSize, "");
+        if (rc != EOK)
+        {
+            ERR_CHK(rc);
+            return -1;
+        }
+
         return 0;
     }
 
@@ -11438,17 +11444,6 @@ ReverseSSH_GetParamStringValue
             }
         }
 
-        return 0;
-    }
-
-    if (strcmp(ParamName, "xOpsReverseSshTrigger") == 0)
-    {
-        rc = strcpy_s(pValue, *pulSize, "");
-        if (rc != EOK)
-        {
-            ERR_CHK(rc);
-            return -1;
-        }
         return 0;
     }
 
@@ -11505,12 +11500,6 @@ ReverseSSH_SetParamStringValue
         return TRUE;
     }
 
-    if (strcmp(ParamName, "xOpsReverseSshTrigger") == 0)
-    {
-        setXOpsReverseSshTrigger(pString);
-        return TRUE;
-    }
-
     CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName));
 
     return FALSE;
@@ -11564,6 +11553,49 @@ ReverseSSH_SetParamUlongValue
     }
 
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
+    return FALSE;
+}
+
+BOOL
+ReverseSSH_GetParamBoolValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        BOOL*                       pBool
+    )
+{
+    UNREFERENCED_PARAMETER(hInsContext);
+
+    if (strcmp(ParamName, "xOpsReverseSshTrigger") == 0)
+    {
+        *pBool = FALSE;
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+BOOL
+ReverseSSH_SetParamBoolValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        BOOL                        bValue
+    )
+{
+    UNREFERENCED_PARAMETER(hInsContext);
+
+    if (strcmp(ParamName, "xOpsReverseSshTrigger") == 0)
+    {
+        if (setXOpsReverseSshTrigger(bValue ? "start" : "stop") == 0)
+        {
+            CcspTraceError(("setXOpsReverseSshTrigger failed\n"));
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
     return FALSE;
 }
 
