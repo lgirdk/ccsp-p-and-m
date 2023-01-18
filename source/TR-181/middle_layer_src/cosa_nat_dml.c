d3931407eaed9b4213c31d6c9479d713fe36954a
mkamat <mkamat@libertyglobal.com>
2023-01-18 15:34:54 -0800

    fix delete instance on Device.NAT.X_LGI-COM_NATPassthrough. is not persistent across reboot ( OFW-4019 )

--- source/TR-181/middle_layer_src/cosa_nat_dml.c
+++ source/TR-181/middle_layer_src/cosa_nat_dml.c
@@ -4268,7 +4268,12 @@ NATPassthrough_DelEntry
 
     if (pLinkObj->bNew)
     {
+        pLinkObj->bNew = FALSE;
         CosaFwReg_NATPassthroughDelInfo((ANSC_HANDLE)pMyObject, (ANSC_HANDLE)pLinkObj);
+    }
+    else
+    {
+        CosaDmlFW_NATPassthrough_DelEntry(pLinkObj->InstanceNumber);
     }
     if (!pNATPassthrough)
     {
