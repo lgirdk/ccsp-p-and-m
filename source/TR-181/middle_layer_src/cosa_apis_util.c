6bf420bc26249917af0d1dbb82164b709ed48a44
Andy Carvalho <acarvalho.contractor@libertyglobal.com>
2023-03-22 15:10:55 +0100

    fix memory leak in CosaUtilGetFullPathNameByKeyword ( OFW-4384 )
    Signed-off-by: Andy Carvalho <acarvalho.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_apis_util.c
+++ source/TR-181/middle_layer_src/cosa_apis_util.c
@@ -1347,6 +1347,13 @@ CosaUtilGetFullPathNameByKeyword
         AnscFreeMemory(pTableStringToken);
     }
 
+    // Free remaining tokens in the token chain
+    while (pTableStringToken)
+    {
+        AnscFreeMemory(pTableStringToken);
+        pTableStringToken = AnscTcUnlinkToken(pTableListTokenChain);
+    }
+
     if ( pTableListTokenChain )
     {
         AnscTcFree((ANSC_HANDLE)pTableListTokenChain);
