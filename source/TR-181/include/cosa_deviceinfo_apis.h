2fbd879db01387c7dc3de806fa533189f15c1b63
Devin Mungan <dmungan@libertyglobal.com>
2023-03-29 16:31:20 +0200

    ReverseSSH: fix Reverse SSH connection uses eRT interface and not the CM Interface ( OFW-4431 )
    [Issue]
    Reverse SSH connection uses eRT interface and not the CM Interface
    
    [Description]
    NA
    
    [Solution]
    trigger rssh with dedicated mgmt interface for each platform
    
    [Testing Performed]
    1) Set RSSH arguments via below command
    	dmcli eRT setv Device.DeviceInfo.X_LGI-COM_DeviceMgmt.ReverseSSH.Arguments string
    	'host=<IP>;user=<user>;passwd=<password>;revsshport=<port>'
    2) start RSSH feature
    	dmcli eRT setv Device.DeviceInfo.X_LGI-COM_DeviceMgmt.ReverseSSH.Enable bool true
    3) Query tunnel status and confirm it's ACTIVE
    	dmcli eRT getv Device.DeviceInfo.X_LGI-COM_DeviceMgmt.ReverseSSH.TunnelStatus
    4) Initiate ssh from jump host
    	ssh -p <port> <CPEUserName>@localhost
    5) Observe from packet capture that the connection is made from the mgmt interface
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)

--- source/TR-181/include/cosa_deviceinfo_apis.h
+++ source/TR-181/include/cosa_deviceinfo_apis.h
@@ -517,7 +517,7 @@ CosaDmlDiSetAndProcessDhcpServDetectionFlag
    );
 
 int setXOpsReverseSshTrigger(char *input); 
-int setXOpsReverseSshArgs(char* pString);
+ANSC_STATUS setXOpsReverseSshArgs(char* pString);
 ANSC_STATUS getXOpsReverseSshArgs
     (
         ANSC_HANDLE                 hContext,
