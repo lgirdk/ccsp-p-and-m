e4b1545a0b6811e9b81452230908a7bdea076275
anil shetty <adshetty@libertyglobal.com>
2023-04-07 15:17:34 -0700

    ReverseSSH: clear reverse SSH arguments once enabled ( OFW-4433 )

--- source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
@@ -3081,9 +3081,14 @@ int setXOpsReverseSshTrigger(char *input)
     trigger = strstr(input, "start");
     if (trigger)
     {
+        if (reverseSSHArgs[0] == '\0')
+        {
+            CcspTraceError(("%s-%d : Error! Reverse SSH arguments!\n", __FUNCTION__, __LINE__ ));
+            return NOK;
+        }
         if (getExtraArgs(extraArgs, sizeof(extraArgs)) != ANSC_STATUS_SUCCESS)
         {
-            CcspTraceError(("getExtraArgs failed! Returning"));
+            CcspTraceError(("getExtraArgs failed! Returning\n"));
             return NOK;
         }
 #ifdef ENABLE_SHORTS
@@ -3114,6 +3119,8 @@ int setXOpsReverseSshTrigger(char *input)
 #ifdef ENABLE_SHORTS
         }
 #endif
+        /* Clear the arguments once reverse SSH is enabled */
+        memset(reverseSSHArgs, 0, sizeof(reverseSSHArgs));
     }
     else
     {
