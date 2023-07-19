f69d46bcddd5e8ad83834900af05a39077e044f1
David Fang <david_fang@cable.comcast.com>
2023-07-19 23:00:04 +0000

    XD4-258: Update Device.DeviceInfo for XD4
    Reason for change: Update for XD4
    Test Procedure: None
    Risks: None
    
    Change-Id: I5beeeae52d440080eecf6daf732ab05d1a95b442
    Signed-off-by: David Fang <david_fang@cable.comcast.com>
    (cherry picked from commit bdacf58bad3c8d4927ada18e646dc9092f8207e8)
    (cherry picked from commit a282649a0449366f305dbf36b0c8067d30a42616)

--- source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
@@ -684,6 +684,14 @@ CosaDmlDiGetProductClass
                     return ANSC_STATUS_FAILURE;
                 }
 	}
+#elif defined(MODEM_ONLY_SUPPORT)
+    {
+        rc = strcpy_s(pValue, *pulSize, "XD4");
+        if ( rc != EOK) {
+            ERR_CHK(rc);
+            return ANSC_STATUS_FAILURE;
+        }
+    }
 #elif defined(_XB8_PRODUCT_REQ_)
     {
         rc = strcpy_s(pValue, *pulSize, "XB8");
