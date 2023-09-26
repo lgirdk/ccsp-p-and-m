88996867b0f85454e846ff12ae82b3aa73db6f58
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2023-09-26 18:02:14 +0200

    RIPv2: Notify Tr069 agent on single static RIPv2 enable scenario ( OFW-5066 )
    implementing support services needing Internet access on Gateway with Single Fix IP
    [DESCRIPTION]
    Use value change signal support for notifying tr069 agent about Single static
    RIPv2 enable.
    Single Static IP Datamodel is configured to pass on to the agent via PSM
    entry SecondUpstreamIpAddress.
    
    During this notification, the active/passive
    notification on Primary erouter0 IP, Secondary erouter IP and RIP Enable
    Data models are toggled at the tr069 agent.
    
    [TESTING PERFORMED]
    Verified that the ACS connection request URL is re generated to use
    RIPv2 secondary IP
    
    Signed-off-by: BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>

--- source-arm/TR-181/board_sbapi/cosa_ip_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_ip_apis.c
@@ -2876,6 +2876,7 @@ CosaDmlIpIfGetV4Addr
             {
                 strcpy(pEntry->Alias, alias);
             }
+            syscfg_set_u(NULL,"erouter_static_ip_instance", pEntry->InstanceNumber); 
             ErouterStaticIfMode("restart");
         }
         else
@@ -3256,6 +3257,7 @@ CosaDmlIpIfSetV4Addr
         if(erouter_static_ip != NULL)
         {
             syscfg_set(NULL, "erouter_static_ip_address", erouter_static_ip);
+            syscfg_set_u(NULL,"erouter_static_ip_instance", pEntry->InstanceNumber);
         }
         /*
             Fixme: uncomment the following logic once find a way to
