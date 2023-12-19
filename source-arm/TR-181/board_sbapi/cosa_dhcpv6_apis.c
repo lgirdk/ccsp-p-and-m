fd4196cd530642de15147b6b8d53d42372cd7952
Damodaran, Goutam <Goutam_Damodaran@comcast.com>
2023-12-19 05:07:44 +0000

    RDKB-53016 : dibbler restart due to incomplete serverconf file"
    Reason for change: Observed continuous dibbler restart due to
    incomplete serverconf file due to trigger restart
    Test Procedure: dibbler should work properly
    Risks: Low
    Priority:P1
    Signed-off-by:Goutam_Damodaran@comcast.com
    
    (cherry picked from commit b56ea9cb5c0384f9c16f1e9b709bf8c20feccb42)
    Change-Id: Ifb122958e290cf272ffe8ca381556983634cb76c

--- source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dhcpv6_apis.c
@@ -9522,6 +9522,13 @@ dhcpv6c_dbg_thrd(void * in)
 #endif
                         g_dhcpv6_server_prefix_ready = TRUE;
 
+#if defined (_COSA_BCM_ARM_)
+#ifndef _SKY_HUB_COMMON_PRODUCT_REQ_
+                        CcspTraceDebug(("%s,%d: Calling CosaDmlDHCPv6sTriggerRestart(FALSE)...\n", __FUNCTION__, __LINE__));
+                        CosaDmlDHCPv6sTriggerRestart(FALSE);
+#endif
+#endif /* _COSA_BCM_ARM_ && !_SKY_HUB_COMMON_PRODUCT_REQ_ */
+
                         /*We need get a global ip addres */
 #if defined(_COSA_BCM_ARM_) || defined(INTEL_PUMA7)
 #ifndef _HUB4_PRODUCT_REQ_
@@ -9593,8 +9600,11 @@ dhcpv6c_dbg_thrd(void * in)
                             CcspTraceWarning(("%s: setting lan-restart\n", __FUNCTION__));
                             commonSyseventSet("lan-restart", "1");
 
+#if defined (_SKY_HUB_COMMON_PRODUCT_REQ_)
                             CcspTraceDebug(("%s,%d: Calling CosaDmlDHCPv6sTriggerRestart(FALSE)...\n", __FUNCTION__, __LINE__));
                             CosaDmlDHCPv6sTriggerRestart(FALSE);
+#endif /* _SKY_HUB_COMMON_PRODUCT_REQ_ */
+
                         }
 #endif
 
