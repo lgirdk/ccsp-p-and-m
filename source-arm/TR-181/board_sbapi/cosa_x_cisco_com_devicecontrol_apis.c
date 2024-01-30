7f6039248a41daf5668139b5cff4902a591ccd97
qlei <rlei@libertyglobal.com>
2024-01-30 13:16:15 -0800

    reset logger to default at factory reset ( OFW-5471 )

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
@@ -2323,6 +2323,9 @@ CosaDmlDcSetFactoryReset
 	}
 #endif
 
+#ifdef _PUMA6_ARM_
+    logger_set_defaults();  /* Fix for OFW-5471 */
+#endif
 
 #if defined(_XER5_PRODUCT_REQ_)
     v_secure_system("rm -rf /nvram/config.db");
