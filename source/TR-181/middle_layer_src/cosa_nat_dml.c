78f7cd5e514ca48007937a567765d597101c09b3
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2024-06-19 17:21:35 +0530

    fix overlapping port rules created for port triggering ( OFW-5311 )
    [Issue]
    port triggering trigger port is not considered for overlap check
    
    [Issue Description]
    Port triggering trigger port is corresonding to erouter0 interface.
    While creating other rules like port forwarding/portfiltering overlap
    check should be done against port trigger rules trigger port as well.
    
    [Fix Description]
    Add missing validations
    
    [Testing Performed]
    Verified Port overlap check between trigger port and other rules
    
    Signed-off-by: BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>

--- source/TR-181/middle_layer_src/cosa_nat_dml.c
+++ source/TR-181/middle_layer_src/cosa_nat_dml.c
@@ -3926,7 +3926,7 @@ PortTrigger_Validate
         (FALSE == CosaDmlNatChkPortRange(pPortTrigger->InstanceNumber, pPortTrigger->bEnabled, pPortTrigger->ForwardPortStart, pPortTrigger->ForwardPortEnd, pPortTrigger->ForwardProtocol, 1 )) ||
         //(!CosaDmlChkDesp(pPortTrigger->Description)) ||
         (FALSE == CosaDmlNatChkTargetPortRange(pPortTrigger->InstanceNumber, pPortTrigger->bEnabled, pPortTrigger->ForwardPortStart,pPortTrigger->ForwardPortEnd, pPortTrigger->ForwardProtocol,0,1)) ||
-        (FALSE == CosaDmlNatChkTriggerPortRange(pPortTrigger->InstanceNumber, pPortTrigger->bEnabled, pPortTrigger->TriggerPortStart, pPortTrigger->TriggerPortEnd, pPortTrigger->TriggerProtocol, 1 ))){
+        (FALSE == CosaDmlNatChkPortRange(pPortTrigger->InstanceNumber, pPortTrigger->bEnabled, pPortTrigger->TriggerPortStart, pPortTrigger->TriggerPortEnd, pPortTrigger->TriggerProtocol, 1 ))){
 
         CcspTraceWarning(("Parameter Error in %s \n", __FUNCTION__));
     
