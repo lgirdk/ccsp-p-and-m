c0cf37a01334424073048e8dfd76ce06107b92cd
Robert Lee <Robert_Lee2@comcast.com>
2024-08-21 18:42:20 +0000

    XER10-462: fix factory reset button behavior
    Reason for change: fix factory reset button behavior
    
    Test Procedure: build and verfiy
    
    Risks: low
    Change-Id: I2324aee95b969732ec9512a44d5adf311f9cb4e4
    Signed-off-by: Robert_Lee2@comcast.com
    (cherry picked from commit 468494698a8355382325c0ea5b0d31dfabf7dc61)

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
@@ -1890,13 +1890,19 @@ void* restoreAllDBs(void* arg)
 //        v_secure_system("sync; find /nvram /nvram2 /data -mindepth 1 | grep -vE \"Q[[:xdigit:]]{8}$\" | xargs rm -r; sync");  /* remove all files from user directory */
 	v_secure_system("sync;find /nvram /nvram2 /data ! \\( -path '/nvram/.partner_ID' -o -regex '.*/Q[[:xdigit:]]\\{8\\}$' -o -path '/nvram/.apply_partner_defaults' \\) -mindepth 1 | xargs rm -r; sync");
 	// set lastreboot reason directly into db
+#if defined (_SCER11BEL_PRODUCT_REQ_)
+/* run another cleanup just to make sure if above script did not clean it */
+        v_secure_system("rm -rf /data/.comcast_config_set.done /data/nvram_cfg.txt /data/psi* /data/.nvram_restore_cfg.txt /data/psi_wifi /data/.user_nvram.setting /data/onewifi_downgrade_required /data/.sky_config_set.done /nvram/.bcmwifi_xhs_lnf_enabled /nvram/secure/wifi/* /nvram/wifi/*");
+        v_secure_system("sync; touch /data/.do_fr_on_boot; sync");
+#endif
         v_secure_system("mkdir -p /nvram/secure/data/ && touch $_/syscfg.db");
         v_secure_system("echo \"X_RDKCENTRAL-COM_LastRebootReason=factory-reset\" > /nvram/secure/data/syscfg.db");
         v_secure_system("echo \"X_RDKCENTRAL-COM_LastRebootCounter=1\" >> /nvram/secure/data/syscfg.db");
        // set factory_reset flag directory into db to restore the db value in bootup case
-#if defined (_WNXL11BWL_PRODUCT_REQ_)
+#if defined (_WNXL11BWL_PRODUCT_REQ_) || defined (_SCER11BEL_PRODUCT_REQ_)
         v_secure_system("echo \"factory_reset=y\" >> /nvram/secure/data/syscfg.db");
 #endif
+
         v_secure_system("sync");
         /* run addition clean up to fix /nvram/secure corruption issue where encryption key didn't get clean up */
         // v_secure_system("/bin/dd if=/dev/zero of=/dev/mmcblk0p7 count=32768");  /* wipe out /nvram mount partition */
@@ -2494,6 +2500,11 @@ CosaDmlDcSetFactoryReset
 		CcspTraceWarning(("FactoryReset:%d  case is both WIFI and Router removing DB\n",__LINE__));
 		v_secure_system("rm -f /nvram/wifi/rdkb-wifi.db"); //Need to remove wifi-db for Onewifi
 		v_secure_system("rm -f /opt/secure/wifi/rdkb-wifi.db"); //Need to remove wifi-db for Onewifi
+#if defined (_SCER11BEL_PRODUCT_REQ_)
+/* clean up BRCM nvram */
+        v_secure_system("rm -rf /data/.comcast_config_set.done /data/nvram_cfg.txt /data/psi* /data/.nvram_restore_cfg.txt /data/psi_wifi /data/.user_nvram.setting /data/onewifi_downgrade_required /data/.sky_config_set.done /nvram/.bcmwifi_xhs_lnf_enabled /nvram/secure/wifi/* /nvram/wifi/*");
+        v_secure_system("sync; touch /data/.do_fr_on_boot; sync");
+#endif
 	}
 #endif
 
